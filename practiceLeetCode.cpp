e8273a6f47cfdcd0eadf0f27ee098a98a9c445e9
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-12-17 23:42:11 +0530

    commit

--- practiceLeetCode.cpp
+++ practiceLeetCode.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+vector<vector<int>> permute(vector<int> &nums)
+{
+}
 int main()
 {
+    vector<int> nums = {1, 2, 3};
+    for (vector<int> vect1d : permute(nums))
+    {
+        for (auto &&i : vect1d)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
--- practiceLeetCode.java
+++ practiceLeetCode.java
@@ -1,9 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * practiceLeetCode
  */
 public class practiceLeetCode {
+    private void recurPermute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq) {
+        if (ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!freq[i]) {
+                freq[i] = true;
+                ds.add(nums[i]);
+                recurPermute(nums, ds, ans, freq);
+                ds.remove(ds.size() - 1);
+                freq[i] = false;
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean freq[] = new boolean[nums.length];
+        recurPermute(nums, ds, ans, freq);
+        return ans;
+    }
 
     public static void main(String[] args) {
+        practiceLeetCode l = new practiceLeetCode();
+        int nums[] = { 1, 2, 3 };
 
+        for (List<Integer> vect1d : l.permute(nums)) {
+            for (int i : vect1d) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
