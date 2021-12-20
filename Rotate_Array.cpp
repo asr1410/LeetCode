2f963dbe31e792e5b40269e50be2adf6fb874f78
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-12-20 12:56:58 +0530

    commit

--- Rotate_Array.cpp
+++ Rotate_Array.cpp
@@ -2,9 +2,30 @@
 using namespace std;
 void rotate(vector<int> &nums, int k)
 {
-    reverse(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.begin() + (k % nums.size()));
-    reverse(nums.begin() + (k % nums.size()), nums.end());
+    for (int n = nums.size(), start = 0; k = k % n; n = n - k, start = start + k)
+    {
+        cout << "outside" << endl;
+        cout << n << endl;
+        cout << start << endl;
+        cout << k << endl;
+        for (int i = 0; i < k; i++)
+        {
+            cout << "inside" << endl;
+            cout << "1st  " << start + i << endl;
+            cout << "2nd  " << start + n - k + i << endl;
+            swap(nums[start + i], nums[start + n - k + i]);
+            for (auto &&i : nums)
+            {
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    // reverse(nums.begin(), nums.end());
+    // reverse(nums.begin(), nums.begin() + (k % nums.size()));
+    // reverse(nums.begin() + (k % nums.size()), nums.end());
+
     // if (nums.size() < k)
     // {
     //     k %= nums.size();
@@ -16,6 +37,7 @@ void rotate(vector<int> &nums, int k)
     //     nums.pop_back();
     // }
     // nums.insert(nums.begin(), temp.begin(), temp.end());
+
     // while (k--)
     // {
     //     int temp = nums.back();
@@ -26,11 +48,11 @@ void rotate(vector<int> &nums, int k)
 
 int main()
 {
-    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
-    rotate(nums, 10);
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8};
+    rotate(nums, 11);
     for (auto &&i : nums)
     {
-        cout << i << endl;
+        // cout << i << endl;
     }
 
     return 0;
--- practiceLeetCode.cpp
+++ practiceLeetCode.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+void rotate(vector<int> &nums, int k)
+{
+    for (int n = nums.size(), start = 0; k = k % n; n = n - k, start = start + k)
+    {
+        cout << "outside" << endl;
+        cout << n << endl;
+        cout << start << endl;
+        cout << k << endl;
+        for (int i = 0; i < k; i++)
+        {
+            cout << "inside" << endl;
+            cout << "1st  " << start + i << endl;
+            cout << "2nd  " << start + n - k + i << endl;
+            swap(nums[start + i], nums[start + n - k + i]);
+            for (auto &&i : nums)
+            {
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+    }
+}
 
 int main()
 {
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8};
+    rotate(nums, 11);
+    for (auto &&i : nums)
+    {
+        // cout << i << endl;
+    }
 
     return 0;
 }
