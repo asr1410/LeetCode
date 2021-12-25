a8fdc8d56aee0c0612a94172f71df95e0dc61ac8
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2021-12-25 19:51:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (20.54%) - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -4,15 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int lo = 0;
-        while( lo <= n )
+        int low = 1;
+        while(low <= n)
         {
-            int mid = lo + (n - lo)/2;
+            int mid = low + (n - low)/2;
             if(isBadVersion(mid))
                 n = mid - 1;
             else
-                lo = mid + 1;
+                low = mid + 1;
         }
-        return lo;
+        return low;
     }
 };
