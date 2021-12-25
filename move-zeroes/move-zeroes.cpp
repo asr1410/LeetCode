635ae4ac84b8f10c933a55269a89f1c1cfbc14a7
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2021-12-25 22:17:04 +0530

    Time: 42 ms (17.51%), Space: 19.4 MB (12.27%) - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -4,14 +4,10 @@ public:
         int j = 0;
         for(int i = 0; i < nums.size(); i++)
         {
-            if(nums[i]!=0)
-            {
+            if(nums[i] != 0)
                 nums[j++] = nums[i];
-            }
         }
-        for( ; j < nums.size(); j++)
-        {
+        for(; j < nums.size(); j++)
             nums[j] = 0;
-        }
     }
 };
