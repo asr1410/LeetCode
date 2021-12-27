1e52fbfd449d4e7ccb68677b1e593dd8f882a2e7
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2021-12-27 15:43:51 +0530

    Time: 16 ms (45.02%), Space: 13.1 MB (96.03%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,33 +1,65 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        queue<pair<int, int>> q;
+        bool cornerrow = false;
+        bool cornercol = false;
     for (int i = 0; i < matrix.size(); i++)
     {
         for (int j = 0; j < matrix[i].size(); j++)
         {
             if (matrix[i][j] == 0)
             {
-                q.push({i, j});
+                if (i == 0)
+                    cornerrow = true;
+                else if(j == 0)
+                    cornercol = true;
+                else
+                    matrix[i][0] = matrix[0][j] = 0;
             }
         }
     }
-    int m = matrix.size();
-    int n = matrix[0].size();
-    // showQueue(q);
-    while (!q.empty())
+    for (int i = matrix.size() - 1; i >= 0; i--)
     {
-        int i = q.front().first;
-        int j = q.front().second;
-        for (int k = 0; k < m; k++)
+        for (int j = matrix[i].size() - 1; j >= 0; j--)
         {
-            matrix[k][j] = 0;
+            if(i == 0 && cornerrow == true)
+                matrix[i][j] = 0;
+            else if(j == 0 && cornercol == true)
+                matrix[i][j] = 0;
+            else if (matrix[i][0] == 0 or matrix[0][j] == 0)
+            {
+                matrix[i][j] = 0;
+            }
         }
-        for (int k = 0; k < n; k++)
-        {
-            matrix[i][k] = 0;
-        }
-        q.pop();
     }
+
+    if (cornerrow or cornercol)
+    {
+        matrix[0][0] = 0;
+    }
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 1, 1, 1}, {1, 0, 1, 1}, {1, 1, 0, 1}, {0, 0, 0, 1}};
+    for (auto &&i : matrix)
+    {
+        for (auto &&i : i)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    setZeroes(matrix);
+    for (auto &&i : matrix)
+    {
+        for (auto &&i : i)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
     }
 };
