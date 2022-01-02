39492e08ff59f8aa5287d21aa92efa6027df664d
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-03 02:18:43 +0530

    Time: 120 ms (11.96%), Space: 54.6 MB (5.50%) - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> M;
-        for(auto &&s: nums)
-        {
-            if (M.find(s) == M.end())
-                M.insert(make_pair(s, 1));
-            else
-                return true;
-        }
-        return false;
+        return set<int>(nums.begin(), nums.end()).size() < nums.size();
     }
 };
