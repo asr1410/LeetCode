a8e41ed578bb354ea0c5eb1b3d8b82b70d7d3566
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-04 05:42:59 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (5.99%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0)
+            return 1;
+        vector<int> v;
+    // convert to binary representation
+    while (n != 0) {
+        v.push_back(n % 2);
+        n = n / 2;
+    }
+    reverse(v.begin(), v.end());
+    // change 1's to 0 and 0's to 1
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == 0)
+            v[i] = 1;
+        else
+            v[i] = 0;
+    }
+    // convert back to number representation
+    int two = 1;
+    for (int i = v.size() - 1; i >= 0; i--) {
+        n = n + v[i] * two;
+        two = two * 2;
+    }
+    return n;
+    }
+};
