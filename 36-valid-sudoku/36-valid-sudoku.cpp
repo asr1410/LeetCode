9d0821e8c4056ff572eba63e8c75907f2bc1368c
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-07 03:08:50 +0530

    Time: 20 ms (79.89%), Space: 17.8 MB (97.10%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,23 +3,25 @@ class Solution
 public:
      bool isValidSudoku(vector<vector<char>> &board)
      {
-          int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
-
-          for (int i = 0; i < board.size(); ++i)
+          bool row[9][9] = {};
+          bool col[9][9] = {};
+          bool sub[9][9] = {};
+          for (int i = 0; i < 9; i++)
           {
-               int used1[9] = {0};
-               for (int j = 0; j < board[i].size(); ++j)
+               for (int j = 0; j < 9; j++)
                {
                     if (board[i][j] != '.')
                     {
-                         int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
-                         if (used1[num] || used2[j][num] || used3[k][num])
+                         int num = board[i][j] - '0' - 1;
+                         int k = i / 3 * 3 + j / 3;
+                         if (row[i][num] || col[j][num] || sub[k][num])
+                         {
                               return false;
-                         used1[num] = used2[j][num] = used3[k][num] = 1;
+                         }
+                         row[i][num] = col[j][num] = sub[k][num] = true;
                     }
                }
           }
-
           return true;
      }
 };
