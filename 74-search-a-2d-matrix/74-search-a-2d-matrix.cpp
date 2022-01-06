2412b44d5a747f6f45e2d4494f55587aa398e883
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-07 03:33:40 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (51.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,21 +1,16 @@
-class Solution
-{
+class Solution {
 public:
-     bool searchMatrix(vector<vector<int>> &matrix, int target)
-     {
-          int row = 0;
-          for (int i = 0; i < matrix.size(); i++)
-          {
-               if (matrix[i][0] <= target && matrix[i][matrix[0].size() - 1] >= target)
-               {
-                    for (int j = 0; j < matrix[i].size(); j++)
-                    {
-                         if (matrix[i][j] == target)
-                              return true;
-                    }
-                    return false;
-               }
-          }
-         return false;
-     }
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int l = 0, r = m * n - 1;
+        while (l != r){
+            int mid = (l + r - 1) >> 1;
+            if (matrix[mid / m][mid % m] < target)
+                l = mid + 1;
+            else 
+                r = mid;
+        }
+        return matrix[r / m][r % m] == target;
+    }
 };
