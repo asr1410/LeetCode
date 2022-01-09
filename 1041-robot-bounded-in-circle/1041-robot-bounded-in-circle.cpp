b9628bc1ed0aff798b05743e11ce9fd6dacd3248
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-09 23:19:37 +0530

    Time: 4 ms (11.40%), Space: 6.3 MB (19.45%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
-    bool isRobotBounded(string inst)
+    bool isRobotBounded(string instructions)
 {
-     int x = 0;
-     int y = 0;
-     char dir = 'N';
-     int count = 100;
-     while (count--)
-     {
-          for (int i = 0; i < inst.size(); i++)
-          {
-               if (inst[i] == 'G')
-                    if (dir == 'N')
-                         y = y + 1;
-                    else if (dir == 'W')
-                         x = x - 1;
-                    else if (dir == 'E')
-                         x = x + 1;
-                    else
-                         y = y - 1;
-               else if (inst[i] == 'L')
-                    if (dir == 'N')
-                         dir = 'W';
-                    else if (dir == 'W')
-                         dir = 'S';
-                    else if (dir == 'S')
-                         dir = 'E';
-                    else
-                         dir = 'N';
-               else if (inst[i] == 'R')
-                    if (dir == 'N')
-                         dir = 'E';
-                    else if (dir == 'E')
-                         dir = 'S';
-                    else if (dir == 'S')
-                         dir = 'W';
-                    else
-                         dir = 'N';
-          }
-          if (x == 0 && y == 0)
-               return true;
-     }
-     return false;
+     int x = 0, y = 0, i = 0;
+     vector<vector<int>> direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+     for (char &ins : instructions)
+          if (ins == 'R')
+               i = (i + 1) % 4;
+          else if (ins == 'L')
+               i = (i + 3) % 4;
+          else
+               x += direction[i][0], y += direction[i][1];
+     return x == 0 && y == 0 || i > 0;
 }
 };
