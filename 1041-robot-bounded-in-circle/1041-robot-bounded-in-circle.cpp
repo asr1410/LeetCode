19789f3b48533b6877ad1d2638d8a19b9f26a5ae
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-09 23:21:48 +0530

    Time: 3 ms (15.96%), Space: 6.1 MB (79.18%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -2,15 +2,45 @@ class Solution {
 public:
     bool isRobotBounded(string instructions)
 {
-     int x = 0, y = 0, i = 0;
-     vector<vector<int>> direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-     for (char &ins : instructions)
-          if (ins == 'R')
-               i = (i + 1) % 4;
-          else if (ins == 'L')
-               i = (i + 3) % 4;
-          else
-               x += direction[i][0], y += direction[i][1];
-     return x == 0 && y == 0 || i > 0;
+     int x = 0;
+     int y = 0;
+     char direction = 'N';
+     int count = 100;
+     while (count--)
+     {
+          for (int i = 0; i < instructions.size(); i++)
+          {
+               if (instructions[i] == 'G')
+                    if (direction == 'N')
+                         y = y + 1;
+                    else if (direction == 'W')
+                         x = x - 1;
+                    else if (direction == 'E')
+                         x = x + 1;
+                    else
+                         y = y - 1;
+               else if (instructions[i] == 'L')
+                    if (direction == 'N')
+                         direction = 'W';
+                    else if (direction == 'W')
+                         direction = 'S';
+                    else if (direction == 'S')
+                         direction = 'E';
+                    else
+                         direction = 'N';
+               else if (instructions[i] == 'R')
+                    if (direction == 'N')
+                         direction = 'E';
+                    else if (direction == 'E')
+                         direction = 'S';
+                    else if (direction == 'S')
+                         direction = 'W';
+                    else
+                         direction = 'N';
+          }
+          if (x == 0 && y == 0)
+               return true;
+     }
+     return false;
 }
 };
