8058bdec321a776e1996985dfbe52d50b52ddfa5
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-11 00:07:59 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (96.15%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -4,15 +4,10 @@ public:
 {
      stack<char> tube;
      for (int i = 0; i < s.length(); i++)
-     {
           if (!tube.empty() and ((tube.top() == '(' and s[i] == ')') || (tube.top() == '[' and s[i] == ']') || (tube.top() == '{' and s[i] == '}')))
                tube.pop();
           else
-          {
-               cout << s[i] << endl;
                tube.push(s[i]);
-          }
-     }
      return tube.empty();
 }
 };
