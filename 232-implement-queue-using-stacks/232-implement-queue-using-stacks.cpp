f2839059b469643d0a6328c37ff76cbc02c3941d
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-13 14:35:55 +0530

    Time: 0 ms (100.00%), Space: 7 MB (88.35%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,63 +1,36 @@
 class MyQueue
 {
-     stack<int> que, help;
+     stack<int> que;
 
 public:
-     MyQueue()
-     {
-     }
-
      void push(int x)
      {
-          que.push(x);
+          if (!que.size())
+          {
+               que.push(x);
+               return;
+          }
+          int data = que.top();
+          que.pop();
+          push(x);
+          que.push(data);
      }
 
      int pop()
      {
-          int x;
-          if (!help.empty())
-          {
-               int x = help.top();
-               help.pop();
-               return x;
-          }
-          else
-          {
-               while (!que.empty())
-               {
-                    x = que.top();
-                    que.pop();
-                    help.push(x);
-               }
-               x = help.top();
-               help.pop();
-               return x;
-          }
+         int x = que.top();
+         que.pop();
+         return x;
      }
 
      int peek()
      {
-          int x;
-          if (!help.empty())
-          {
-               x = help.top();
-               return x;
-          }
-          else
-          {
-               while (!que.empty())
-               {
-                    x = que.top();
-                    que.pop();
-                    help.push(x);
-               }
-               x = help.top();
-               return x;
-          }
+          int x = que.top();
+         return x;
      }
 
      bool empty()
      {
-          return (que.size() + help.size()) == 0;
+          return que.size() == 0;
      }
 };
