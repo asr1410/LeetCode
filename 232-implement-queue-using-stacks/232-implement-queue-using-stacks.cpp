ce0a15ae2e337af4416bfa252e9270f19da55cb2
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-13 14:39:10 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (98.74%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -15,20 +15,17 @@ public:
           push(x);
           que.push(data);
      }
-
      int pop()
      {
-         int x = que.top();
-         que.pop();
-         return x;
+          int x = que.top();
+          que.pop();
+          return x;
      }
-
      int peek()
      {
           int x = que.top();
-         return x;
+          return x;
      }
-
      bool empty()
      {
           return que.size() == 0;
