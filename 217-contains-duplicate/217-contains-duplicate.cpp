b942164139b9756862a2db64947d8684cc1100e9
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-17 22:59:23 +0530

    Time: 116 ms (33.59%), Space: 52.1 MB (21.13%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,6 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        return nums.size() > set<int>(nums.begin(), nums.end()).size();   
-    }
+     bool containsDuplicate(vector<int> &nums)
+     {
+          set<int> mark;
+          for (auto &&i : nums)
+          {
+               if (mark.find(i) == mark.end())
+                    mark.insert(i);
+               else
+                    return true;
+          }
+          return false;
+     }
 };
