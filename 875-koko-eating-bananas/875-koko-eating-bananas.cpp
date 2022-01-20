ebf6661c16ed6ce3948fa5cc8d383b0df391cdd4
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-21 00:24:48 +0530

    Time: 36 ms (95.55%), Space: 18.9 MB (16.35%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,31 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {     
-        // Initalize the left and right boundaries 
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-
-        while (left < right) {
-            // Get the middle index between left and right boundary indexes.
-            // hourSpent stands for the total hour Koko spends.
-            int middle = (left + right) / 2;
-            int hourSpent = 0;
-
-            // Iterate over the piles and calculate hourSpent.
-            // We increase the hourSpent by ceil(pile / middle).
-            for (int pile : piles) {
-                hourSpent += pile / middle + (pile % middle != 0);
-            }
-
-            // Check if middle is a workable speed, and cut the search space by half.
-            if (hourSpent <= h) {
-                right = middle;
-            } else {
-                left = middle + 1;
-            }
-        }
-
-        // Once the left and right boundaries coincide, we find the target value,
-        // that is, the minimum workable eating speed.
-        return right;
-    }
+     int minEatingSpeed(vector<int> &piles, int h)
+     {
+          int left = 1, right = 0;
+          for (auto &&banana : piles)
+          {
+               right = max(right, banana);
+          }
+          while (left < right)
+          {
+               int time = 0;
+               int speed = (left + right) / 2;
+               for (auto &&banana : piles)
+               {
+                    time += ceil(1.0 * banana / speed);
+               }
+               if (time <= h)
+                    right = speed;
+               else
+                    left = speed + 1;
+          }
+          return right;
+     }
 };
