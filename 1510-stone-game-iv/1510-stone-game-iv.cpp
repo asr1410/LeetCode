b522a3d77d895243546e1e2daf639a9f265dc4ce
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-22 14:21:16 +0530

    Time: 65 ms (55.70%), Space: 6.3 MB (75.17%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,16 +1,20 @@
-class Solution {
- public:
-  bool winnerSquareGame(int n) {
-    // dp[i] := the winning result for n = i
-    vector<bool> dp(n + 1);
-
-    for (int i = 1; i <= n; ++i)
-      for (int j = 1; j * j <= i; ++j)
-        if (!dp[i - j * j]) {  // remove j^2 stones make the opponent lose
-          dp[i] = true;        // so we win
-          break;
-        }
-
-    return dp[n];
-  }
+class Solution
+{
+public:
+     bool winnerSquareGame(int n)
+     {
+          vector<bool> dp(n + 1);
+          for (int i = 1; i < n + 1; i++)
+          {
+               for (int j = 1; j * j < i + 1; j++)
+               {
+                    if (!dp[i - j * j])
+                    {
+                         dp[i] = true;
+                         break;
+                    }
+               }
+          }
+          return dp[n];
+     }
 };
