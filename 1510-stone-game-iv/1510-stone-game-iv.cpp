ef37e64300440e0640a10a095fd651cff504dc30
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-22 06:54:27 +0530

    Time: 56 ms (59.40%), Space: 6.4 MB (75.17%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,23 +1,20 @@
 class Solution
 {
 public:
-     int dp[100001];
-     int helper(int n)
-     {
-          if (n <= 0)
-               return 0;
-          if (dp[n] != -1)
-               return dp[n];
-          for (int i = 1; i * i <= n; i++)
-          {
-               if (helper(n - i * i) == 0)
-                    return dp[n] = 1;
-          }
-          return dp[n] = 0;
-     }
      bool winnerSquareGame(int n)
+{
+     vector<bool> dp(n + 1, false);
+     for (int i = 1; i <= n; ++i)
      {
-          memset(dp, -1, sizeof(dp));
-          return helper(n);
+          for (int k = 1; k * k <= i; ++k)
+          {
+               if (!dp[i - k * k])
+               {
+                    dp[i] = true;
+                    break;
+               }
+          }
      }
+     return dp[n];
+}
 };
