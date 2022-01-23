be9f4659899bed5777c9548b453ca77f12f3eceb
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-23 11:14:43 +0530

    Time: 591 ms (26.67%), Space: 303.7 MB (46.67%) - LeetHub

---
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+     vector<int> findLonely(vector<int> &nums)
+     {
+          vector<int> mark(1000001);
+          for (auto &&num : nums)
+               mark[num]++;
+          vector<int> ans;
+          for (int i = 0; i < nums.size(); i++)
+          {
+               if (nums[i] == 0 and !(mark[nums[i] + 1] or mark[nums[i]] > 1))
+               {
+                    ans.push_back(nums[i]);
+               }
+               else if (nums[i] == 1000000 and !(mark[nums[i] - 1] or mark[nums[i]] > 1))
+               {
+                    ans.push_back(nums[i]);
+               }
+               else if ((nums[i] > 0 and nums[i] < 1000000) and !(mark[nums[i] - 1] or mark[nums[i]] > 1 or mark[nums[i] + 1]))
+               {
+                    ans.push_back(nums[i]);
+               }
+          }
+          return ans;
+     }
+};
