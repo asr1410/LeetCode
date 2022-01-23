3e73e0594c16444a371224dcfa1bbdceb99025cd
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-23 11:21:05 +0530

    Time: 714 ms (13.33%), Space: 167.3 MB (46.67%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -1,19 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-     vector<int> findLonely(vector<int> &nums)
-     {
-          vector<int> mark(1000001);
-          for (auto &&num : nums)
-               mark[num]++;
-          vector<int> ans;
-          for (int i = 0; i < nums.size(); i++)
-               if (nums[i] == 0 and !(mark[nums[i] + 1] or mark[nums[i]] > 1))
-                    ans.push_back(nums[i]);
-               else if (nums[i] == 1000000 and !(mark[nums[i] - 1] or mark[nums[i]] > 1))
-                    ans.push_back(nums[i]);
-               else if ((nums[i] > 0 and nums[i] < 1000000) and !(mark[nums[i] - 1] or mark[nums[i]] > 1 or mark[nums[i] + 1]))
-                    ans.push_back(nums[i]);
-          return ans;
-     }
+    vector<int> findLonely(vector<int>& nums) {
+        vector<int> res;
+    unordered_map<int, int> m;
+    for (int n : nums)
+        ++m[n];
+    for (const auto [n, cnt] : m)
+        if (cnt == 1 && m.count(n + 1) == 0 && m.count(n - 1) == 0)
+            res.push_back(n);
+    return res;
+}
 };
