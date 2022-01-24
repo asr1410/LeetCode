cadc3bf34d3ed25be6f11eacf19fd67b0ecb9b66
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-25 02:48:33 +0530

    Time: 78 ms (55.80%), Space: 48.1 MB (94.76%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,25 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-     bool canJump(vector<int> &nums)
-     {
-          if (nums.size() == 1)
-          {
-               return true;
-          }
-          if (nums[0] == 0)
-          {
-               return false;
-          }
-          int jump = 0;
-          for (int i = 0; i < nums.size(); i++)
-          {
-               if (nums[i] + i <= i && jump <= i)
-               {
-                    break;
-               }
-               jump = max(jump, nums[i] + i);
-          }
-          return jump >= nums.size() - 1;
-     }
+    bool canJump(vector<int>& nums) {
+        int reachable = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(reachable < i)
+                return false;
+            reachable = max(reachable, i + nums[i]);
+        }
+        return true;
+    }
 };
