dd69c16db5c35ce43177e12d17d63ad1091ceb78
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-25 00:32:56 +0530

    Time: 3 ms (39.46%), Space: 5.9 MB (90.32%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+     bool detectCapitalUse(string word)
+     {
+          int capital = 0;
+          for (auto &&alphabet : word)
+          {
+               if (alphabet >= 65 and alphabet <= 90)
+               {
+                    capital++;
+               }
+          }
+          if (capital == 0 or capital == word.size())
+          {
+               return true;
+          }
+          else if (capital == 1 and (word[0] >= 65 and word[0] <= 90))
+          {
+              cout << "work";
+               return true;
+          }
+          return false;
+     }
+};
