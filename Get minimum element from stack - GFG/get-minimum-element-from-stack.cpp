207e7ba34dd1c95947037d8f0f544f1c9b53393c
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-26 17:01:19 +0530

    Added solution - LeetHub

---
+++ Get minimum element from stack - GFG/get-minimum-element-from-stack.cpp
@@ -0,0 +1,105 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*
+The structure of the class is as follows
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+*/
+
+class Solution{
+    int minEle;
+    stack<int> s;
+    public:
+    int getMin()
+     {
+          if (s.empty())
+               return -1;
+          else
+               return minEle;
+     }
+     int pop()
+     {
+          if (s.empty())
+               return -1;
+          else
+          {
+               int x = s.top();
+               s.pop();
+               if (x < minEle)
+               {
+                    int k = minEle;
+                    minEle = 2 * minEle - x;
+                    return k;
+               }
+               else
+                    return x;
+          }
+     }
+     void push(int x)
+     {
+          if (s.empty() == true)
+          {
+               minEle = x;
+               s.push(x);
+          }
+          else
+          {
+               if (minEle > x)
+               {
+                    s.push(2 * x - minEle);
+                    minEle = x;
+               }
+               else
+                    s.push(x);
+          }
+     }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int q;
+        cin>>q;
+        Solution ob;
+        while(q--){
+           int qt;
+           cin>>qt;
+           if(qt==1)
+           {
+              //push
+              int att;
+              cin>>att;
+              ob.push(att);
+           }
+           else if(qt==2)
+           {
+              //pop
+              cout<<ob.pop()<<" ";
+           }
+           else if(qt==3)
+           {
+              //getMin
+              cout<<ob.getMin()<<" ";
+           }
+       }
+       cout<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
