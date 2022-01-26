1e55b1ffe7be8d1e989d559dea13b642143fc4b7
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-26 20:09:12 +0530

    Added solution - LeetHub

--- Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
@@ -10,12 +10,12 @@ using namespace std;
 
 class Solution
 {
-  public:
-    //Function to find maximum of each subarray of size k.
-    vector <int> max_of_subarrays(int *arr, int n, int k)
-    {
-        deque<int> dq;
-        vector<int> ans;
+public:
+     // Function to find maximum of each subarray of size k.
+     vector<int> max_of_subarrays(int *arr, int n, int k)
+     {
+          deque<int> dq;
+          vector<int> ans;
           for (int i = 0; i < n; i++)
           {
                if (!dq.empty() and dq.front() == i - k)
@@ -27,7 +27,7 @@ class Solution
                     ans.push_back(arr[dq.front()]);
           }
           return ans;
-    }
+     }
 };
 
 // { Driver Code Starts.
