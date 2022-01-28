a791aee429fa675615e03b9af6e392ce2350c057
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-01-28 05:48:57 +0530

    Time: 72 ms (86.86%), Space: 31.7 MB (94.79%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,33 @@
+class WordDictionary
+{
+public:
+     WordDictionary() {}
+
+     void addWord(string word)
+     {
+          words[word.size()].push_back(word);
+     }
+
+     bool search(string word)
+     {
+          for (auto s : words[word.size()])
+               if (isEqual(s, word))
+                    return true;
+          return false;
+     }
+
+private:
+     unordered_map<int, vector<string>> words;
+
+     bool isEqual(string a, string b)
+     {
+          for (int i = 0; i < a.size(); i++)
+          {
+               if (b[i] == '.')
+                    continue;
+               if (a[i] != b[i])
+                    return false;
+          }
+          return true;
+     }
+};
