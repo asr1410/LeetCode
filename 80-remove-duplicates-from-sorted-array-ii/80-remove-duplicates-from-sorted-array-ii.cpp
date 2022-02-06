6795a4eebd82eac036984b31ceee575266e52dac
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-06 14:40:45 +0530

    Time: 12 ms (23.05%), Space: 10.9 MB (37.19%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -2,15 +2,5 @@ class Solution
 {
 public:
     int removeDuplicates(vector<int> &nums)
-    {
-        int i = 0;
-        for (auto &&num : nums)
-        {
-            if (i < 2 or num > nums[i - 2])
-            {
-                nums[i++] = num;
-            }
-        }
-        return i;
-    }
+    {int i = 0;for (auto &&num : nums){if (i < 2 or num > nums[i - 2]){nums[i++] = num;}}return i;}
 };
