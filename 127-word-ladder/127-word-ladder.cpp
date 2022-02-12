094273bd46d149354d29fe86ac60aece08585ac2
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-12 18:11:16 +0530

    Time: 805 ms (27.93%), Space: 15.9 MB (39.50%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,45 +1,29 @@
-class Solution
-{
+class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string> &wordList)
-    {
-        unordered_set<string> myset;
-        bool isPresent = false; // Checks if endWord is present in Dict
-        // Insert all words from Dict in myset
-        for (auto word : wordList)
-        {
-            if (endWord.compare(word) == 0)
-                isPresent = true;
-            myset.insert(word); // Insert word in Dict
-        }
-        if (isPresent == false) // endWord is not present in Dict
-            return 0;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         queue<string> q;
+        set<string> st,words;
+        for(string s:wordList)
+            words.insert(s);
+        st.insert(beginWord);
         q.push(beginWord);
-        int depth = 0;
-        while (!q.empty())
-        {
-            depth += 1;
-            int lsize = q.size(); // No of elements at a level
-            while (lsize--)
-            {
-                string curr = q.front();
+        int res=0;
+        while(q.size()){
+            int n = q.size();
+            res++;
+            for(int i=0;i<n;i++){
+                string s = q.front();
+                if(s==endWord)
+                    return res;
                 q.pop();
-                // check for all possible 1 depth words
-                for (int i = 0; i < curr.length(); ++i) // For each index
-                {
-                    string temp = curr;
-                    for (char c = 'a'; c <= 'z'; ++c) // Try all possible chars
-                    {
-                        temp[i] = c;
-                        if (curr.compare(temp) == 0)
-                            continue; // Skip the same word
-                        if (temp.compare(endWord) == 0)
-                            return depth + 1; // endWord found
-                        if (myset.find(temp) != myset.end())
-                        {
-                            q.push(temp);
-                            myset.erase(temp);
+                string t;
+                for(int i=0;i<s.size();i++){
+                    t=s;
+                    for(int j='a';j<='z';j++){
+                        t[i]=j;
+                        if(words.count(t) and !st.count(t)){
+                            q.push(t);
+                            st.insert(t);
                         }
                     }
                 }
