54987449caa4ce3f9ae6312af60b70246ed6f5f8
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-13 20:55:40 +0530

    Time: 329 ms (93.33%), Space: 107.3 MB (100.00%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -5,16 +5,10 @@ public:
     {
         int even[100001][1] = {}, odd[1000001][1] = {}, maxeven = 0, maxevenindex = 0, secondmaxeven = 0, maxodd = 0, maxoddindex = 0, secondmaxodd = 0;
         for (int i = 0; i < nums.size(); i++)
-        {
             if (i % 2 == 0)
-            {
                 even[nums[i]][0]++;
-            }
             else
-            {
                 odd[nums[i]][0]++;
-            }
-        }
         for (int i = 0; i < 100001; i++)
         {
             if (even[i][0] > maxeven)
