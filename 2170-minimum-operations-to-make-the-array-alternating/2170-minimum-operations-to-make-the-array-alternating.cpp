21bb8580c8c63e61d5f1106005bf32d1a41a8406
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-13 20:53:38 +0530

    Time: 298 ms (100.00%), Space: 107.4 MB (100.00%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -3,15 +3,31 @@ class Solution
 public:
     int minimumOperations(vector<int> &nums)
     {
-        int freq[100001][2] = {}, j, k, res;
+        int even[100001][1] = {}, odd[1000001][1] = {}, maxeven = 0, maxevenindex = 0, secondmaxeven = 0, maxodd = 0, maxoddindex = 0, secondmaxodd = 0;
         for (int i = 0; i < nums.size(); i++)
-            freq[nums[i]][1 & i]++;
-        for (int i = 1, j = k = res = 0; i < 100001; i++)
         {
-            res = max(res, max(freq[i][0] + k, freq[i][1] + j));
-            j = max(j, freq[i][0]);
-            k = max(k, freq[i][1]);
+            if (i % 2 == 0)
+            {
+                even[nums[i]][0]++;
+            }
+            else
+            {
+                odd[nums[i]][0]++;
+            }
         }
-        return nums.size() - res;
+        for (int i = 0; i < 100001; i++)
+        {
+            if (even[i][0] > maxeven)
+                secondmaxeven = maxeven, maxeven = even[i][0], maxevenindex = i;
+            else if (even[i][0] > secondmaxeven)
+                secondmaxeven = even[i][0];
+            if (odd[i][0] > maxodd)
+                secondmaxodd = maxodd, maxodd = odd[i][0], maxoddindex = i;
+            else if (odd[i][0] > secondmaxodd)
+                secondmaxodd = odd[i][0];
+        }
+        if (maxevenindex != maxoddindex)
+            return nums.size() - maxeven - maxodd;
+        return min(nums.size() - maxeven - secondmaxodd, nums.size() - maxodd - secondmaxeven);
     }
 };
