811b1130db23a49d569114e1ad21975ee249a1ad
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-13 17:48:05 +0530

    Time: 320 ms (100.00%), Space: 103.4 MB (100.00%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -3,12 +3,15 @@ class Solution
 public:
     int minimumOperations(vector<int> &nums)
     {
-        int freq[100005][2] = {};
-        int i, j, k, ans = 0;
-        for (i = 0; i < nums.size(); i++)
-            freq[nums[i]][i & 1]++;
-        for (i = 1, j = k = 0; i <= 100000; i++)
-            ans = max(ans, max(freq[i][0] + k, freq[i][1] + j)), j = max(j, freq[i][0]), k = max(k, freq[i][1]);
-        return nums.size() - ans;
+        int freq[100001][2] = {}, j, k, res;
+        for (int i = 0; i < nums.size(); i++)
+            freq[nums[i]][1 & i]++;
+        for (int i = 1, j = k = res = 0; i < 100001; i++)
+        {
+            res = max(res, max(freq[i][0] + k, freq[i][1] + j));
+            j = max(j, freq[i][0]);
+            k = max(k, freq[i][1]);
+        }
+        return nums.size() - res;
     }
 };
