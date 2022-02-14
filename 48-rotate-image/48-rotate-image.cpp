07568db86400ddf034bb9ebceb36e5c9baa20f49
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-14 18:29:10 +0530

    Time: 3 ms (63.71%), Space: 7.2 MB (33.93%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,16 +4,16 @@ public:
     void rotate(vector<vector<int>> &matrix)
     {
         int n = matrix.size();
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < (n + 1) / 2; i++)
         {
-            for (int j = 0; j < i; j++)
+            for (int j = 0; j < n / 2; j++)
             {
-                swap(matrix[i][j], matrix[j][i]);
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
             }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
