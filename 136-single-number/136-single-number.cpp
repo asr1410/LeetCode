4f07529a31e1e1fd1606621e8534dc79c06662be
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-15 15:46:53 +0530

    Time: 25 ms (57.97%), Space: 17 MB (50.83%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -3,6 +3,6 @@ class Solution
 public:
     int singleNumber(vector<int> &nums)
     {
-        return accumulate(nums.cbegin(), nums.cend(), 0, bit_xor<int>());
+        return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
     }
 };
