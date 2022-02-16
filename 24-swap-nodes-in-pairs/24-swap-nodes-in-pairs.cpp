7126a0bfbf97830b705440e52ac75bdf95a6eee5
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-16 17:47:12 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (7.38%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -1,33 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+class Solution
+{
 public:
-    ListNode* swapPairs(ListNode* head) {
-        if(head == NULL or head->next == NULL)
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (head == NULL or head->next == NULL)
             return head;
-        ListNode* curr = head;
-        ListNode* nex = head->next;
-        while(nex and nex->next)
+        ListNode *curr = head, *next = head->next;
+        while (next and next->next)
         {
             int temp = curr->val;
-            curr->val = nex->val;
-            nex->val = temp;
+            curr->val = next->val;
+            next->val = temp;
             curr = curr->next->next;
-            nex = nex->next->next;
+            next = next->next->next;
         }
-        if(curr and nex)
+        if (curr and next)
         {
             int temp = curr->val;
-            curr->val = nex->val;
-            nex->val = temp;
+            curr->val = next->val;
+            next->val = temp;
         }
         return head;
     }
