bf5949f2a803a82de35d01c0aca3022a50ab11e7
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-16 17:51:08 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (7.38%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -1,19 +1,14 @@
-class Solution
-{
+class Solution {
 public:
-    ListNode *swapPairs(ListNode *head)
-    {
-        if (!head or !head->next)
-            return head;
+    ListNode *swapPairs(ListNode *head) {
+        if (!head or !head->next) return head;
         ListNode *curr = head, *next = head->next;
         while (next and next->next)
         {
             swap(curr->val, next->val);
-            curr = curr->next->next;
-            next = next->next->next;
+            curr = curr->next->next, next = next->next->next;
         }
-        if (curr and next)
-            swap(curr->val, next->val);
+        if (curr and next) swap(curr->val, next->val);
         return head;
     }
 };
