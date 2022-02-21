92f5178c87dfbec922dd31943705f06be594b030
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-22 02:38:10 +0530

    Time: 37 ms (24.01%), Space: 19.7 MB (9.45%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -6,12 +6,10 @@ public:
         int mx = nums[0];
         int count = 1;
         for (int i = 1; i < nums.size(); i++)
-        {
             if (nums[i] == mx) count++;
             else
                 if(count > 1) count--;
                 else mx = nums[i], count = 1;
-        }
         return mx;
     }
 };
