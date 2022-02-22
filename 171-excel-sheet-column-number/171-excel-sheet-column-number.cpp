753113f3156fb9bd6aeed6ce51081fa8827501d0
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-22 20:14:15 +0530

    Time: 0 ms (100.00%), Space: 6 MB (67.10%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,16 +1,11 @@
 class Solution
 {
-private:
-    long helper(string &s, int idx)
-    {
-        if (!idx)
-            return s[idx] - 'A' + 1;
-        return 26 * helper(s, idx - 1) + s[idx] - 'A' + 1;
-    }
-
 public:
     int titleToNumber(string columnTitle)
     {
-        return helper(columnTitle, columnTitle.size() - 1);
+        long result = 0;
+        for (auto alpha : columnTitle)
+            result = result * 26 + alpha - 'A' + 1;
+        return result;
     }
 };
