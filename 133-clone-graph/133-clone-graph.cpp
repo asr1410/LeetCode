905a55dcadb4473f6fb5c08f4155531e9304007e
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-23 20:00:24 +0530

    Time: 8 ms (65.22%), Space: 8.9 MB (23.69%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -1,18 +1,21 @@
+
+
 class Solution
 {
-private:
     unordered_map<Node *, Node *> mp;
 
 public:
     Node *cloneGraph(Node *node)
     {
-        if (node == NULL)
+        if (!node)
             return NULL;
         if (mp.find(node) == mp.end())
         {
             mp[node] = new Node(node->val);
-            for (auto adj : node->neighbors)
+            for (auto &&adj : node->neighbors)
+            {
                 mp[node]->neighbors.push_back(cloneGraph(adj));
+            }
         }
         return mp[node];
     }
