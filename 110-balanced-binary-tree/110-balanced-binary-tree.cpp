395e902318e7b2f33bd2a69ee9600c4375a8d623
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-24 17:59:26 +0530

    Time: 16 ms (60.79%), Space: 21 MB (23.38%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -5,14 +5,18 @@ public:
     {
         if (!root)
             return 0;
-        return max(depth(root->left), depth(root->right)) + 1;
+        int lh = depth(root->left);
+        if (lh == -1)
+            return -1;
+        int rh = depth(root->right);
+        if (rh == -1)
+            return -1;
+        if (abs(lh - rh) > 1)
+            return -1;
+        return max(lh, rh) + 1;
     }
     bool isBalanced(TreeNode *root)
     {
-        if (!root)
-            return true;
-        int lh = depth(root->left);
-        int rh = depth(root->right);
-        return abs(lh - rh) <= 1 and isBalanced(root->left) and isBalanced(root->right);
+        return depth(root) != -1;
     }
 };
