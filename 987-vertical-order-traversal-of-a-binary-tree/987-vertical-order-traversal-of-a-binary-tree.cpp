41e564f1134c70626ff9c80ef05b92966eb9b55f
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-28 12:27:09 +0530

    Time: 4 ms (81.93%), Space: 12.6 MB (71.61%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,28 +1,36 @@
 class Solution
 {
-    map<int, map<int, multiset<int>>> mymap;
-    void solve(TreeNode *curr, int col, int row)
-    {
-        if (!curr)
-            return;
-        mymap[col][row].insert(curr->val);
-        solve(curr->left, col - 1, row + 1);
-        solve(curr->right, col + 1, row + 1);
-    }
-
 public:
     vector<vector<int>> verticalTraversal(TreeNode *root)
     {
-        solve(root, 0, 0);
+        TreeNode *curr;
+        queue<pair<TreeNode *, int>> q;
+        q.push({root, 0});
         vector<vector<int>> ans;
-        for (auto m1 : mymap)
+        map<int, vector<int>> mymap;
+
+        // BFS
+        while (!q.empty())
         {
-            vector<int>temp;
-            for (auto m2 : m1.second)
-                for (auto m3 : m2.second)
-                    temp.push_back(m3);
-            ans.push_back(temp);
+            int size = q.size();
+            map<int, multiset<int>> mapset;
+            while (size--)
+            {
+                curr = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                mapset[col].insert(curr->val);
+                if (curr->left)
+                    q.push({curr->left, col - 1});
+                if (curr->right)
+                    q.push({curr->right, col + 1});
+            }
+            for (auto it : mapset)
+                for (auto it2 : it.second)
+                    mymap[it.first].push_back(it2);
         }
+        for (auto it : mymap)
+            ans.push_back(it.second);
         return ans;
     }
 };
