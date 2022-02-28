ebc8fce296a92ce54bbffed52d3975adf08be5df
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-02-28 17:00:48 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (62.02%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -7,16 +7,13 @@ public:
         auto len = nums.size();
         if (!len)
             return ans;
-        auto first = nums[0], second = nums[0];
+        int f = nums[0], s = nums[0];
         for (int i = 1; i < len; i++)
-            if (second + 1 == nums[i])
-                second = nums[i];
+            if (s + 1 == nums[i])
+                s = nums[i];
             else
-            {
-                first == second ? ans.push_back(to_string(first)) : ans.push_back(to_string(first) + "->" + to_string(second));
-                first = second = nums[i];
-            }
-        first == second ? ans.push_back(to_string(first)) : ans.push_back(to_string(first) + "->" + to_string(second));
+                f == s ? ans.push_back(to_string(f)) : ans.push_back(to_string(f) + "->" + to_string(s)), f = s = nums[i];
+        f == s ? ans.push_back(to_string(f)) : ans.push_back(to_string(f) + "->" + to_string(s));
         return ans;
     }
 };
