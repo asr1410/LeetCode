a6439509ae7d1e3cd0b3d526ae2fcdd743651c7d
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-04 18:57:21 +0530

    Time: 9 ms (67.09%), Space: 6.3 MB (68.10%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,15 +1,11 @@
-class Solution {
-public:
-    double champagneTower(int poured, int row, int glass)
+class Solution
 {
-    vector<double> top{(double)poured};
-    for (int i = 0; i < row; i++)
-    {
-        vector<double> bottom(i + 2);
-        for (int j = 0; j <= i; j++)
-            bottom[j] += max(top[j] - 1.0, 0.0) / 2.0, bottom[j + 1] += max(top[j] - 1.0, 0.0) / 2.0;
-        top = bottom;
-    }
-    return min(top[glass], 1.0);
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+    vector<double> dp(101, 0); dp[0] = poured;
+    for(int row=1; row<=query_row; row++)
+        for(int i=row; i>=0; i--)
+            dp[i+1] += dp[i] = max(0.0, (dp[i]-1)/2);
+    return min(dp[query_glass], 1.0);
 }
 };
