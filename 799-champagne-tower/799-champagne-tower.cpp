b0ea86ee24ccf7bfef8f98e3d364cf03167c5484
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-04 19:13:25 +0530

    Time: 21 ms (40.51%), Space: 14.1 MB (54.18%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -3,12 +3,16 @@ class Solution
 public:
     double champagneTower(int poured, int row, int glass)
     {
-        vector<double> top{(double)poured};
+        vector<double> top(1);
+        top[0] = poured;
         for (int i = 0; i < row; i++)
         {
             vector<double> bottom(i + 2);
             for (int j = 0; j <= i; j++)
-                bottom[j] += max(top[j] - 1.0, 0.0) / 2.0, bottom[j + 1] += max(top[j] - 1.0, 0.0) / 2.0;
+            {
+                bottom[j] += max((top[j] - 1) / 2, 0.0);
+                bottom[j + 1] += max((top[j] - 1) / 2, 0.0);
+            }
             top = bottom;
         }
         return min(top[glass], 1.0);
