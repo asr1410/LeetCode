29fcd836bff72a17ae97f0e4971a3c5f61f9d454
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-05 22:28:25 +0530

    Time: 13 ms (45.83%), Space: 9.3 MB (77.38%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int deleteAndEarn(vector<int> &nums)
+    {
+        auto mx = max_element(nums.begin(), nums.end());
+        int len = *mx + 1, ans = 0;
+        vector<int> mark(len);
+        for (auto &&num : nums)
+            mark[num] += num;
+        int take = 0, skip = 0;
+        for (int i = 0; i < len; i++)
+        {
+            int takei = skip + mark[i];
+            int skipi = max(skip, take);
+            take = takei;
+            skip = skipi;
+        }
+        return max(take, skip);
+    }
+};
