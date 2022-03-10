50b6e76792d164ea8a564429ffec50817ca06e52
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-10 18:19:34 +0530

    Time: 36 ms (77.58%), Space: 71 MB (94.13%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -15,14 +15,14 @@ public:
         }
         else if (l1 and l2)
         {
-            l1->val = l1->val + l2->val + carry;
+            l1->val += l2->val + carry;
             carry = l1->val / 10;
             l1->val = l1->val % 10;
             l1->next = addTwoNumbers(l1->next, l2->next);
         }
         else if (l1)
         {
-            l1->val = l1->val + carry;
+            l1->val += carry;
             carry = l1->val / 10;
             l1->val = l1->val % 10;
             l1->next = addTwoNumbers(l1->next, nullptr);
@@ -30,7 +30,7 @@ public:
         }
         else if (l2)
         {
-            l2->val = l2->val + carry;
+            l2->val += carry;
             carry = l2->val / 10;
             l2->val = l2->val % 10;
             l2->next = addTwoNumbers(nullptr, l2->next);
