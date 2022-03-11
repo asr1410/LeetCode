b0bb2d2e2c00e74df415dc19761c749110ce4974
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-11 18:35:22 +0530

    Time: 15 ms (29.30%), Space: 11.8 MB (55.87%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    ListNode *rotateRight(ListNode *head, int k)
+    {
+        if (!k or !head)
+            return head;
+        int size = 0;
+        auto curr = head;
+        while (curr)
+        {
+            size++;
+            curr = curr->next;
+        }
+        k = k % size;
+        if (!k)
+            return head;
+        k = size - k;
+        curr = head;
+        while (curr->next)
+        {
+            curr = curr->next;
+        }
+        curr->next = head;
+        curr = head;
+        while (k--)
+        {
+            if (k == 0)
+            {
+                head = curr->next;
+                curr->next = nullptr;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
