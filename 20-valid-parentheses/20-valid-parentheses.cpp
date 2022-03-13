223542452dcdc7a9bba77154e817851b52a80f10
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-13 09:21:29 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,13 +1,14 @@
-class Solution {
+class Solution
+{
 public:
     bool isValid(string s)
-{
-     stack<char> tube;
-     for (int i = 0; i < s.length(); i++)
-          if (!tube.empty() and ((tube.top() == '(' and s[i] == ')') || (tube.top() == '[' and s[i] == ']') || (tube.top() == '{' and s[i] == '}')))
-               tube.pop();
-          else
-               tube.push(s[i]);
-     return tube.empty();
-}
+    {
+        stack<char> tube;
+        for (int i = 0; i < s.length(); i++)
+            if (!tube.empty() and ((tube.top() == '(' and s[i] == ')') || (tube.top() == '[' and s[i] == ']') || (tube.top() == '{' and s[i] == '}')))
+                tube.pop();
+            else
+                tube.push(s[i]);
+        return tube.empty();
+    }
 };
