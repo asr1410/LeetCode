a6c757437c44d7aa26ac2ec04108c615f13f17e1
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-18 23:09:58 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (81.09%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,18 +1,20 @@
-
-class Solution {
+class Solution
+{
 public:
-    string removeDuplicateLetters(string s) {
-        string res="";
-        int lastPos[26] = {}; // the last position of the char
-        bool added[26] = {};  // if the char is added to the result
+    string removeDuplicateLetters(string s)
+    {
+        string res = "";
+        vector<int> lastPos(26);
+        vector<bool> added(26);
         for (int i = 0; i < s.size(); i++)
         {
             lastPos[s[i] - 'a'] = i;
         }
         for (int i = 0; i < s.size(); i++)
         {
-            if (added[s[i] - 'a']) continue;
-            while ( !res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i)
+            if (added[s[i] - 'a'])
+                continue;
+            while (!res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i)
             {
                 added[res.back() - 'a'] = false;
                 res.pop_back();
