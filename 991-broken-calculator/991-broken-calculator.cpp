47e7e2d4679e7618a4744ac27ed6e80cdc06fa28
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-23 13:45:46 +0530

    Time: 26 ms (7.55%), Space: 5.9 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -3,6 +3,8 @@ class Solution
 public:
     int brokenCalc(int startValue, int target)
     {
+        if (startValue > target)
+            return startValue - target;
         int count = 0;
         while (startValue != target)
         {
