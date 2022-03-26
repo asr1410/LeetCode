b14bac7881754a945840bcd99365107ed4c92762
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-26 19:30:13 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (78.35%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,10 +1,11 @@
 class Solution
 {
-public:
     static bool comparator(vector<int> &a, vector<int> &b)
     {
         return a[1] - a[0] > b[1] - b[0];
     }
+
+public:
     int twoCitySchedCost(vector<vector<int>> &costs)
     {
         sort(costs.begin(), costs.end(), comparator);
