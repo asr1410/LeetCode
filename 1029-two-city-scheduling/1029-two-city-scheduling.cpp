168c70eb4e308c59c41e5734cb8b569c6e4975e8
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-26 19:39:05 +0530

    Time: 11 ms (28.08%), Space: 7.8 MB (78.35%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -8,7 +8,9 @@ class Solution
 public:
     int twoCitySchedCost(vector<vector<int>> &costs)
     {
-        sort(costs.begin(), costs.end(), comparator);
+        sort(costs.begin(), costs.end(), [](const auto &a, const auto &b){
+            return a[1] - a[0] > b[1] - b[0];
+        });
         int n = costs.size(), ans = 0;
         for (int i = 0; i < n; i++)
         {
