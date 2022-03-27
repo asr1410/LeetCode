158a3858961e1f514ccc684fec2141acdcbafae5
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-27 07:46:28 +0530

    Time: 13 ms (69.54%), Space: 10.5 MB (75.81%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,17 +1,14 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
-{
-    set<pair<int, int>> s;
-    for (auto row = 0; row < mat.size(); row++)
-    {
-        auto count = accumulate(mat[row].begin(), mat[row].end(), 0);
-        s.insert(make_pair(count, row));
+    vector<int> kWeakestRows(vector<vector<int>> &mat, int k) {
+        set<pair<int, int>> s;
+        for (auto row = 0; row < mat.size(); row++) {
+            auto count = accumulate(mat[row].begin(), mat[row].end(), 0);
+            s.insert(make_pair(count, row));
+        }
+        vector<int> res;
+        for (auto it = s.begin(); it != s.end(), k > 0; k--, it++)
+            res.push_back(it->second);
+        return res;
     }
-    vector<int> res;
-    for (auto it = s.begin(); it != s.end(), k > 0; k--, it++)
-        res.push_back(it->second);
-    return res;
-}
 };
