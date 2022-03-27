06fe7ec02d245c7a9048008aa1e9a1a693160359
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-27 10:37:17 +0530

    Time: 254 ms (14.29%), Space: 121.1 MB (57.14%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -1,18 +1,10 @@
-class Solution
-{
+class Solution {
 public:
-    int minDeletion(vector<int> &nums)
-    {
+    int minDeletion(vector<int> &nums) {
         int ans = 0;
         for (int i = 0; i < nums.size() - 1; i++)
-        {
-            if (nums[i] == nums[i + 1] and (i - ans) % 2 == 0)
-            {
-                ans++;
-            }
-        }
-        if((nums.size() - ans) % 2 != 0)
-                ans++;
+            if (nums[i] == nums[i + 1] and (i - ans) % 2 == 0) ans++;
+        if ((nums.size() - ans) % 2 != 0) return ++ans;
         return ans;
     }
 };
