853261693b666b0880edbd99d131d207e78fefd4
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-03-27 10:56:00 +0530

    Time: 324 ms (14.29%), Space: 121.1 MB (57.14%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -4,7 +4,6 @@ public:
         int ans = 0;
         for (int i = 0; i < nums.size() - 1; i++)
             if (nums[i] == nums[i + 1] and (i - ans) % 2 == 0) ans++;
-        if ((nums.size() - ans) % 2 != 0) return ++ans;
-        return ans;
+        return ans + (nums.size() - ans) % 2;
     }
 };
