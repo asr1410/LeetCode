25cc2d9fc0b8ed3f470e510c0a261c8732a8a4bc
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-04-05 00:34:26 +0530

    Time: 890 ms (49.51%), Space: 184.9 MB (10.66%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,25 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* swapNodes(ListNode* head, int k) {
-    ListNode *n1 = nullptr, *n2 = nullptr;
-    for (auto p = head; p != nullptr; p = p->next) {
-        n2 = n2 == nullptr ? nullptr : n2->next;
-        if (--k == 0) {
-            n1 = p;
-            n2 = head;
-        }
+class Solution
+{
+    int size = 0, count = 0;
+    ListNode *first = nullptr;
+    ListNode *second = nullptr;
+    void recursion(ListNode *node, int k)
+    {
+        if (!node)
+            return;
+        if (++count == k)
+            first = node;
+        recursion(node->next, k);
+        if (++size == k)
+            second = node;
     }
-    swap(n1->val, n2->val);
-    return head;
-}
+
+public:
+    ListNode *swapNodes(ListNode *head, int k)
+    {
+        recursion(head, k);
+        swap(first->val, second->val);
+        return head;
+    }
 };
