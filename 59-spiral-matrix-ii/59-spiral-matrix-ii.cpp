79db2f4e6bc86888902594f46daa6bbb9b9065aa
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-04-13 23:27:54 +0530

    Time: 4 ms (19.27%), Space: 6.6 MB (63.66%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,36 +1,33 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>m(n,vector<int>(n,0));
-        int c=1;
-        int left=0,right=n-1,top=0,bottom=n-1;
-        while(left<=right && top<=bottom){
-            
-                for(int i=left;i<=right;i++){
-                    m[top][i]=c;
-                    c++;
-                }
-                top++;
-            
-                for(int i=top;i<=bottom;i++){
-                   m[i][right]=c;
-                    c++; 
-                }
-                right--;
-            
-                for(int i=right;i>=left;i--){
-                    m[bottom][i]=c;
-                    c++;
-                }
-                bottom--;
-            
-             
-                for(int i=bottom;i>=top;i--){
-                   m[i][left]=c;
-                    c++; 
-                }
-                left++;
-            
+    vector<vector<int>> generateMatrix(int n)
+    {
+        vector<vector<int>> m(n, vector<int>(n));
+
+        int i = 0, j = 0, c = 1;
+        while (c <= n * n)
+        {
+            while (j < n and !m[i][j])
+            {
+                m[i][j++] = c++;
+            }
+            j--, i++;
+            while (i < n and !m[i][j])
+            {
+                m[i++][j] = c++;
+            }
+            i--, j--;
+            while (j >= 0 and !m[i][j])
+            {
+                m[i][j--] = c++;
+            }
+            i--, j++;
+            while (i >= 0 and !m[i][j])
+            {
+                m[i--][j] = c++;
+            }
+            i++, j++;
         }
         return m;
     }
