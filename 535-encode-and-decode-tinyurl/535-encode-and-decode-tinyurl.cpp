bdee597ac8229d3c0ead3fdceac9c39b0ede4a35
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-04-23 22:24:35 +0530

    Time: 4 ms (70.07%), Space: 6.5 MB (87.04%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,22 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    unordered_map<string, string> m;
-    int num = 0;
+
     // Encodes a URL to a shortened URL.
-    string encode(string longUrl)
-    {
-        num++;
-        string addon = to_string(num);
-        string ans = "http://tinyurl.com/";
-        ans += (string)addon;
-        m[ans] = longUrl;
-        return ans;
+    string encode(string longUrl) {
+        return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
-    string decode(string shortUrl)
-    {
-        return m[shortUrl];
+    string decode(string shortUrl) {
+        return shortUrl;
     }
 };
