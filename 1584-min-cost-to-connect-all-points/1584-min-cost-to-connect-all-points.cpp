4a5d97611f899c19b5f17c9e732c795a1ce8985c
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-04-27 03:08:49 +0530

    Time: 98 ms (96.22%), Space: 10.1 MB (98.82%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,43 +1,19 @@
-class UnionFind {
-    vector<int> id;
-    int size;
-public:
-    UnionFind(int N) : id(N), size(N) {
-        iota(begin(id), end(id), 0);
-    }
-    int find(int a) {
-        return id[a] == a ? a : (id[a] = find(id[a]));
-    }
-    void connect(int a, int b) {
-        int p = find(a), q = find(b);
-        if (p == q) return;
-        id[p] = q;
-        --size;
-    }
-    bool connected(int a, int b) {
-        return find(a) == find(b);
-    }
-    int getSize() { return size; }
-};
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& A) {
-        int N = A.size(), ans = 0;
-        vector<vector<int>> E;
-        for (int i = 0; i < N; ++i) {
-            for (int j = i + 1; j < N; ++j) E.push_back({ abs(A[i][0] - A[j][0]) + abs(A[i][1] - A[j][1]), i, j });
-        }
-        make_heap(begin(E), end(E), greater<vector<int>>());
-        UnionFind uf(N);
-        while (uf.getSize() > 1) {
-            pop_heap(begin(E), end(E), greater<vector<int>>());
-            auto e = E.back();
-            int w = e[0], u = e[1], v = e[2];
-            E.pop_back();
-            if (uf.connected(u, v)) continue;
-            uf.connect(u, v);
-            ans += w;
-        } 
-        return ans;
+    int minCostConnectPoints(vector<vector<int>>& ps) {
+    int n = ps.size(), res = 0, i = 0, connected = 0;
+    vector<int> min_d(n, 10000000);
+    while (++connected < n) {
+        min_d[i] = INT_MAX;
+        int min_j = i;
+        for (int j = 0; j < n; ++j)
+            if (min_d[j] != INT_MAX) {
+                min_d[j] = min(min_d[j], abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1]));
+                min_j = min_d[j] < min_d[min_j] ? j : min_j;
+            }
+        res += min_d[min_j];
+        i = min_j;
     }
+    return res;
+}
 };
