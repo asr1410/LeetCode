6f9c3d7d5fc196390d23fa02a7a5f358a2a201d3
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-05-11 13:06:31 +0530

    Time: 4 ms (35.46%), Space: 6.1 MB (25.34%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -6,9 +6,9 @@ public:
         vector<int> v(5, 1);
         for (int i = 2; i <= n; i++)
         {
-            for (int j = 3; j >= 0; j--)
+            for (int i = 3; i >= 0; i--)
             {
-                v[j] += v[j + 1];
+                v[i] += v[i + 1];
             }
         }
         return accumulate(v.begin(), v.end(), 0);
