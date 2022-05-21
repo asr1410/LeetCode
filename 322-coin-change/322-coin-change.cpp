088d967c0b1a9a0277bad6629acfe2819db3a7cf
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-05-21 15:51:20 +0530

    Time: 33 ms (98.62%), Space: 10 MB (97.58%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    int dp[10000 + 1][12 + 1];
-    
-    int tabulation(vector<int> wt, int w, int n)
-    {
-        for (int i = 0; i < w + 1; i++)
-            for (int j = 0; j < n + 1; j++)
-                if (i == 0 || j == 0)
-                    dp[i][j] = (i == 0) ? 0 : INT_MAX - 1;
-        
-        for (int i = 1; i < w + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if (wt[j - 1] > i) 
-                    dp[i][j] = 0 + dp[i - 0][j - 1];
-                else 
-                    dp[i][j] = min(0 + dp[i - 0][j - 1], 1 + dp[i - wt[j - 1]][j - 0]);
+    int coinChange(vector<int>& coins, int n) {
+        // creating the base dp array, with first value set to 0
+        int dp[++n];
+        dp[0] = 0;
+        // more convenient to have the coins sorted
+        sort(begin(coins), end(coins));
+        // populating our dp array
+        for (int i = 1; i < n; i++) {
+            // setting dp[0] base value to 1, 0 for all the rest
+            dp[i] = INT_MAX;
+            for (int c: coins) {
+                if (i - c < 0) break;
+                // if it was a previously not reached cell, we do not add use it
+                if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
             }
         }
-        
-        return dp[w][n];
-    }
-    
-    int coinChange(vector<int>& coins, int amount) 
-    {
-        memset(dp, -1, sizeof(dp));
-        int minCoins = tabulation(coins, amount, coins.size());
-        return minCoins == INT_MAX - 1 ? -1 : minCoins;    
+        return dp[--n] == INT_MAX ? -1 : dp[n];
     }
 };
