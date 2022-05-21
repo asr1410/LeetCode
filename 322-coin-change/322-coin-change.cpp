b84e9e195cdf9f365675be2a8bf92be9cd8ba031
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-05-21 15:50:35 +0530

    Time: 101 ms (64.61%), Space: 10.4 MB (88.53%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int dp[10000 + 1][12 + 1];  // New Line Added
+    int dp[10000 + 1][12 + 1];
     
-    int memoization(vector<int>& wt, int w, int n)
+    int tabulation(vector<int> wt, int w, int n)
     {
-        if (n == 0 || w == 0)
-            return (w == 0) ? 0 : INT_MAX - 1;
+        for (int i = 0; i < w + 1; i++)
+            for (int j = 0; j < n + 1; j++)
+                if (i == 0 || j == 0)
+                    dp[i][j] = (i == 0) ? 0 : INT_MAX - 1;
         
-        if (dp[w][n] != -1) // New Line Added
-            return dp[w][n];  // New Line Added
-			
-        if (wt[n - 1] > w) 
-            return dp[w][n] = 0 + memoization(wt, w - 0, n - 1);
-        else 
-            return dp[w][n] = min(0 + memoization(wt, w - 0, n - 1), 1 + memoization(wt, w - wt[n - 1], n));
+        for (int i = 1; i < w + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (wt[j - 1] > i) 
+                    dp[i][j] = 0 + dp[i - 0][j - 1];
+                else 
+                    dp[i][j] = min(0 + dp[i - 0][j - 1], 1 + dp[i - wt[j - 1]][j - 0]);
+            }
+        }
+        
+        return dp[w][n];
     }
     
     int coinChange(vector<int>& coins, int amount) 
     {
-        memset(dp, -1, sizeof(dp)); // New Line Added
-        int minCoins = memoization(coins, amount, coins.size());
+        memset(dp, -1, sizeof(dp));
+        int minCoins = tabulation(coins, amount, coins.size());
         return minCoins == INT_MAX - 1 ? -1 : minCoins;    
     }
 };
