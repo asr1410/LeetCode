228ef9e42601609cc95986ded12adf0d879ab436
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-06-12 00:19:20 +0530

    Time: 193 ms (84.51%), Space: 98.4 MB (90.95%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,26 +4,18 @@ public:
     int minOperations(vector<int> &nums, int x)
     {
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum < x)
-            return -1;
-        int remain = sum - x;
-        int len = 0, left = 0;
-        int summ = 0;
-        bool check = false;
-        for (int i = 0; i < nums.size(); i++)
+        if (sum < x) return -1;
+        if(sum == x) return nums.size();
+        int remain = sum - x, len = 0, left = 0, summ = 0;
+        for (int right = 0; right < nums.size(); right++)
         {
-            summ += nums[i];
+            summ += nums[right];
             while (summ > remain)
-            {
                 summ -= nums[left++];
-            }
             if (summ == remain)
-            {
-                len = max(len, i - left + 1);
-                check = true;
-            }
+                len = max(len, right - left + 1);
         }
-        if (check)
+        if (len)
             return nums.size() - len;
         return -1;
     }
