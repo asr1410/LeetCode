25e1cc4020f743a01d92d33751187ecb7eb8f48e
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-06-13 00:09:57 +0530

    Time: 188 ms (97.93%), Space: 89 MB (100.00%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -3,28 +3,15 @@ class Solution
 public:
     int maximumUniqueSubarray(vector<int> &nums)
     {
-        int score = 0;
-        int left = 0;
-        int temp = 0;
-        unordered_map<int, pair<bool, int>> mark;
-        for (int right = 0; right < nums.size(); right++)
+        int n = size(nums), cur_sum = 0, ans = 0, l = 0, r = 0;
+        bool freq[10001]{false};
+        while (r < n)
         {
-            if (mark.find(nums[right]) == mark.end())
-            {
-                mark.emplace(nums[right], make_pair(true, right));
-                temp += nums[right];
-            }
-            else
-            {
-                while (left <= mark[nums[right]].second)
-                {
-                    temp -= nums[left++];
-                }
-                mark[nums[right]].second = right;
-                temp += nums[right];
-            }
-            score = max(score, temp);
+            while (freq[nums[r]])
+                cur_sum -= nums[l], freq[nums[l++]] = false;
+            cur_sum += nums[r], freq[nums[r++]] = true;
+            ans = max(ans, cur_sum);
         }
-        return score;
+        return ans;
     }
 };
