0ac6689db35ac9201b37a55e22c2865a0d2d2bf6
ABHISHEK SINGH RAWAT <40198410+asr1410@users.noreply.github.com>
2022-07-16 00:16:08 +0530

    Time: 26 ms (68.48%), Space: 23.3 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,32 +1,31 @@
-class Solution {
-public:
-    void recursive(vector<vector<int>> &grid, int &area, int i, int j)
+class Solution
 {
-    if (i >= grid.size() || j >= grid[0].size() || i < 0 || j < 0 || grid[i][j] != 1)
+public:
+    void helper(int row, int col, vector<vector<int>> &grid, int &temp)
     {
-        return;
-    }
-    grid[i][j] = 0;
-    area += 1;
-    recursive(grid, area, i, j+1);
-    recursive(grid, area, i + 1, j);
-    recursive(grid, area, i, j - 1);
-    recursive(grid, area, i - 1, j);
-}
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxarea = INT_MIN;
-    for (int i = 0; i < grid.size(); i++)
-    {
-        for (int j = 0; j < grid[i].size(); j++)
+        if (row >= grid.size() || col >= grid[0].size() || row < 0 || col < 0 || grid[row][col] != 1)
         {
-            int area = 0;
-            if (grid[i][j] == 1)
-            {
-                recursive(grid, area, i, j);
-            }
-            maxarea = max(area, maxarea);
+            return;
         }
+        grid[row][col] = 0;
+        temp++;
+        helper(row, col - 1, grid, temp);
+        helper(row, col + 1, grid, temp);
+        helper(row - 1, col, grid, temp);
+        helper(row + 1, col, grid, temp);
     }
-    return maxarea;
+    int maxAreaOfIsland(vector<vector<int>> &grid)
+    {
+        int maxarea = INT_MIN;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[i].size(); j++)
+            {
+                int temp = 0;
+                helper(i, j, grid, temp);
+                maxarea = max(maxarea, temp);
+            }
+        }
+        return maxarea;
     }
 };
