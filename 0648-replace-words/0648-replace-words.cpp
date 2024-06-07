11a7dada428cf264f2ab73eb74d61690fd2cfc23
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-07 13:49:20 +0530

    Time: 56 ms (95.93%), Space: 72 MB (73.14%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,36 +1,76 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEnd;
+    
+    TrieNode() {
+        for(int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+        isEnd = false;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(const string& word) {
+        TrieNode* node = root;
+        for(char c : word) {
+            if(node->children[c - 'a'] == nullptr) {
+                node->children[c - 'a'] = new TrieNode();
+            }
+            node = node->children[c - 'a'];
+        }
+        node->isEnd = true;
+    }
+    
+    string getShortestPrefix(const string& word) {
+        TrieNode* node = root;
+        string prefix = "";
+        for(char c : word) {
+            if(node->children[c - 'a'] == nullptr) {
+                return word; // No prefix found, return the original word
+            }
+            node = node->children[c - 'a'];
+            prefix += c;
+            if(node->isEnd) {
+                return prefix; // Return the found prefix
+            }
+        }
+        return word; // No prefix found, return the original word
+    }
+};
+
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_map<string, string> umap;
-        for(int i = 0; i < dictionary.size(); i++) {
-            umap[dictionary[i]] = dictionary[i];
+        Trie trie;
+        for(const string& word : dictionary) {
+            trie.insert(word);
         }
-        string ans = "";
-        string s;
+        
         stringstream ss(sentence);
-        vector<string> v;
-        while (getline(ss, s, ' ')) {
-            v.push_back(s);
-        }
-        for(int i = 0; i < v.size(); i++) {
-            string tmp = "";
-            bool found = false;
-            for(int j = 0; j < v[i].size(); j++) {
-                tmp += v[i][j];
-                if(umap.find(tmp) != umap.end()) {
-                    ans += tmp;
-                    found = true;
-                    break;
-                }
+        string word;
+        string result;
+        while(ss >> word) {
+            if(!result.empty()) {
+                result += " ";
             }
-            if (!found) {
-                ans += tmp;
-            }
-            ans += " ";
+            result += trie.getShortestPrefix(word);
         }
-        if (!ans.empty() && ans.back() == ' ') {
-            ans.pop_back();
-        }
-        return ans;
+        
+        return result;
     }
 };
