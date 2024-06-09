8bd58f1eed3f57ed03d3ef9d3cce27fdffbe957e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-09 13:10:15 +0530

    Time: 932 ms (20.00%), Space: 53.8 MB (60.00%) - LeetHub

---
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -0,0 +1,24 @@
+#define ll long long
+int dp[2001][4000];
+
+int find(int pos, int sum, const vector<int> &nums) {
+    if (pos == nums.size()) return 0;
+    if (dp[pos][sum] != -1) return dp[pos][sum];
+    
+    int ans = find(pos + 1, sum, nums);
+    if (nums[pos] > sum) {
+        ans = max(ans, nums[pos] + find(pos + 1, sum + nums[pos], nums));
+    }
+    
+    return dp[pos][sum] = ans;
+}
+
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rv) {
+        memset(dp, -1, sizeof(dp));
+        sort(rv.begin(), rv.end());
+        
+        return find(0, 0, rv);
+    }
+};
