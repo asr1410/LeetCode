f7533469b99b5341e070e6b2eba586750f56c051
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-09 13:14:09 +0530

    Time: 192 ms (100.00%), Space: 23.6 MB (60.00%) - LeetHub

--- 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -1,20 +1,19 @@
-#define ll long long
-int dp[2001][4000];
-int find(int pos, int sum, const vector<int> &nums) {
-    if (pos == nums.size()) return 0;
-    if (dp[pos][sum] != -1) return dp[pos][sum];
-    int ans = find(pos + 1, sum, nums);
-    if (nums[pos] > sum) {
-        ans = max(ans, nums[pos] + find(pos + 1, sum + nums[pos], nums));
-    }
-    return dp[pos][sum] = ans;
-}
-
 class Solution {
 public:
-    int maxTotalReward(vector<int>& rv) {
-        memset(dp, -1, sizeof(dp));
-        sort(rv.begin(), rv.end());
-        return find(0, 0, rv);
+    int maxTotalReward(vector<int>& a) {
+        sort(a.begin(), a.end());
+        const int S = 100100;
+        bitset<S> b;
+        b[0] = 1;
+        bitset<S> m;
+        int p = 0;
+        for (int x : a) {
+            while (p < x) 
+                m.set(p++);
+            b |= (b & m) << x;
+        }
+        int r = b.size() - 1;
+        while (!b[r]) r--;
+        return r;
     }
 };
