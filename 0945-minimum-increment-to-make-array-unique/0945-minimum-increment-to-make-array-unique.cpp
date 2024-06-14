7cd6b49e585f2c6ea52e7093afd352cf686ae912
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-15 00:58:34 +0530

    Time: 99 ms (90.55%), Space: 96.5 MB (11.42%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -5,17 +5,13 @@ public:
         for(auto num : nums) {
             map[num]++;
         }
-
         int ans = 0;
-
         for(int i = 0; i < 100000; i++) {
-                int temp = max(0, map[i] - 1);
-                ans += temp;
-                map[i + 1] += temp;
+            int temp = max(0, map[i] - 1);
+            ans += temp;
+            map[i + 1] += temp;
         }
-
         ans += (map[100000] * (map[100000] - 1)) / 2;
-
         return ans;
     }
 };
