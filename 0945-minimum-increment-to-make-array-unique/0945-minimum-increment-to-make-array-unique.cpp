b2cd2edb81998e86382342d8ff550b93c4f93429
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-15 00:53:12 +0530

    Time: 80 ms (98.23%), Space: 96.5 MB (11.42%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        vector<int> map(200003);
-        for(auto num:nums) {
+        vector<int> map(100001, 0);
+        
+        // Populate the frequency map with counts of each number in nums
+        for(int num : nums) {
             map[num]++;
         }
+        
         int ans = 0;
-        for(int i = 0; i < 200002; i++) {
+        
+        // Process the frequencies from 0 to 9999
+        for(int i = 0; i < 100000; i++) {
             if(map[i] > 1) {
-                int temp = map[i] - 1;
-                ans += temp;
-                map[i + 1] += temp;
+                int temp = map[i] - 1; // Calculate the excess count
+                ans += temp; // Increment the answer by the excess count
+                map[i + 1] += temp; // Move the excess to the next number
             }
         }
+        
+        // Handle the case for the number 100000 specifically
+        ans += (map[100000] * (map[100000] - 1)) / 2;
+        
         return ans;
     }
 };
