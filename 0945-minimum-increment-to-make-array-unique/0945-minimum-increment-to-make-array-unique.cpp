cfc28a13844b8397e8be316647a6bc26eabb97a2
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-15 00:59:38 +0530

    Time: 82 ms (97.24%), Space: 96.3 MB (11.81%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        vector<int> map(100001, 0);
+        vector<int> map(100001);
+
         for(auto num : nums) {
             map[num]++;
         }
+
         int ans = 0;
+
         for(int i = 0; i < 100000; i++) {
-            int temp = max(0, map[i] - 1);
-            ans += temp;
-            map[i + 1] += temp;
+            if(map[i] > 1) {
+                int temp = map[i] - 1;
+                ans += temp;
+                map[i + 1] += temp;
+            }
         }
+
         ans += (map[100000] * (map[100000] - 1)) / 2;
+
         return ans;
     }
 };
