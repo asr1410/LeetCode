6b888f249f2a13b41bcb6d833df1365af4296708
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-16 15:49:20 +0530

    Time: 6 ms (36.72%), Space: 13.6 MB (82.46%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
-        long miss = 1, added = 0, i = 0;
-        while (miss <= n) {
-            if (i < nums.size() && nums[i] <= miss) {
+        long added = 0, miss = 1, i = 0;
+        while(miss <= n) {
+            if(i < nums.size() and nums[i] <= miss) {
                 miss += nums[i++];
             } else {
                 miss += miss;
