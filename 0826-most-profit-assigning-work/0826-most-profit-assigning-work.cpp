849aada88773cf4ee8a96f8527ca277229c4b0e0
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-18 16:19:30 +0530

    Time: 43 ms (85.18%), Space: 41 MB (72.17%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -5,19 +5,24 @@ public:
         for (int i = 0; i < profit.size(); i++) {
             comb.emplace_back(difficulty[i], profit[i]);
         }
+        
+        // Sort by difficulty, and if same difficulty then by profit descending
         sort(comb.begin(), comb.end());
+        
+        // Make sure we are tracking the max profit up to each point
+        for (int i = 1; i < comb.size(); i++) {
+            comb[i].second = max(comb[i].second, comb[i - 1].second);
+        }
+        
         int ans = 0;
         for (int i = 0; i < worker.size(); i++) {
-            int temp = 0;
-            for (int j = 0; j < comb.size(); j++) {
-                if (worker[i] < comb[j].first) {
-                    break;
-                } else {
-                    temp = max(temp, comb[j].second);
-                }
+            // Use binary search to find the maximum profit that a worker can get
+            auto it = upper_bound(comb.begin(), comb.end(), make_pair(worker[i], INT_MAX)) - 1;
+            if (it >= comb.begin()) {
+                ans += it->second;
             }
-            ans += temp;
         }
+        
         return ans;
     }
 };
