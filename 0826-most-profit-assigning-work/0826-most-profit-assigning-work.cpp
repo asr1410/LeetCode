37e10dac886835c1bbe2830ede80c760174e0e87
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-18 16:33:50 +0530

    Time: 46 ms (76.16%), Space: 41.3 MB (47.29%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -5,32 +5,19 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        vector<pair<int, int>> comb;
-        comb.reserve(profit.size());
-        for (int i = 0; i < profit.size(); i++) {
-            comb.push_back(make_pair(difficulty[i], profit[i]));
+int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<pair<int, int>> jobs;
+        int N = profit.size(), res = 0, i = 0, best = 0;
+        for (int j = 0; j < N; ++j)
+            jobs.push_back(make_pair(difficulty[j], profit[j]));
+        sort(jobs.begin(), jobs.end());
+        sort(worker.begin(), worker.end());
+        for (int & ability : worker) {
+            while (i < N && ability >= jobs[i].first)
+                best = max(jobs[i++].second, best);
+            res += best;
         }
-        sort(comb.begin(), comb.end());
-        for (int i = 1; i < profit.size(); i++) {
-            comb[i].second = max(comb[i].second, comb[i - 1].second);
-        }
-        int maxProfit = 0;
-        for (int w : worker) {
-            int left = 0, right = comb.size() - 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (comb[mid].first <= w) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-            if (right >= 0) {
-                maxProfit += comb[right].second;
-            }
-        }
-        
-        return maxProfit;
+        return res;
     }
+
 };
