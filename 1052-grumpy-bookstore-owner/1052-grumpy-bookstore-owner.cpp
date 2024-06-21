dcd41b3d983ff5c8a686d18b09d2ee248d85fbe5
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-22 01:13:14 +0530

    Time: 24 ms (59.87%), Space: 34.3 MB (82.34%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& cus, vector<int>& gru, int min) {
+        int left = 0, right = 0, n = cus.size(), zcount = 0, count = 0, msum = 0, temp = 0, lb = -1, ub = -1;
+        
+        while (right < n) {
+            while (right < n && count != min) {
+                if(gru[right] == 1) {
+                    temp += cus[right];
+                }
+                zcount += gru[right];
+                count++;
+                
+                if (zcount && temp > msum) {
+                    msum = temp;
+                    lb = left;
+                    ub = right;
+                }
+                right++;
+            }
+            
+            zcount -= gru[left];
+            if(gru[left] == 1)
+                temp -= cus[left];
+            left++;
+            count--;
+        }
+        cout << msum << " " << lb << " " << ub << endl;
+        for (int i = 0; i < n; i++) {
+            if (gru[i] == 0) {
+                msum += cus[i];
+            }
+        }
+        
+        return msum;
+    }
+};
