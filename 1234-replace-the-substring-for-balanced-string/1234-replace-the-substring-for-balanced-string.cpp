ada4eb11a4ce6e66004f0c44ff6095173d206a8c
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-22 13:37:01 +0530

    Time: 41 ms (31.67%), Space: 10.1 MB (67.26%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int balancedString(string s) {
+        unordered_map<char, int> umap;
+        int n = s.size();
+        int limit = n / 4;
+        for (char c : s) {
+            umap[c]++;
+        }
+        int qc = max(0, umap['Q'] - limit);
+        int wc = max(0, umap['W'] - limit);
+        int ec = max(0, umap['E'] - limit);
+        int rc = max(0, umap['R'] - limit);
+        if (qc == 0 && wc == 0 && ec == 0 && rc == 0) {
+            return 0;
+        }
+        int ans = n;
+        unordered_map<char, int> window;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            window[s[i]]++;
+            while (window['Q'] >= qc && window['W'] >= wc && window['E'] >= ec && window['R'] >= rc) {
+                ans = min(ans, i - j + 1);
+                window[s[j]]--;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
