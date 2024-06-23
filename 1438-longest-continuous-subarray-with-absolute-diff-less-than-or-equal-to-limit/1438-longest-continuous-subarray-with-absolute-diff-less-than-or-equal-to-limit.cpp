d7e894fda49198c10f34fa39827d906470484b57
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-23 14:25:15 +0530

    Time: 122 ms (44.13%), Space: 61.8 MB (56.20%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -9,7 +9,7 @@ public:
             mx.push({nums[j], j});
             mn.push({nums[j], j});
             while(mx.top().first - mn.top().first > limit) {
-                i++;
+                i = min(mx.top().second, mn.top().second) + 1;
                 while(!mx.empty() && mx.top().second < i) mx.pop();
                 while(!mn.empty() && mn.top().second < i) mn.pop();
             }
