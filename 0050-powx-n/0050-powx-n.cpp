805943add0f02a80320200918099a462bbec2a6c
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-24 15:47:52 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (44.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        if(n < 0) {
+            x = 1 / x;
+        } 
+        long num = labs(n);
+        double pow = 1;
+        while(num){
+            if(num & 1) {
+                pow *= x;
+            }
+            
+            x *= x;
+            num >>= 1;
+        }
+        return pow;
     }
 };
