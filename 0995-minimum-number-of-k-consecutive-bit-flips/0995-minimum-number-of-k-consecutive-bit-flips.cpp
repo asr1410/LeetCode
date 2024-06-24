eced0d56fb7516476f180042258ef32990ebcf9e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-25 00:06:50 +0530

    Time: 79 ms (91.60%), Space: 113.1 MB (38.11%) - LeetHub

---
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        vector<int> flipCount(nums.size());
+        int flips = 0, ans = 0, n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(i >= k) {
+                flips -= flipCount[i - k];
+            }
+            if((nums[i] == 0 and flips % 2 == 0) or (nums[i] == 1 and flips % 2 == 1)) {
+                if(i + k > n) {
+                    return -1;
+                } else {
+                    ans++;
+                    flips++;
+                    flipCount[i] = 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
