62df3dca33513dbaaf36aa7e9069da1e53832fbf
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-24 15:02:04 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (67.13%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int myAtoi(string s) {
         long long ans = 0, i = 0, n = s.size(), sign = 1, scount = 0;
-        for(; i < n; i++) {
-            if(s[i] != ' ') {
+
+        for (; i < n; i++) {
+            if (s[i] != ' ') {
                 break;
             }
         }
-        for(; i < n; i++) {
-            if(s[i] != '-' and s[i] != '+') {
+
+        for (; i < n; i++) {
+            if (s[i] != '-' && s[i] != '+') {
                 break;
-            } else if(s[i] == '-') {
+            } else if (s[i] == '-') {
                 sign = -1;
                 scount++;
             } else {
@@ -18,28 +20,31 @@ public:
                 scount++;
             }
         }
-        for(; i < n; i++) {
-            if(s[i] != '0') {
+
+        for (; i < n; i++) {
+            if (s[i] != '0') {
                 break;
             }
         }
-        if(scount > 1) {
+
+        if (scount > 1) {
             return 0;
         }
-        for(; i < n; i++) {
-            if(s[i] >= '0' and s[i] <= '9') {
-                ans += s[i] - '0';
-                if(sign * ans > INT_MAX) {
+
+        for (; i < n; i++) {
+            if (s[i] >= '0' && s[i] <= '9') {
+                ans = ans * 10 + (s[i] - '0');
+                if (sign * ans > INT_MAX) {
                     return INT_MAX;
-                } else if(sign * ans < INT_MIN) {
+                } else if (sign * ans < INT_MIN) {
                     return INT_MIN;
                 }
-                ans *= 10;
             } else {
                 break;
             }
         }
-        ans = ans * sign / 10;
-        return ans;
+
+        ans = ans * sign;
+        return static_cast<int>(ans);
     }
 };
