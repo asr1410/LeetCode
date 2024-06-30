b993549811e7dcdf0418bd4db3c1d59f961a16aa
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-06-30 11:58:27 +0530

    Time: 2 ms (66.72%), Space: 11.8 MB (86.44%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -3,16 +3,17 @@ public:
     void helper(vector<string>& ans, string& temp, int open, int close) {
         if (open == 0 && close == 0) {
             ans.push_back(temp);
-        }
-        if (open > 0) {
-            temp.push_back('(');
-            helper(ans, temp, open - 1, close);
-            temp.pop_back();
-        }
-        if (close > open) {
-            temp.push_back(')');
-            helper(ans, temp, open, close - 1);
-            temp.pop_back();
+        } else {
+            if (open > 0) {
+                temp.push_back('(');
+                helper(ans, temp, open - 1, close);
+                temp.pop_back();
+            }
+            if (close > open) {
+                temp.push_back(')');
+                helper(ans, temp, open, close - 1);
+                temp.pop_back();
+            }
         }
     }
     
