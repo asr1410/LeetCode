5daefd4100ee059bc87bd1427e9d246942a69eea
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-01 09:28:49 +0530

    Time: 3 ms (46.58%), Space: 7 MB (85.89%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,34 +1,22 @@
-#define ll long long
-
+#define mod 1000000007
 class Solution {
 public:
-    // evens  = 0, 2, 4, 6, 8  => 5 evens
-    // primes = 2, 3, 5, 7     => 4 primes
-  
-    int p = 1e9 + 7;
-    
-    // calculating x^y efficeiently
-    ll power(long long x, long long y) {
-      long long res = 1;    
-
-      x = x % p; // Update x if it is more than or equal to p
-      if (x == 0) return 0; 
-
-      while (y > 0) {
-        // If y is odd, multiply x with result
-        if (y & 1) res = (res*x) % p;
-        
-        // we have did the odd step is it was odd, now do the even step
-        y = y>>1; // dividing y by 2, y>>1 is same as y/2
-        x = (x*x) % p;
-      }
-      return res;
+    long long power(long long x, long long y) {
+        if(y == 0) {
+            return 1;
+        }
+        long long ans = power(x, y / 2);
+        ans = (ans * ans) % mod; // Ensure multiplication does not overflow before applying mod
+        if(y % 2) {
+            ans = (ans * x) % mod; // Apply mod after multiplication for the same reason
+        }
+        return ans;
     }
-  
     int countGoodNumbers(long long n) {
-      ll count_of_4s = n/2;
-      ll count_of_5s = n - count_of_4s;
-      ll ans = ((power(4LL, count_of_4s) % p  * power(5LL, count_of_5s) % p) % p);
-      return (int)ans;
+        long long odd = n / 2;
+        long long even = n - odd;
+        // The correction is in the calculation of power for even places. It should be power(4, odd) instead of power(5, odd)
+        // because even positions can have one of the 5 prime numbers (2, 3, 5, 7, 11), and odd positions can have one of the 4 even digits (0, 2, 4, 6, 8).
+        return (power(5, even) * power(4, odd)) % mod;
     }
 };
