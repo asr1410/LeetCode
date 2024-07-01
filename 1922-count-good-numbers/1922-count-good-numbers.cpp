bc7ae64388fddf85611182f5ecb4e9212b3467e3
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-01 09:29:07 +0530

    Time: 3 ms (46.58%), Space: 7.2 MB (47.95%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -6,17 +6,15 @@ public:
             return 1;
         }
         long long ans = power(x, y / 2);
-        ans = (ans * ans) % mod; // Ensure multiplication does not overflow before applying mod
+        ans = (ans * ans) % mod;
         if(y % 2) {
-            ans = (ans * x) % mod; // Apply mod after multiplication for the same reason
+            ans = (ans * x) % mod;
         }
         return ans;
     }
     int countGoodNumbers(long long n) {
         long long odd = n / 2;
         long long even = n - odd;
-        // The correction is in the calculation of power for even places. It should be power(4, odd) instead of power(5, odd)
-        // because even positions can have one of the 5 prime numbers (2, 3, 5, 7, 11), and odd positions can have one of the 4 even digits (0, 2, 4, 6, 8).
         return (power(5, even) * power(4, odd)) % mod;
     }
 };
