8873c95bd7ef7b7808aab13b2819eb420cf63864
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-02 00:14:30 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (85.51%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    void h(int s, vector<int> &c, vector<vector<int>> &r, int k, int n)
+    {
+        if (c.size() > k or n < 0)
+            return;
+        if (n == 0 and c.size() == k)
+            r.push_back(c);
+        for (int i = s; i <= 9; i++)
+        {
+            c.push_back(i);
+            h(i + 1, c, r, k, n - i);
+            c.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n)
+    {
+        vector<vector<int>> r;
+        vector<int> c;
+        h(1, c, r, k, n);
+        return r;
+    }
+};
