3cd00c3d295e3ae409353383d9fad71d3d20dd44
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-02 13:32:13 +0530

    Time: 8 ms (37.02%), Space: 11.8 MB (57.47%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int count = 0;
+            for(int j = 0; j < nums.size(); j++) {
+                if(nums[j] & (1 << i)) {
+                    count++;
+                }
+            }
+            if(count % 3 == 1) {
+                ans = ans | (1 << i);
+            }
+        }
+        return ans;
+    }
+};
