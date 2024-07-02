e99f70d92c3d29707a9323b9639c05bba9e70ea1
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-03 00:02:17 +0530

    Time: 14 ms (59.51%), Space: 19.2 MB (85.03%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorr = nums[0];
+        for(int i = 1; i < nums.size(); i++)
+            xorr ^= nums[i];
+        return xorr;
+    }
+};
