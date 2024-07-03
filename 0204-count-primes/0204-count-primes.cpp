f2eb302f49de5d9c7c9e221aaf0acab71651da32
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-03 15:59:38 +0530

    Time: 443 ms (41.32%), Space: 11.5 MB (70.53%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> count(n);
-        for(int i = 2; i < n; i++) {
-            if(count[i] == 0) {
-                for(int j = i + i; j < n; j += i) {
-                    count[j] = 1;
-                }
-            }
-        }
+        vector<bool> seen(n, false);
         int ans = 0;
-        for(int i = 2; i < n; i++) {
-            ans += count[i] == 0;
+        for (int num = 2; num < n; num++) {
+            if (seen[num]) continue;
+            ans++;
+            for (long mult = (long)num * num; mult < n; mult += num)
+                seen[mult] = true;
         }
         return ans;
     }
