696777ed98936c4d79619f2aa3637e87761416c4
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-04 18:17:10 +0530

    Time: 6 ms (78.90%), Space: 8.1 MB (80.50%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-int characterReplacement(string s, int k) {
-    int ans = 0, n = s.size();
-    for(char c = 'A'; c <= 'Z'; c++) { // Loop through each possible character 'A' to 'Z'
-        int i = 0, t = k;
-        for(int j = 0; j < n; j++) {
-            t -= s[j] != c; // If s[j] is not equal to c, decrement t (number of replacements left)
-            t += t < 0 and s[i++] != c; // If t becomes negative, increment i (move i to the right)
+    int characterReplacement(string s, int k) {
+        int ans = 0, n = s.size();
+        for(char c = 'A'; c <= 'Z'; c++) {
+            int i = 0, t = k;
+            for(int j = 0; j < n; j++) {
+                t -= s[j] != c;
+                t += t < 0 and s[i++] != c;
+            }
+            ans = max(ans, n - i);
         }
-        ans = max(ans, n - i); // Update ans to be the maximum length found
+        return ans;
     }
-    return ans;
-}
 };
