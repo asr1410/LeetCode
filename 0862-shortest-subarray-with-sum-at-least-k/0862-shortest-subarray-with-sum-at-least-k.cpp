8d651d76fb5f29060d5a9fe79ca2b23f19b25510
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-05 23:09:03 +0530

    Time: 166 ms (47.83%), Space: 120.3 MB (17.17%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -11,12 +11,12 @@ public:
         dq.push_back({0, -1});
         int ans = INT_MAX;
         for(int i = 0; i < n; i++) {
+            while(dq.empty() == false and dq.back().first >= prefix[i]) {
+                dq.pop_back();
+            }
             while(dq.empty() == false and prefix[i] - k >= dq.front().first) {
                 ans = min(ans, i - dq.front().second);
                 dq.pop_front();
-            }
-            while(dq.empty() == false and dq.back().first >= prefix[i]) {
-                dq.pop_back();
             }
             dq.push_back({prefix[i], i});
         }
