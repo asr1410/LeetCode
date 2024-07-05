53617dbc43c8cf0b827057288ea540d0e46b2d96
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-05 23:12:54 +0530

    Time: 155 ms (65.11%), Space: 112.2 MB (61.87%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> prefix(n);
-        prefix[0] = nums[0];
-        for(int i = 1, j = 0; i < n; i++, j++) {
-            prefix[i] = prefix[j] + nums[i];
-        }
+        long long prefix = 0;
         deque<pair<long long, int>> dq;
         dq.push_back({0, -1});
         int ans = INT_MAX;
         for(int i = 0; i < n; i++) {
-            while(dq.empty() == false and dq.back().first >= prefix[i]) {
+            prefix += nums[i];
+            while(dq.empty() == false and dq.back().first >= prefix) {
                 dq.pop_back();
             }
-            while(dq.empty() == false and prefix[i] - k >= dq.front().first) {
+            while(dq.empty() == false and prefix - k >= dq.front().first) {
                 ans = min(ans, i - dq.front().second);
                 dq.pop_front();
             }
-            dq.push_back({prefix[i], i});
+            dq.push_back({prefix, i});
         }
         return ans == INT_MAX ? -1 : ans;
     }
