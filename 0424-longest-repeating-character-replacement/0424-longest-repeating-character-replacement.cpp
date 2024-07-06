b7c9e14a690606889dc85ff3fb5c40f7705a9774
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-06 11:40:17 +0530

    Time: 7 ms (70.61%), Space: 8.2 MB (80.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.size(), ans = 0;
-        for(char c = 'A'; c <= 'Z'; c++) {
-            for(int l = 0, r = 0, count = 0; r < n; r++) {
-                count += s[r] != c;
-                while(count > k) {
-                    count -= s[l] != c;
-                    l++;
-                }
+        vector<int> count(256);
+        int n = s.size(), ans = 0, maxf = 0;
+        for(int l = 0, r = 0; r < n; r++) {
+            count[s[r]]++;
+            maxf = max(maxf, count[s[r]]);
+            if(r - l - maxf + 1 <= k) {
                 ans = max(ans, r - l + 1);
+            } else {
+                count[s[l]]--;
+                l++;
             }
         }
         return ans;
