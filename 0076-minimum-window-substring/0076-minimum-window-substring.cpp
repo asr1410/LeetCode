1f70f5bb5ccb376a2832cc0fe223e0af50bf2a4f
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-06 14:41:26 +0530

    Time: 27 ms (37.59%), Space: 10.8 MB (39.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -4,46 +4,26 @@ public:
         unordered_map<char, int> umapt;
         unordered_map<char, int> umaps;
         int n = s.size(), k = t.size();
-        pair<int, int> ans = {-1, n}; // Initialize ans with an invalid window
-        
-        // Populate umapt with frequency of characters in t
+        pair<int, int> ans = {-1, n};
         for(int i = 0; i < k; i++) {
             umapt[t[i]]++;
         }
-        
-        // Two pointers approach
         for(int l = 0, r = 0, count = 0; r < n; r++) {
-            // Expand the window by including s[r]
             if(umapt.find(s[r]) != umapt.end()) {
                 umaps[s[r]]++;
-                // Count only when the frequency of s[r] in the window matches or exceeds its requirement in umapt
-                if(umaps[s[r]] <= umapt[s[r]]) {
-                    count++;
-                }
+                count += umaps[s[r]] <= umapt[s[r]];
             }
-            
-            // Contract the window from the left if all characters of t are found in the current window
             while(count == k) {
-                // Update ans if the current window is smaller than the previously recorded one
-                if(r - l + 1 < ans.second - ans.first + 1) {
+                if(ans.second - ans.first + 1 > r - l + 1) {
                     ans = {l, r};
                 }
-                
-                // Remove s[l] from the window
                 if(umapt.find(s[l]) != umapt.end()) {
                     umaps[s[l]]--;
-                    // Update count only when the frequency of s[l] becomes less than required
-                    if(umaps[s[l]] < umapt[s[l]]) {
-                        count--;
-                    }
+                    count -= umaps[s[l]] < umapt[s[l]];
                 }
-                
-                // Move the left pointer to shrink the window
                 l++;
             }
         }
-        
-        // If ans was not updated, return an empty string
         if(ans.first == -1) {
             return "";
         } else {
