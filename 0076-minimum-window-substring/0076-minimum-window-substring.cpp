5b016d9b5033e9457bb9e3de90c4272575e72e09
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-06 14:32:42 +0530

    Time: 26 ms (40.43%), Space: 10.8 MB (25.28%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -5,36 +5,26 @@ public:
         unordered_map<char, int> umaps;
         int n = s.size(), k = t.size();
         pair<int, int> ans = {-1, n};
-        
-        for (char c : t) {
-            umapt[c]++;
+        for(int i = 0; i < k; i++) {
+            umapt[t[i]]++;
         }
-        
-        for (int l = 0, r = 0, count = 0; r < n; r++) {
-            if (umapt.find(s[r]) != umapt.end()) {
+        for(int l = 0, r = 0, count = 0; r < n; r++) {
+            if(umapt.find(s[r]) != umapt.end()) {
                 umaps[s[r]]++;
-                if (umaps[s[r]] <= umapt[s[r]]) {
-                    count++;
-                }
+                count += umaps[s[r]] <= umapt[s[r]];
             }
-            
-            while (count == k) {
-                if (r - l < ans.second - ans.first) {
+            while(count == k) {
+                if(ans.second - ans.first > r - l) {
                     ans = {l, r};
                 }
-                
-                if (umapt.find(s[l]) != umapt.end()) {
+                if(umapt.find(s[l]) != umapt.end()) {
                     umaps[s[l]]--;
-                    if (umaps[s[l]] < umapt[s[l]]) {
-                        count--;
-                    }
+                    count -= umaps[s[l]] < umapt[s[l]];
                 }
-                
                 l++;
             }
         }
-        
-        if (ans.first == -1) {
+        if(ans.first == -1) {
             return "";
         } else {
             return s.substr(ans.first, ans.second - ans.first + 1);
