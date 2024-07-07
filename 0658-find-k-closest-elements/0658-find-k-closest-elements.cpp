a3b14ad8873e125ff227b79915a8e728ace13848
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-07 18:46:46 +0530

    Time: 33 ms (51.44%), Space: 33.6 MB (79.93%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& nums, int k, int x) {
-        vector<int> ans;
-        priority_queue<pair<int, int>> pq;
-        
-        for(int i = 0; i < k; i++) {
-            pq.push({abs(nums[i] - x), nums[i]});
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0, right = arr.size() - k;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        
-        for(int i = k; i < nums.size(); i++) {
-            if(abs(nums[i] - x) < pq.top().first) {
-                pq.pop();
-                pq.push({abs(nums[i] - x), nums[i]});
-            } else if(abs(nums[i] - x) == pq.top().first and nums[i] < pq.top().second) {
-                pq.pop();
-                pq.push({abs(nums[i] - x), nums[i]});
-            }
-        }
-        
-        while(!pq.empty()) {
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        sort(ans.begin(), ans.end());
-        return ans;
+        return vector<int>(arr.begin() + left, arr.begin() + left + k);
     }
 };
