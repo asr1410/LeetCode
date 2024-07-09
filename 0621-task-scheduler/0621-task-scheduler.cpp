f345a1431f9ef2869a277b49222251c209def3d6
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-09 14:44:54 +0530

    Time: 52 ms (72.78%), Space: 38 MB (77.65%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        // Create a frequency array to keep track of the count of each task
+        int maxf = 0;
         vector<int> freq(26);
-        for (char task : tasks) {
-            freq[task - 'A']++;
+        for(auto &task:tasks) {
+            maxf = max(maxf, ++freq[task - 'A']);
         }
-
-        // Sort the frequency array in non-decreasing order
-        sort(freq.begin(), freq.end());
-        // Calculate the maximum frequency of any task
-        int maxFreq = freq[25] - 1;
-        // Calculate the number of idle slots that will be required
-        int idleSlots = maxFreq * n;
-
-        // Iterate over the frequency array from the second highest frequency to the lowest frequency
-        for (int i = 24; i >= 0 && freq[i] > 0; i--) {
-            // Subtract the minimum of the maximum frequency and the current frequency from the idle slots
-            idleSlots -= min(maxFreq, freq[i]);
+        int time = (maxf - 1) * (n + 1);
+        for(auto f:freq) {
+            time += maxf == f;
         }
-
-        // If there are any idle slots left, add them to the total number of tasks
-        return idleSlots > 0 ? idleSlots + tasks.size() : tasks.size();
+        return max((int)tasks.size(), time);
     }
 };
