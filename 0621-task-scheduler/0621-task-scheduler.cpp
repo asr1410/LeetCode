ac2c187166552976df71140c6ed577f1d5ec0be4
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-09 11:16:22 +0530

    Time: 75 ms (47.64%), Space: 38.2 MB (58.30%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         vector<int> pcount(26);
-        for(auto task:tasks) {
+        for (auto task : tasks) {
             pcount[task - 'A']++;
         }
         vector<pair<int, int>> temp;
-        for(int i = 0; i < 26; i++) {
-            if(pcount[i]) {
+        for (int i = 0; i < 26; i++) {
+            if (pcount[i] > 0) {
                 temp.push_back({pcount[i], i});
             }
         }
@@ -15,20 +15,20 @@ public:
         reverse(temp.begin(), temp.end());
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         int time = 1;
-        for(int i = 0; i < temp.size(); i++) {
+        for (int i = 0; i < temp.size(); i++) {
             pq.push({i + 1, temp[i].second});
         }
-        while(!pq.empty()) {
-            if(time >= pq.top().first) {
+        while (!pq.empty()) {
+            if (time >= pq.top().first) {
                 auto top = pq.top();
                 pq.pop();
                 top.first = top.first + n + 1;
                 pcount[top.second]--;
-                if(pcount[top.second] > 0) {
+                if (pcount[top.second] > 0) {
                     pq.push(top);
                 }
             }
-            if(pq.empty()) {
+            if (pq.empty()) {
                 break;
             }
             time++;
