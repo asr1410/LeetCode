fdaf6f55b39b517913d17834c1f66d3fda0dada6
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-10 23:27:14 +0530

    Time: 8 ms (96.59%), Space: 17.4 MB (29.51%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,29 +1,35 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+typedef pair<int, ListNode*> PIL;
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> mnh;
+        priority_queue<PIL, vector<PIL>, greater<PIL>> minHeap;
+        for (auto list : lists) {
+            if (list) minHeap.push({list->val, list});
+        }
         ListNode* head = nullptr;
         ListNode* tail = nullptr;
-        for (int i = 0; i < lists.size(); ++i) {
-            if (lists[i]) {
-                mnh.push({ lists[i]->val, i });
-            }
-        }
-        while (!mnh.empty()) {
-            auto minNodeIdx = mnh.top().second;
-            mnh.pop();
-            if (head == nullptr) {
-                head = lists[minNodeIdx];
+        while (minHeap.empty() == false) {
+            auto smallest = minHeap.top();
+            minHeap.pop();
+            ListNode* newNode = smallest.second;
+            if (!head) {
+                head = newNode;
                 tail = head;
             } else {
-                tail->next = lists[minNodeIdx];
+                tail->next = newNode;
                 tail = tail->next;
             }
-            lists[minNodeIdx] = lists[minNodeIdx]->next;
-            if (lists[minNodeIdx]) {
-                mnh.push({ lists[minNodeIdx]->val, minNodeIdx });
+            
+            if (newNode->next) {
+                minHeap.push({newNode->next->val, newNode->next});
             }
         }
+        
         return head;
     }
 };
