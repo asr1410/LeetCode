4207e584d3e88c4a163b963c0a41b8c4c5864fc0
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-11 10:35:47 +0530

    Time: 232 ms (98.46%), Space: 121.9 MB (20.14%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,37 @@
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>, greater<int>> minh;
+    priority_queue<int> maxh;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        maxh.push(num);
+        if(minh.empty() or minh.size() + 1 < maxh.size()) {
+            minh.push(maxh.top());
+            maxh.pop();
+        } else if(minh.top() < maxh.top()) {
+            maxh.push(minh.top());
+            minh.pop();
+            minh.push(maxh.top());
+            maxh.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(minh.size() > maxh.size()) {
+            return minh.top();
+        } else if(maxh.size() > minh.size()) {
+            return maxh.top();
+        }
+        return ((double)minh.top() + maxh.top()) / 2;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
