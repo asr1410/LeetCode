cdda5a9576c32b68514d2f2fbb8e2cb0adfdd4ef
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-12 13:45:56 +0530

    Time: 67 ms (79.47%), Space: 86.1 MB (7.36%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    bool check(int bill, int &five, int &ten) {
-        if(bill == 20) {
-            if(ten > 0 && five > 0) {
-                ten--;
-                five--;
-            } else if(five >= 3) {
-                five -= 3;
-            } else {
-                return false;
-            }
-        } else if(bill == 10) {
-            if(five > 0) {
-                five--;
-                ten++;
-            } else {
-                return false;
-            }
-        } else {
-            five++;
-        }
-        return true;
-    }
-
     bool lemonadeChange(vector<int>& bills) {
         int five = 0, ten = 0;
-        for(int bill : bills) {
-            if(!check(bill, five, ten)) {
-                return false;
+        for(auto &bill:bills) {
+            if(bill == 20) {
+                if(ten and five) {
+                    ten--;
+                    five--;
+                } else if(five > 2) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            } else if(bill == 10) {
+                if(five) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else {
+                five++;
             }
         }
         return true;
