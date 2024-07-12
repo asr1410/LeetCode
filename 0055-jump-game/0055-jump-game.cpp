d6bacdaf8bdda0bacc02eaa3bf006ddfaddf4d45
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-12 16:19:34 +0530

    Time: 46 ms (51.53%), Space: 51.2 MB (18.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int i = 0, n = nums.size();
-        while(i < n - 1) {
-            if(nums[i] == 0) {
-                return false;
-            } else if(nums[i] + i >= n - 1) {
-                return true;
-            } else {
-                int limit = nums[i] + i, next = limit, mreach = limit + nums[limit];
-                for(int j = i + 1; j <= limit; j++) {
-                    if(j + nums[j] > mreach) {
-                        mreach = j + nums[j];
-                        next = j;
-                    }
-                }
-                i = next;
-            }
+        int farthest = 0, n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            // If we can't reach this position, return false
+            if (i > farthest) return false;
+            
+            // Update the farthest position reachable
+            farthest = max(farthest, i + nums[i]);
+            
+            // If we can reach or surpass the last index, return true
+            if (farthest >= n - 1) return true;
         }
-        return true;
+        
+        return false;
     }
 };
