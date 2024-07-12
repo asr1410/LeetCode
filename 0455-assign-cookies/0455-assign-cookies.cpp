9c5d21a6cc3aaaaa92e000126d1d297a890a05c3
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-12 10:45:48 +0530

    Time: 19 ms (74.20%), Space: 20.9 MB (28.15%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,12 +3,11 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        int ans = 0;
         int i = 0, j = 0, ni = g.size(), nj = s.size();
         while(i < ni && j < nj) {
-            ans += g[i] <= s[j] and ++i;
+            i += g[i] <= s[j];
             j++;
         }
-        return ans;
+        return i;
     }
 };
