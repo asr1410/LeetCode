7370fb7aed5fb0f7a5f5585b150cfbef9207af4e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-14 10:23:06 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (83.33%) - LeetHub

--- 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.cpp
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.cpp
@@ -3,7 +3,7 @@ public:
     string getSmallestString(string s) {
         for (int i = 1; i < s.size(); i++) {
             if (s[i] < s[i - 1]) {
-                if ((s[i] % 2) == (s[i - 1] % 2)) {
+                if ((s[i] & 1) == (s[i - 1] & 1)) {
                     swap(s[i], s[i - 1]);
                     return s;
                 }
