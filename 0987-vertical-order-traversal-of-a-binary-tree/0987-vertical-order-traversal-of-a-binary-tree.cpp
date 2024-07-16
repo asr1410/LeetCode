b4f3c050fac77594ee2a4af0afba8ce11fa2690e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-16 14:30:02 +0530

    Time: 8 ms (16.63%), Space: 14.5 MB (68.01%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,54 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void traversal(TreeNode *root, vector<pair<int, pair<int, int>>> &store, int x, int y) {
-        if(root == nullptr) {
-            return;
-        }
-        store.push_back(make_pair(y, make_pair(x, root->val)));
+    int mn = INT_MAX;
+    int mx = INT_MIN;
+
+    void traversal(TreeNode *root, unordered_map<int, vector<pair<int, int>>> &store, int x, int y) {
+        if (!root) return;
+        mn = min(mn, y);
+        mx = max(mx, y);
+        store[y].push_back(make_pair(x, root->val));
         traversal(root->left, store, x + 1, y - 1);
         traversal(root->right, store, x + 1, y + 1);
     }
+
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<pair<int, pair<int, int>>> store;
+        unordered_map<int, vector<pair<int, int>>> store;
         traversal(root, store, 0, 0);
-        sort(store.begin(), store.end());
         vector<vector<int>> ans;
-        vector<pair<int, int>> temp;
-        for(int i = 0, start = 0; i < store.size(); i++) {
-            if(i == 0 or start != store[i].first) {
-                start = store[i].first;
-                if(temp.empty() == false) {
-                    vector<int> t;
-                    sort(temp.begin(), temp.end());
-                    for(auto &c:temp) {
-                        t.push_back(c.second);
-                    }
-                    ans.push_back(t);
-                    temp.clear();
+
+        for (int i = mn; i <= mx; i++) {
+            if (store.find(i) != store.end()) {
+                sort(store[i].begin(), store[i].end());
+                vector<int> temp;
+                for (const auto& p : store[i]) {
+                    temp.push_back(p.second);
                 }
+                ans.push_back(temp);
             }
-            temp.push_back(make_pair(store[i].second.first, store[i].second.second));
         }
-        if(temp.empty() == false) {
-                    vector<int> t;
-                    sort(temp.begin(), temp.end());
-                    for(auto &c:temp) {
-                        t.push_back(c.second);
-                    }
-                    ans.push_back(t);
-                    temp.clear();
-                }
         return ans;
     }
 };
