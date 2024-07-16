c1e9389512ce8bc7a4fb924644853b38ad0563cb
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-16 16:05:00 +0530

    Time: 0 ms (100.00%), Space: 15.2 MB (6.61%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int mx = INT_MIN;
+
+    void traverse(TreeNode* root, unordered_map<int, pair<int, int>> &store, int x, int y) {
+        if (root == nullptr) {
+            return;
+        }
+        if (store.find(x) != store.end()) {
+            if (store[x].first <= x) {
+                store[x].first = y;
+                store[x].second = root->val;
+            }
+        } else {
+            mx = max(mx, x);
+            store[x] = {y, root->val};
+        }
+        traverse(root->left, store, x + 1, y - 1);
+        traverse(root->right, store, x + 1, y + 1);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        unordered_map<int, pair<int, int>> store;
+        traverse(root, store, 0, 0);
+        vector<int> ans;
+        for (int i = 0; i <= mx; i++) {
+            if (store.find(i) != store.end()) {
+                ans.push_back(store[i].second);
+            }
+        }
+        return ans;
+    }
+};
