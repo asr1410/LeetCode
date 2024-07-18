0eafe4c32d0a165ceff6f9c951b9d47cda6d6b28
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-19 00:52:15 +0530

    Time: 20 ms (80.12%), Space: 33.6 MB (7.52%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root) {
-            if (root->val > key) {
-                root->left = deleteNode(root->left, key);
-            } else if (root->val < key) {
-                root->right = deleteNode(root->right, key);
-            } else {
-                if (root->left == nullptr) {
-                    TreeNode* rightChild = root->right;
-                    delete root;
-                    return rightChild;
-                } else if (root->right == nullptr) {
-                    TreeNode* leftChild = root->left;
-                    delete root;
-                    return leftChild;
-                }
-                TreeNode* successor = findMin(root->right);
-                root->val = successor->val;
-                root->right = deleteNode(root->right, successor->val);
+        if(root) 
+            if(key < root->val) root->left = deleteNode(root->left, key);     //We frecursively call the function until we find the target node
+            else if(key > root->val) root->right = deleteNode(root->right, key);       
+            else{
+                if(!root->left && !root->right) return NULL;          //No child condition
+                if (!root->left || !root->right)
+                    return root->left ? root->left : root->right;    //One child contion -> replace the node with it's child
+					                                                //Two child condition   
+                TreeNode* temp = root->left;                        //(or) TreeNode *temp = root->right;
+                while(temp->right != NULL) temp = temp->right;     //      while(temp->left != NULL) temp = temp->left;
+                root->val = temp->val;                            //       root->val = temp->val;
+                root->left = deleteNode(root->left, temp->val);  //        root->right = deleteNode(root->right, temp);		
             }
-        }
         return root;
-    }
-    
-private:
-    TreeNode* findMin(TreeNode* node) {
-        while (node && node->left) {
-            node = node->left;
-        }
-        return node;
-    }
+    }   
 };
