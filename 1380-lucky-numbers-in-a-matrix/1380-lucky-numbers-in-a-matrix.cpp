3b09a6c4b023666a778e9d73778cb466beb707dd
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-19 09:55:13 +0530

    Time: 21 ms (14.23%), Space: 14.5 MB (16.09%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& mat) {
-        unordered_set<int> uset;
-        for(int i = 0; i < mat.size(); i++) {
-            int mn = mat[i][0];
-            for(int j = 1; j < mat[0].size(); j++) {
-                mn = min(mn, mat[i][j]);
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        vector<int> minInRow(m, INT_MAX);
+        vector<int> maxInCol(n, INT_MIN);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                minInRow[i] = min(minInRow[i], mat[i][j]);
             }
-            uset.insert(mn);
         }
+        
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < m; ++i) {
+                maxInCol[j] = max(maxInCol[j], mat[i][j]);
+            }
+        }
+        
         vector<int> ans;
-        for(int j = 0; j < mat[0].size(); j++) {
-            int mx = mat[0][j];
-            for(int i = 1; i < mat.size(); i++) {
-                mx = max(mx, mat[i][j]);
-            }
-            if(uset.count(mx)) {
-                ans.push_back(mx);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == minInRow[i] && mat[i][j] == maxInCol[j]) {
+                    ans.push_back(mat[i][j]);
+                }
             }
         }
+        
         return ans;
     }
 };
