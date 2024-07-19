3536636762eb0ac3a41ca39f5708049a1abdf092
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-19 10:05:37 +0530

    Time: 3 ms (97.75%), Space: 20.5 MB (13.29%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,14 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        TreeNode* prev = NULL;
-        return validate(root, prev);
+        TreeNode* prev = nullptr;
+        return traverse(root, prev);
     }
-    bool validate(TreeNode* node, TreeNode* &prev) {
-        if (node == NULL) return true;
-        if (!validate(node->left, prev)) return false;
-        if (prev != NULL && prev->val >= node->val) return false;
-        prev = node;
-        return validate(node->right, prev);
+    bool traverse(TreeNode* root, TreeNode* &prev) {
+        if(root == nullptr) {
+            return true;
+        }
+        if(traverse(root->left, prev) == false) {
+            return false;
+        }
+        if(prev != nullptr and prev->val >= root->val) {
+            return false;
+        }
+        prev = root;
+        return traverse(root->right, prev);
     }
 };
