a4758fd7966c2eca7e18dcae0089c2af7670a3b7
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-21 00:54:35 +0530

    Time: 84 ms (100.00%), Space: 90.2 MB (100.00%) - LeetHub

--- 3224-minimum-array-changes-to-make-differences-equal/3224-minimum-array-changes-to-make-differences-equal.cpp
+++ 3224-minimum-array-changes-to-make-differences-equal/3224-minimum-array-changes-to-make-differences-equal.cpp
@@ -1,129 +1,22 @@
-//start of jonathanirvings' template v3.0.3 (BETA)
-
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long LL;
-typedef pair<int,int> pii;
-typedef pair<LL,LL> pll;
-typedef pair<string,string> pss;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<pii> vii;
-typedef vector<LL> vl;
-typedef vector<vl> vvl;
-
-double EPS = 1e-9;
-int INF = 1000000005;
-long long INFF = 1000000000000000005LL;
-double PI = acos(-1);
-int dirx[8] = {-1,0,0,1,-1,-1,1,1};
-int diry[8] = {0,1,-1,0,-1,1,-1,1};
-
-#ifdef TESTING
-  #define DEBUG fprintf(stderr,"====TESTING====\n")
-  #define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
-  #define debug(...) fprintf(stderr, __VA_ARGS__)
-#else
-  #define DEBUG 
-  #define VALUE(x)
-  #define debug(...)
-#endif
-
-#define FOR(a,b,c) for (int (a)=(b);(a)<(c);++(a))
-#define FORN(a,b,c) for (int (a)=(b);(a)<=(c);++(a))
-#define FORD(a,b,c) for (int (a)=(b);(a)>=(c);--(a))
-#define FORSQ(a,b,c) for (int (a)=(b);(a)*(a)<=(c);++(a))
-#define FORC(a,b,c) for (char (a)=(b);(a)<=(c);++(a))
-#define FOREACH(a,b) for (auto &(a) : (b))
-#define REP(i,n) FOR(i,0,n)
-#define REPN(i,n) FORN(i,1,n)
-#define MAX(a,b) a = max(a,b)
-#define MIN(a,b) a = min(a,b)
-#define SQR(x) ((LL)(x) * (x))
-#define RESET(a,b) memset(a,b,sizeof(a))
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define ALL(v) v.begin(),v.end()
-#define ALLA(arr,sz) arr,arr+sz
-#define SIZE(v) (int)v.size()
-#define SORT(v) sort(ALL(v))
-#define REVERSE(v) reverse(ALL(v))
-#define SORTA(arr,sz) sort(ALLA(arr,sz))
-#define REVERSEA(arr,sz) reverse(ALLA(arr,sz))
-#define PERMUTE next_permutation
-#define TC(t) while(t--)
-
-inline string IntToString(LL a){
-  char x[100];
-  sprintf(x,"%lld",a); string s = x;
-  return s;
-}
-
-inline LL StringToInt(string a){
-  char x[100]; LL res;
-  strcpy(x,a.c_str()); sscanf(x,"%lld",&res);
-  return res;
-}
-
-inline string GetString(void){
-  char x[1000005];
-  scanf("%s",x); string s = x;
-  return s;
-}
-
-inline string uppercase(string s){
-  int n = SIZE(s); 
-  REP(i,n) if (s[i] >= 'a' && s[i] <= 'z') s[i] = s[i] - 'a' + 'A';
-  return s;
-}
-
-inline string lowercase(string s){
-  int n = SIZE(s); 
-  REP(i,n) if (s[i] >= 'A' && s[i] <= 'Z') s[i] = s[i] - 'A' + 'a';
-  return s;
-}
-
-inline void OPEN (string s) {
-  #ifndef TESTING
-  freopen ((s + ".in").c_str (), "r", stdin);
-  freopen ((s + ".out").c_str (), "w", stdout);
-  #endif
-}
-
-#ifdef TESTING
-int main() {}
-#endif
-
-//end of jonathanirvings' template v3.0.3 (BETA)
-
-int tambah[100005];
-int ada[100005];
-
 class Solution {
 public:
-  int minChanges(vector<int>& nums, int k) {
-    FORN(i,0,k + 1)
-    {
-      tambah[i] = 0;
-      ada[i] = 0;
+    int minChanges(vector<int>& nums, int K) {
+        int n = nums.size();
+        int f[K + 2];
+        memset(f, 0, sizeof(f));
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            int d = abs(nums[i] - nums[j]);
+            int mx = max({nums[i], K - nums[i], nums[j], K - nums[j]});
+            f[0]++; f[d]--;
+            f[d + 1]++; f[mx + 1]--;
+            f[mx + 1] += 2;
+        }
+
+        int ans = n;
+        for (int i = 0, now = 0; i <= K + 1; i++) {
+            now += f[i];
+            ans = min(ans, now);
+        }
+        return ans;
     }
-    int n = SIZE(nums);
-    
-    REP(i,n/2)
-    {
-      ++ada[abs(nums[i] - nums[n-i-1])];
-      int x = max(nums[i],k-nums[i]);
-      int y = max(nums[n-i-1],k-nums[n-i-1]);
-      int z = max(x,y);
-      ++tambah[z + 1];
-    }
-    FORN(i,1,k) tambah[i] += tambah[i-1];
-    
-    int risan = n/2;
-    FORN(i,0,k) MIN(risan,n/2 - ada[i] + tambah[i]);
-    return risan;
-  }
 };
