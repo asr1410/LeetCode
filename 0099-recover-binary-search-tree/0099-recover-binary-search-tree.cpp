d2aeebf81d071da7280ce2d299893e7d7e56176e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-23 01:55:56 +0530

    Time: 32 ms (9.60%), Space: 60.5 MB (52.64%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,35 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int i = 0;
-    void traverse(TreeNode* root, vector<int> &nums) {
-        if(root != nullptr) {
-            traverse(root->left, nums);
-            nums.push_back(root->val);
-            traverse(root->right, nums);
-        }
-    }
-    void update(TreeNode* root, vector<int> &nums) {
-        if(root != nullptr) {
-            update(root->left, nums);
-            root->val = nums[i++];
-            update(root->right, nums);
-        }
-    }
     void recoverTree(TreeNode* root) {
-        vector<int> nums;
-        traverse(root, nums);
-        sort(nums.begin(), nums.end());
-        update(root, nums);
+        TreeNode* first = nullptr;
+        TreeNode* second = nullptr;
+        TreeNode* prev = nullptr;
+        
+        inorderTraversal(root, prev, first, second);
+        
+        if (first && second) {
+            swap(first->val, second->val);
+        }
+    }
+    
+    void inorderTraversal(TreeNode* root, TreeNode*& prev, TreeNode*& first, TreeNode*& second) {
+        if (root == nullptr) return;
+        
+        inorderTraversal(root->left, prev, first, second);
+        
+        if (prev && prev->val > root->val) {
+            if (!first) {
+                first = prev;
+            }
+            second = root;
+        }
+        prev = root;
+        
+        inorderTraversal(root->right, prev, first, second);
     }
 };
