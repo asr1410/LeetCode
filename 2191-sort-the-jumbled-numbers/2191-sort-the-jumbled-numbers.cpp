a3b0780ebc94526ba4ecee718db52e36408d9f0a
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-24 16:01:35 +0530

    Time: 388 ms (42.42%), Space: 76.5 MB (96.10%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        auto mapNumber = [&mapping](int num) {
+            if (num == 0) return mapping[0];
+            int mappedNumber = 0;
+            int place = 1;
+            while (num > 0) {
+                int digit = num % 10;
+                mappedNumber = mapping[digit] * place + mappedNumber;
+                num /= 10;
+                place *= 10;
+            }
+            return mappedNumber;
+        };
+
+        // Sort nums using a custom comparator
+        sort(nums.begin(), nums.end(), [&mapping, &mapNumber](const int &a, const int &b) {
+            int mappedA = mapNumber(a);
+            int mappedB = mapNumber(b);
+            return mappedA < mappedB;
+        });
+
+        return nums;
+    }
+};
