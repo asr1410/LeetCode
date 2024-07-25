d5c63794afd264134f3412426246831f761ce8bd
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-25 23:46:45 +0530

    Time: 4 ms (84.00%), Space: 17 MB (46.07%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void helper(vector<vector<int>> &image, int sr, int sc, int ncol, int ocol) {
-        if (sr >= 0 && sr < image.size() && sc >= 0 && sc < image[0].size() && image[sr][sc] == ocol && image[sr][sc] != ncol) {
+        if (sr >= 0 && sr < image.size() && sc >= 0 && sc < image[0].size() && image[sr][sc] == ocol) {
             image[sr][sc] = ncol;
             helper(image, sr - 1, sc, ncol, ocol);
             helper(image, sr, sc - 1, ncol, ocol);
@@ -12,7 +12,9 @@ public:
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int ncol) {
         int ocol = image[sr][sc];
-        helper(image, sr, sc, ncol, ocol);
+        if (ocol != ncol) {
+            helper(image, sr, sc, ncol, ocol);
+        }
         return image;
     }
 };
