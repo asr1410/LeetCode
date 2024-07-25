90696839cd815ff0062f45b6bf269062d507a840
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-25 17:20:17 +0530

    Time: 25 ms (29.70%), Space: 20.9 MB (9.47%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        unordered_map<int, vector<int>> umap;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(isConnected[i][j] == 1) {
+                    umap[i].push_back(j);
+                }
+            }
+        }
+        
+        int ans = 0;
+        vector<int> vis(n);
+        
+        for(int i = 0; i < n; i++) {
+            if(vis[i] == 0) {
+                queue<int> q;
+                q.push(i);
+                vis[i] = 1;
+                ans++;
+                
+                while(!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+                    
+                    for(auto neighbor : umap[node]) {
+                        if(vis[neighbor] == 0) {
+                            q.push(neighbor);
+                            vis[neighbor] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
