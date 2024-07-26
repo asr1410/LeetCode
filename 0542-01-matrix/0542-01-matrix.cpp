806e309e5aa178e8974425442be2acda47c75dee
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-27 01:02:52 +0530

    Time: 59 ms (35.62%), Space: 34.2 MB (77.70%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,7 +4,7 @@ public:
         int rows = mat.size();
         int cols = mat[0].size();
         queue<pair<int, int>> q;
-        vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
+        vector<vector<int>> dist(rows, vector<int>(cols, -1));
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 if (mat[i][j] == 0) {
@@ -21,12 +21,10 @@ public:
             for (int i = 0; i < size; ++i) {
                 auto front = q.front();
                 q.pop();
-                
                 for (auto& d : dir) {
                     int nr = front.first + d[0];
                     int nc = front.second + d[1];
-                    
-                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && dist[nr][nc] == INT_MAX) {
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && dist[nr][nc] == -1) {
                         dist[nr][nc] = dist[front.first][front.second] + 1;
                         q.emplace(nr, nc);
                     }
