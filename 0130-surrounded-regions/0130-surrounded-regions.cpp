7db279cda1742e0a9ae17785403a912c4087faf7
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-27 12:23:49 +0530

    Time: 11 ms (58.33%), Space: 14.3 MB (26.73%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -5,12 +5,25 @@ public:
         int cols = board[0].size();
         queue<pair<int, int>> q;
         vector<vector<int>> vis(rows, vector<int> (cols, 0));
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(board[i][j] == 'O' && (i == 0 || i == rows - 1 || (j == 0 || j == cols - 1))) {
-                    q.emplace(i, j);
-                    vis[i][j] = 1;
-                }
+        for (int j = 0; j < cols; j++) {
+            if (board[0][j] == 'O' && !vis[0][j]) {
+                q.emplace(0, j);
+                vis[0][j] = 1;
+            }
+            if (board[rows - 1][j] == 'O' && !vis[rows - 1][j]) {
+                q.emplace(rows - 1, j);
+                vis[rows - 1][j] = 1;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O' && !vis[i][0]) {
+                q.emplace(i, 0);
+                vis[i][0] = 1;
+            }
+            if (board[i][cols - 1] == 'O' && !vis[i][cols - 1]) {
+                q.emplace(i, cols - 1);
+                vis[i][cols - 1] = 1;
             }
         }
         int dir[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
