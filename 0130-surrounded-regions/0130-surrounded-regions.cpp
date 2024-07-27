5ead20572c176661e86adb0c8243fa83e49450c3
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-07-27 12:28:20 +0530

    Time: 0 ms (100.00%), Space: 14 MB (50.80%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,31 +1,22 @@
 class Solution {
+    void dfs(vector<vector<char>>& board, vector<vector<int>>& vis, int& rows, int& cols, int row, int col) {
+        if(row >= 0 and col >= 0 and row < rows and col < cols and board[row][col] == 'O' and vis[row][col] == 0) {
+            vis[row][col] = 1;
+            dfs(board, vis, rows, cols, row - 1, col);
+            dfs(board, vis, rows, cols, row, col - 1);
+            dfs(board, vis, rows, cols, row, col + 1);
+            dfs(board, vis, rows, cols, row + 1, col);
+        }
+    }
 public:
     void solve(vector<vector<char>>& board) {
         int rows = board.size();
         int cols = board[0].size();
-        queue<pair<int, int>> q;
         vector<vector<int>> vis(rows, vector<int> (cols, 0));
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(board[i][j] == 'O' && (i == 0 || i == rows - 1 || (j == 0 || j == cols - 1))) {
-                    q.emplace(i, j);
-                    vis[i][j] = 1;
-                }
-            }
-        }
-        int dir[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-        while(q.empty() == false) {
-            int size = q.size();
-            while(size--) {
-                auto [row, col] = q.front();
-                q.pop();
-                for(auto &d:dir) {
-                    int nrow = d[0] + row;
-                    int ncol = d[1] + col;
-                    if(nrow >= 0 and ncol >= 0 and nrow < rows and ncol < cols and board[nrow][ncol] == 'O' and vis[nrow][ncol] == 0) {
-                        vis[nrow][ncol] = 1;
-                        q.emplace(nrow, ncol);
-                    }
+                    dfs(board, vis, rows, cols, i, j);
                 }
             }
         }
