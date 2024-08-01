56e96200371c52149097748993186dd512bc8e84
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-01 21:15:05 +0530

    Time: 10 ms (18.05%), Space: 17.7 MB (16.71%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for(auto& detail:details) {
+            if(stoi(detail.substr(11, 2)) > 60) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
