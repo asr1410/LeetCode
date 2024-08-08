9a120b9883ccb765ebe037e8e6b47afd2484f5b1
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-08 22:23:29 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (38.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int helper(int n, vector<int>& dp) {
+        if(n <= 0) {
+            return n == 0 ? 1 : 0;
+        }
         if(dp[n] != -1) {
             return dp[n];
         }
         return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
     }
-
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
-        dp[0] = 1, dp[1] = 2;
-        return helper(n - 1, dp);
+        return helper(n, dp);
     }
 };
