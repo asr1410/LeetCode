a80978b09058703934444da923fab8d0d6490be1
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-11 12:44:40 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (11.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,37 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
 class Solution {
 public:
-    int helper(vector<vector<int>>& dp, int& m, int& n, int i, int j) {
-        if(i >= m or j >= n) {
-            return 0;
-        } else if(i == m - 1 and j == n - 1) {
-            return 1;
-        } else if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        return dp[i][j] = helper(dp, m, n, i + 1, j) + helper(dp, m, n, i, j + 1);
-    }
     int uniquePaths(int m, int n) {
-        int ans = 0;
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return helper(dp, m, n, 0, 0);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+        
+        vector<int> directions = {1, 0, 0, 1};
+        
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        
+        while (!q.empty()) {
+            auto [i, j] = q.front();
+            q.pop();
+            
+            for (int d = 0; d < 2; ++d) {
+                int ni = i + directions[2 * d];
+                int nj = j + directions[2 * d + 1];
+                
+                if (ni < m && nj < n) {
+                    dp[ni][nj] += dp[i][j];
+                    
+                    if (dp[ni][nj] == dp[i][j]) {
+                        q.push({ni, nj});
+                    }
+                }
+            }
+        }
+        
+        return dp[m - 1][n - 1];
     }
 };
