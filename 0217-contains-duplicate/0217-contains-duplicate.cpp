7858082031d9d02d1221b9ccec7afa768b3ad15d
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-15 00:56:58 +0530

    Time: 107 ms (27.87%), Space: 73.4 MB (36.34%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
       unordered_set<int> uset;
-      for(auto num : nums) {
+      for(auto &&num : nums) {
         if(uset.find(num) != uset.end()) {
           return true;
         }
