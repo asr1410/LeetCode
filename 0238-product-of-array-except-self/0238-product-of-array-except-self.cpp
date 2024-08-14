70fbab5970ed31abde431866c274a2dd82c774e9
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-15 01:16:24 +0530

    Time: 23 ms (83.06%), Space: 38.5 MB (59.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-      int zcount = 0, n = nums.size(), zindex;
+      int zcount = 0, n = nums.size(), zindex = 0, mul = 1;
       for(int i = 0; i < n; i++) {
-        if(nums[i] == 0) {
-          zcount++,
-          zindex = i;
-        }
+        zcount += nums[i] == 0;
+        zindex = nums[i] == 0 ? i : zindex;
+        mul *= nums[i] == 0 ? 1 : nums[i];
         if(zcount > 1) {
           return vector<int> (n, 0);
-        }
-      }
-      int mul = 1;
-      for(int i = 0; i < n; i++) {
-        if(nums[i] != 0) {
-          mul *= nums[i];
         }
       }
       if(zcount == 1) {
