540601e3371635e53684c201e9af75131083d773
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-20 00:06:05 +0530

    Time: 27 ms (18.08%), Space: 157.1 MB (10.43%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int helper(int num, int copy, int target) {
+    int helper(int num, int copy, int target, vector<vector<int>>& dp) {
         if(num == target) {
             return 0;
         }
-        int cpmove = num + num <= target ? 2 + helper(num + num, num, target) : 1e9;
-        int pmove = num + copy <= target ? 1 + helper(num + copy, copy, target) : 1e9;
-        return min(cpmove, pmove);
+        if(dp[num][copy] != -1) {
+            return dp[num][copy];
+        }
+        int cpmove = num + num <= target ? 2 + helper(num + num, num, target, dp) : 1e9;
+        int pmove = num + copy <= target ? 1 + helper(num + copy, copy, target, dp) : 1e9;
+        return dp[num][copy] = min(cpmove, pmove);
     }
     int minSteps(int target) {
         if(target == 1) {
             return 0;
         }
+        vector<vector<int>> dp(target + 1, vector<int> (target + 1, -1));
         int num = 1, copy = 1;
-        return helper(num, copy, target) + 1;
+        return helper(num, copy, target, dp) + 1;
     }
 };
