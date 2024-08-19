26758a898a8c6e1e43bbf253dab2ae4d2c8dc352
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-20 00:08:10 +0530

    Time: 28 ms (16.71%), Space: 157.1 MB (9.34%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,21 +1,27 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
 class Solution {
 public:
     int helper(int num, int copy, int target, vector<vector<int>>& dp) {
-        if(num == target) {
-            return 0;
-        }
-        if(dp[num][copy] != -1) {
-            return dp[num][copy];
-        }
-        int cpmove = num + num <= target ? 2 + helper(num + num, num, target, dp) : 1e9;
-        int pmove = num + copy <= target ? 1 + helper(num + copy, copy, target, dp) : 1e9;
+        if (num == target) return 0;
+        if (num > target) return 1e9;
+        if (dp[num][copy] != -1) return dp[num][copy];
+
+        int cpmove = (num + num <= target) ? 2 + helper(num + num, num, target, dp) : 1e9;
+        int pmove = (num + copy <= target) ? 1 + helper(num + copy, copy, target, dp) : 1e9;
+
         return dp[num][copy] = min(cpmove, pmove);
     }
+
     int minSteps(int target) {
-        if(target == 1) {
-            return 0;
-        }
-        vector<vector<int>> dp(target + 1, vector<int> (target + 1, -1));
+        if (target == 1) return 0;
+        
+        vector<vector<int>> dp(target + 1, vector<int>(target + 1, -1));
+        
         int num = 1, copy = 1;
         return helper(num, copy, target, dp) + 1;
     }
