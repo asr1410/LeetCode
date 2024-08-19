463afec35a8be963680397c9649a6eb3baef6434
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-20 00:13:34 +0530

    Time: 3 ms (69.17%), Space: 7.5 MB (55.97%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,19 +1,15 @@
 class Solution {
-    int n;
-
 public:
     int minSteps(int n) {
-        if (n == 1) return 0;
-        this->n = n;
-        return 1 + minStepsHelper(1, 1);
-    }
-
-private:
-    int minStepsHelper(int currLen, int pasteLen) {
-        if (currLen == n) return 0;
-        if (currLen > n) return 1000;
-        int opt1 = 2 + minStepsHelper(currLen * 2, currLen);
-        int opt2 = 1 + minStepsHelper(currLen + pasteLen, pasteLen);
-        return min(opt1, opt2);
+        int ans = 0;
+        int d = 2;
+        while (n > 1) {
+            while (n % d == 0) {
+                ans += d;
+                n /= d;
+            }
+            d++;
+        }
+        return ans;
     }
 };
