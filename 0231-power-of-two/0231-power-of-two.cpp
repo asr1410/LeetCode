f8770537d5a7488ba5c416b66d122258507bb5e2
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-23 21:44:19 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (19.64%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) {
+        if(n == 0 or n == INT_MIN) {
             return false;
         }
         return (n & (n - 1)) == 0;
