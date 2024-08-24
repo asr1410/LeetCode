d472cdf26150f87bcfb67b935fb5b6b102f669a5
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-24 19:34:10 +0530

    Time: 193 ms (84.16%), Space: 101.9 MB (73.62%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    vector<int> fen;
-    int size;
-    int query(int idx) {
-        int sum = 0;
-        while(idx > 0) {
-            sum += fen[idx];
-            idx -= (idx & -idx);
+    vector<int> seg;
+
+    void update(int idx, int low, int high, int pos) {
+        if (low == high) {
+            seg[idx]++;
+            return;
         }
-        return sum;
-    }
-    void update(int idx) {
-        while(idx < size) {
-            fen[idx] += 1;
-            idx += (idx & -idx);
+        int mid = (low + high) >> 1;
+        if (pos <= mid) {
+            update(2 * idx + 1, low, mid, pos);
+        } else {
+            update(2 * idx + 2, mid + 1, high, pos);
         }
+        seg[idx] = seg[2 * idx + 1] + seg[2 * idx + 2];
     }
+
+    int query(int idx, int low, int high, int l, int r) {
+        if (r < low || l > high) {
+            return 0;
+        }
+        if (l <= low && high <= r) {
+            return seg[idx];
+        }
+        int mid = (low + high) >> 1;
+        return query(2 * idx + 1, low, mid, l, r) + query(2 * idx + 2, mid + 1, high, l, r);
+    }
+
     vector<int> countSmaller(vector<int>& nums) {
-        int mn = abs(*min_element(nums.begin(), nums.end()));
-        int mx = *max_element(nums.begin(), nums.end());
-        size = mx + mn + 2;
-        fen.resize(size, 0);
-        vector<int> ans(nums.size());
-        for(int i = nums.size() - 1; i >= 0; i--) {
-            ans[i] = query(nums[i] + mn);
-            update(nums[i] + mn + 1);
+        int minValue = *min_element(nums.begin(), nums.end());
+        int maxValue = *max_element(nums.begin(), nums.end());
+        int offset = minValue < 0 ? -minValue : 0;
+        int range = maxValue + offset;
+        seg.resize(4 * (range + 1), 0);
+        vector<int> result(nums.size(), 0);
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            result[i] = query(0, 0, range, 0, nums[i] + offset - 1);
+            update(0, 0, range, nums[i] + offset);
         }
-        return ans;
+
+        return result;
     }
 };
