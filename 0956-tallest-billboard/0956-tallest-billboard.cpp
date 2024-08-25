355e80aeecd2352fe4d795c5c282403a75a3ec08
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-26 01:34:53 +0530

    Time: 10 ms (98.20%), Space: 9.5 MB (99.45%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int tallestBillboard(vector<int>& arr) {
-        unordered_map<int, int> dp;
-        dp[0] = 0;
-        
-        for (int num : arr) {
-            unordered_map<int, int> temp = dp;
-            for (auto &[diff, height] : dp) {
-                temp[diff + num] = max(temp[diff + num], height);
-                temp[abs(diff - num)] = max(temp[abs(diff - num)], height + min(num, diff));
-            }
-            dp = temp;
-        }
-        
-        return dp[0];
+  int tallestBillboard(vector<int>& rods) {
+    int sum = 0;
+    for (int rod : rods) {
+      sum += rod;
     }
+    int dp[sum + 1];
+    dp[0] = 0;
+    for (int i = 1; i <= sum; i++) {
+      dp[i] = -1;
+    }
+    for (int rod : rods) {
+      int dpCopy[sum + 1];
+      copy(dp, dp + (sum + 1), dpCopy);
+      for (int i = 0; i <= sum - rod; i++) {
+        if (dpCopy[i] < 0) continue;
+        dp[i + rod] = max(dp[i + rod], dpCopy[i]);
+        dp[abs(i - rod)] = max(dp[abs(i - rod)], dpCopy[i] + min(i, rod));
+      }
+    }
+    return dp[0];
+  }
 };
