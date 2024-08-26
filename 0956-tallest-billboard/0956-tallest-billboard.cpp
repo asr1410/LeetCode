1a93ab65dbd52f0640378b75d44f3a944b0926ea
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-26 10:38:27 +0530

    Time: 42 ms (72.54%), Space: 20.5 MB (64.63%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-  int tallestBillboard(vector<int>& rods) {
-    int sum = 0;
-    for (int rod : rods) {
-      sum += rod;
+    int dfs(vector<int>& rs, int i, int s1, int s2, int m_sum, vector<vector<int>> &m) {
+        if (s1 > m_sum || s2 > m_sum) return -1;
+        if (i >= rs.size()) return s1 == s2 ? s1 : -1;
+        if (m[i][abs(s1 - s2)] == -2) {
+            m[i][abs(s1 - s2)] = max(-1, max({dfs(rs, i + 1, s1, s2, m_sum, m), dfs(rs, i + 1, s1 + rs[i], s2, m_sum, m), dfs(rs, i + 1, s1, s2 + rs[i], m_sum, m)}) - max(s1, s2));
+        }
+        return m[i][abs(s1 - s2)] + (m[i][abs(s1 - s2)] == -1 ? 0 : max(s1, s2));
     }
-    int dp[sum + 1];
-    memset(dp, -1, sizeof dp);
-    dp[0] = 0;
-    for (int rod : rods) {
-      int dpCopy[sum + 1];
-      copy(dp, dp + (sum + 1), dpCopy);
-      for (int i = 0; i <= sum - rod; i++) {
-        if (dpCopy[i] < 0) continue;
-        dp[i + rod] = max(dp[i + rod], dpCopy[i]);
-        dp[abs(i - rod)] = max(dp[abs(i - rod)], dpCopy[i] + min(i, rod));
-      }
+
+    int tallestBillboard(vector<int>& rods) {
+        int m_sum = accumulate(begin(rods), end(rods), 0) / 2;
+        vector<vector<int>> m(rods.size(), vector<int>(m_sum + 1, -2));
+        return max(0, dfs(rods, 0, 0, 0, m_sum, m));
     }
-    return dp[0];
-  }
 };
