dcb8649e2ce7d56b29a2a63352882040b1419b5d
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-26 11:31:36 +0530

    Time: 28 ms (90.29%), Space: 10.4 MB (87.66%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int dfs(vector<int>& rs, int i, int s1, int s2, int m_sum, vector<vector<int>> &m) {
-        if (s1 > m_sum || s2 > m_sum) return -1;
-        if (i >= rs.size()) return s1 == s2 ? s1 : -1;
-        if (m[i][abs(s1 - s2)] == -2) {
-            m[i][abs(s1 - s2)] = max(-1, max({dfs(rs, i + 1, s1, s2, m_sum, m), dfs(rs, i + 1, s1 + rs[i], s2, m_sum, m), dfs(rs, i + 1, s1, s2 + rs[i], m_sum, m)}) - max(s1, s2));
+    int dp[20][10001];
+    int helper(int i, int diff, vector<int>& rods) {
+        if (i < 0) {
+            return diff == 0 ? 0 : INT_MIN;
         }
-        return m[i][abs(s1 - s2)] + (m[i][abs(s1 - s2)] == -1 ? 0 : max(s1, s2));
+        if (dp[i][diff + 5000] != -1) {
+            return dp[i][diff + 5000];
+        }
+        int ntaken = helper(i - 1, diff, rods);
+        int ftaken = rods[i] + helper(i - 1, diff + rods[i], rods);
+        int staken = rods[i] + helper(i - 1, diff - rods[i], rods);
+        return dp[i][diff + 5000] = max(ntaken, max(ftaken, staken));
     }
-
     int tallestBillboard(vector<int>& rods) {
-        int m_sum = accumulate(begin(rods), end(rods), 0) / 2;
-        vector<vector<int>> m(rods.size(), vector<int>(m_sum + 1, -2));
-        return max(0, dfs(rods, 0, 0, 0, m_sum, m));
+        memset(dp, -1, sizeof dp);
+        int n = rods.size();
+        return helper(n - 1, 0, rods) >> 1;
     }
 };
