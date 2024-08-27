db2af69517b9f1aa49b66d2897d29d0a90588f40
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-27 15:49:02 +0530

    Time: 94 ms (92.41%), Space: 52.1 MB (89.56%) - LeetHub

--- 3266-final-array-state-after-k-multiplication-operations-ii/3266-final-array-state-after-k-multiplication-operations-ii.cpp
+++ 3266-final-array-state-after-k-multiplication-operations-ii/3266-final-array-state-after-k-multiplication-operations-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
-    const int mod = 1e9+7;
+public:
+    int mod = 1e9+7;
     int modpow(long base, int exp) {
         long result = 1;
         while (exp) {
@@ -13,32 +14,42 @@ class Solution {
         }
         return result;
     }
-public:
-    vector<int> getFinalState(vector<int>& nums, int k, int mult) {
-        if(mult == 1)
-            return nums;
-        int n = nums.size();
-        const long mx = *max_element(nums.cbegin(), nums.cend());
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-        for(int i=0;i<n;i++){
-            pq.push(make_pair(nums[i],i));
-        }
-        while (k && 1LL*mult *pq.top().first <= mx) {
-            --k;
-            pq.push(make_pair((mult * pq.top().first)%mod, pq.top().second));  pq.pop();
-        }
-        const long pow = modpow(mult, k / n);
-        while (!pq.empty()) {
-            int val = pq.top().first;
-            int ind = pq.top().second; pq.pop();
 
-            if (k % n) {
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        if(multiplier == 1)
+            return nums;
+        
+        int n = nums.size();
+        const long mx = *max_element(nums.begin(), nums.end());
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+        for(int i=0;i<n;i++) pq.push({nums[i],i}); // nlogn
+
+        while (k && (1LL * multiplier * pq.top().first) <= mx) {
+            k--;
+            int val = (multiplier * pq.top().first) % mod;
+            int ind = pq.top().second;
+            pq.pop();
+            pq.push({val,ind});
+        } // 50*logn
+        
+        const long pow = modpow(multiplier, k / n); // log(10^9) - 50
+
+        while (!pq.empty()) { // n
+            int val = pq.top().first;
+            int ind = pq.top().second; 
+            pq.pop();
+            if (k % n > 0) {
                 --k;
-                nums[ind] = ((1LL*mult * pow)%mod * val)%mod; 
+                nums[ind] = ((1LL * multiplier * pow)%mod * val)%mod; 
             } else {
-                nums[ind] = (1LL*pow * val)%mod;
+                nums[ind] = (1LL * pow * val)%mod;
             }
         }
+
         return nums;
     }   
+    // tc - O(nlogn)
+    // sc - O(n)
 };
