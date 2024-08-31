b0fb284cd8e4739bb274c3e6629085bae47cc7c6
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-31 19:39:33 +0530

    Time: 860 ms (5.06%), Space: 461.9 MB (5.06%) - LeetHub

--- 0732-my-calendar-iii/0732-my-calendar-iii.cpp
+++ 0732-my-calendar-iii/0732-my-calendar-iii.cpp
@@ -1,100 +1,36 @@
-class Node{
+class MyCalendarThree {
 public:
-    Node* left,* right;  // pointers to left and right of our segment tree node
-    int val, lazy; // val equals the maximum value for the range considered under this node, lazy contains the value that's to be used for lazy propogation
-	
-    Node(): left(NULL), right(NULL), val(0), lazy(0) {}
+    unordered_map<int, int> seg;
+    unordered_map<int, int> lazy;
     
-    void extend() { // used to create the left and right nodes of this node if they don't exist
-        if(left==NULL) { // Note that since both the nodes are created together checking only the left or the right's creation ensures wether both of them have been created or not
-            left=new Node();
-            right=new Node();
-        }
-    }
-};
-class Segtree {
-public:
-    Node* root;
-    int n;
-    
-    Segtree(int n): n(n) {
-        root = new Node();
-    }
-    
-    void up(int l,int r, int val) {
-        update(root,1,n,l,r,val);
-    }
-    
-    int get(int l,int r) {
-        return query(root,1,n,l,r);
-    }
-    
-    void update(Node* p,int s,int e,int l,int r,int val) {
-        if(p->lazy) {
-            p->val+=p->lazy;
-            if(s!=e) { 
-                p->extend();
-                p->left->lazy+=p->lazy;
-                p->right->lazy+=p->lazy;
+    MyCalendarThree() {}
+
+    void update(int idx, int low, int high, int l, int r) {
+        if (lazy[idx] != 0) {
+            seg[idx] += lazy[idx];
+            if (low != high) {
+                lazy[2 * idx + 1] += lazy[idx];
+                lazy[2 * idx + 2] += lazy[idx];
             }
-            p->lazy=0;
+            lazy[idx] = 0;
         }
-        
-        if(s>r || e<l) return;
-        
-        if(s>=l && e<=r) {
-            p->val+=val;
-            if(s!=e) {
-                p->extend();
-                p->left->lazy+=val;
-                p->right->lazy+=val;
+        if (l > high || r < low) return;
+        if (l <= low && high <= r) {
+            seg[idx] += 1;
+            if (low != high) {
+                lazy[2 * idx + 1] += 1;
+                lazy[2 * idx + 2] += 1;
             }
             return;
         }
-        
-        int m=(s+e)/2;
-        p->extend();
-        update(p->left,s,m,l,r,val);
-        update(p->right,m+1,e,l,r,val);
-        p->val=max(p->left->val, p->right->val);
+        int mid = (low + high) >> 1;
+        update(2 * idx + 1, low, mid, l, r);
+        update(2 * idx + 2, mid + 1, high, l, r);
+        seg[idx] = max(seg[2 * idx + 1], seg[2 * idx + 2]);
     }
-    
-    int query(Node* p,int s,int e,int l, int r) {
-         if(p->lazy) {
-            p->val+=p->lazy;
-            if(s!=e) { 
-                p->extend();
-                p->left->lazy+=p->lazy;
-                p->right->lazy+=p->lazy;
-            }
-            p->lazy=0;
-        }
-        
-        if(s>r || e < l) {
-			return 0;
-		}
-        
-        if(s>=l && e<=r) {
-            return p->val;
-        }
-        
-        int m=(s+e)/2;
-        p->extend();
-        return max(query(p->left,s,m,l,r),query(p->right,m+1,e,l,r));
-    }
-};
 
-class MyCalendarThree {
-public:
-    int n;
-    Segtree* sg;
-    MyCalendarThree() {
-        n=1e9;
-        sg=new Segtree(n);
-    }
-    
-    int book(int start, int end) {
-        sg->up(start,end-1,1);
-        return sg->get(1,n);
+    int book(int startTime, int endTime) {
+        update(0, 0, 1000000000, startTime, endTime - 1);
+        return seg[0];
     }
 };
