dec1b5581e1e394cbad249c067270225df4df4e2
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-31 15:12:20 +0530

    Time: 11 ms (98.68%), Space: 15.4 MB (78.89%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -1,64 +1,91 @@
-#include <vector>
-#include <algorithm>
-#include <iostream>
-
-using namespace std;
+struct segmentTree{
+    std::vector<int> tree;
+    int N;
+    int length;
+    
+    segmentTree(std::vector<int>& v){
+        N = v.size();
+        length = 4*N;
+        tree.resize(length, INT_MIN);
+        build(v, 1, 0, N-1);
+    }
+    
+    void build(std::vector<int>& v, int x, int left, int right){
+        if (left==right){
+            tree[x] = v[left];
+            return;
+        }
+        int mid = (left+right)/2;
+        build(v, 2*x, left, mid);
+        build(v, 2*x+1, mid+1, right);
+        tree[x] = tree[2*x] + tree[2*x+1];
+        return;
+    }
+    
+    //update tree if v[index]=value
+    void update(int x, int left, int right, int index, int value){
+        if (left==right){
+            tree[x] = value;
+            return;
+        }
+        int mid = (left+right)/2;
+        if (index>mid){
+            update(2*x+1, mid+1, right, index, value);
+        }else{
+            update(2*x, left, mid, index, value);
+        }
+        tree[x] = tree[2*x] + tree[2*x+1];
+        return;
+    }
+    
+    //find the k-th available leaf node
+    int find(int x, int left, int right, int k){
+        if (left==right){
+            return left;
+        }
+        
+        int mid = (left+right)/2;
+        if (k<=tree[2*x]){
+            return find(2*x, left, mid, k);
+        }else{
+            return find(2*x+1, mid+1, right, k-tree[2*x]);
+        }
+    }    
+};
 
 class Solution {
-    class SegmentTree {
-    private:
-        int N;
-        vector<int> node;
-
-    public:
-        SegmentTree(int n) {
-            // N = Round up n to the next highest power of 2
-            N = 1;
-            while (N < n) {
-                N <<= 1;
-            }
-            node.resize((N << 1) - 1, 0);  // total number of nodes in the tree: 2*N-1
-            for (int i = 0; i < n; ++i) {  // leaf nodes: initialize each with 1 available spot
-                node[N - 1 + i] = 1;
-            }
-            for (int i = N - 2; i >= 0; --i) {  // internal nodes: calculate available spots as sum of available spots in child nodes
-                node[i] = node[(i << 1) + 1] + node[(i << 1) + 2];  // left child: 2*i+1, right child: 2*i+2
-            }
-        }
-
-        int insert(int k) {  // return the index of k'th 1 and make it 0
-            int i = 0;  // root node
-            while (i < N - 1) {  // internal nodes
-                node[i]--;  // decrement available spots by 1 as we go down towards leaf node
-                int left = (i << 1) + 1;
-                if (node[left] > k) {  // decide whether to go to left child or to right child based on available spots in them
-                    i = left;
-                } else {
-                    i = left + 1;  // left + 1 means right
-                    k -= node[left];  // if going to right child, reduce k by the no of available spots in left child
-                }
-            }
-            node[i]--;  // this is the target leaf node: make 1 to 0
-            return i - (N - 1);  // convert to the index that caller expects; leaf nodes begin at N-1
-        }
-    };
-
 public:
+    static bool myCompare(std::vector<int>& u, std::vector<int>& v){
+        if (u[0]<v[0]) return true;
+        if (u[0]>v[0]) return false;
+        
+        if (u[1]>v[1]) return true;
+        return false;
+    }
+    
+    void printQ(std::vector<std::vector<int>>& q){
+        for (int i=0; i<q.size(); i++)
+            std::cout<<"("<<q[i][0]<<","<<q[i][1]<<"), ";
+        std::cout<<std::endl;
+    }
+    
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        // Sort people based on height in ascending order, and then by k in descending order
-        sort(people.begin(), people.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
-        });
-
         int n = people.size();
-        SegmentTree st(n);
-        vector<vector<int>> res(n);
+        std::sort(people.begin(), people.end(), myCompare);
+        std::vector<std::vector<int>> result(n, std::vector<int>{0,0});
 
-        for (const auto& p : people) {
-            int index = st.insert(p[1]);
-            res[index] = p;
+        std::vector<int> v(n, 1);
+        segmentTree sgTree = segmentTree(v); 
+        int index;
+        
+        for (int i=0; i<n; i++){
+            index = sgTree.find(1, 0, n-1, people[i][1]+1);    
+            result[index][0] = people[i][0];
+            result[index][1] = people[i][1];
+            //update sgTree
+            sgTree.update(1, 0, n-1, index, 0);
         }
-
-        return res;
+        
+        return result;
     }
 };
