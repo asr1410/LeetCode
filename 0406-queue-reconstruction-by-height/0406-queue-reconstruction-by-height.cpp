363bf88dbe2e6ab3e251caa4238c2e1b73e8fd0d
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-31 17:42:14 +0530

    Time: 6 ms (99.82%), Space: 15.4 MB (78.89%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -1,91 +1,66 @@
-#include <vector>
-#include <algorithm>
-#include <iostream>
-#include <climits>
-
-struct segmentTree {
-    std::vector<int> tree;
-    int N;
-    int length;
-
-    segmentTree(std::vector<int>& v) {
-        N = v.size();
-        length = 4 * N;
-        tree.resize(length, 0);  // Initialize with 0 since we're dealing with counts or sums
-        build(v, 0, 0, N - 1);
+class ST {
+public:
+    vector<int> seg;
+    ST(int n) {
+        seg.resize(4 * n, 0);
     }
-
-    void build(std::vector<int>& v, int idx, int left, int right) {
-        if (left == right) {
-            tree[idx] = v[left];
+    
+    void build(int idx, int low, int high) {
+        if(low == high) {
+            seg[idx] = 1;
             return;
         }
-        int mid = (left + right) / 2;
-        build(v, 2 * idx + 1, left, mid);
-        build(v, 2 * idx + 2, mid + 1, right);
-        tree[idx] = tree[2 * idx + 1] + tree[2 * idx + 2];
+        int mid = (low + high) >> 1;
+        build(2 * idx + 1, low, mid);
+        build(2 * idx + 2, mid + 1, high);
+        seg[idx] = seg[2 * idx + 1] + seg[2 * idx + 2];
     }
-
-    void update(int idx, int left, int right, int position, int value) {
-        if (left == right) {
-            tree[idx] = value;
+    
+    void update(int idx, int low, int high, int pos) {
+        if(low == high) {
+            seg[idx] = 0;
             return;
         }
-        int mid = (left + right) / 2;
-        if (position <= mid) {
-            update(2 * idx + 1, left, mid, position, value);
+        int mid = (low + high) >> 1;
+        if(pos <= mid) {
+            update(2 * idx + 1, low, mid, pos);
         } else {
-            update(2 * idx + 2, mid + 1, right, position, value);
+            update(2 * idx + 2, mid + 1, high, pos);
         }
-        tree[idx] = tree[2 * idx + 1] + tree[2 * idx + 2];
+        seg[idx] = seg[2 * idx + 1] + seg[2 * idx + 2];
     }
-
-    int find(int idx, int left, int right, int k) {
-        if (left == right) {
-            return left;
+    
+    int query(int idx , int low, int high, int pos) {
+        if(low == high) {
+            return low;
         }
-
-        int mid = (left + right) / 2;
-        if (k <= tree[2 * idx + 1]) {
-            return find(2 * idx + 1, left, mid, k);
-        } else {
-            return find(2 * idx + 2, mid + 1, right, k - tree[2 * idx + 1]);
+        int mid = (low + high) >> 1;
+        if(pos <= seg[2 * idx + 1]) {
+            return query(2 * idx + 1, low, mid, pos);
         }
+        return query(2 * idx + 2, mid + 1, high, pos - seg[2 * idx + 1]);
     }
 };
 
 class Solution {
 public:
-    static bool myCompare(std::vector<int>& u, std::vector<int>& v) {
+    static bool myCompare(vector<int>& u, vector<int>& v) {
         if (u[0] < v[0]) return true;
         if (u[0] > v[0]) return false;
-
         return u[1] > v[1];
     }
-
-    void printQ(std::vector<std::vector<int>>& q) {
-        for (int i = 0; i < q.size(); i++)
-            std::cout << "(" << q[i][0] << "," << q[i][1] << "), ";
-        std::cout << std::endl;
-    }
-
-    std::vector<std::vector<int>> reconstructQueue(std::vector<std::vector<int>>& people) {
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         int n = people.size();
-        std::sort(people.begin(), people.end(), myCompare);
-        std::vector<std::vector<int>> result(n, std::vector<int>{0, 0});
-
-        std::vector<int> v(n, 1);
-        segmentTree sgTree(v); 
-        int index;
-
-        for (int i = 0; i < n; i++) {
-            index = sgTree.find(0, 0, n - 1, people[i][1] + 1);
-            result[index][0] = people[i][0];
-            result[index][1] = people[i][1];
-            // Update segment tree
-            sgTree.update(0, 0, n - 1, index, 0);
+        sort(people.begin(), people.end(), myCompare);
+        ST st(n);
+        st.build(0, 0, n - 1);
+        vector<vector<int>> ans(n, vector<int>{0, 0});
+        for(int i = 0; i < n; i++) {
+            int idx = st.query(0, 0, n - 1, people[i][1] + 1);
+            ans[idx][0] = people[i][0];
+            ans[idx][1] = people[i][1];
+            st.update(0, 0, n - 1, idx);
         }
-
-        return result;
+        return ans;
     }
 };
