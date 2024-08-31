136a9bccf063b93d81e0d9f7dd0666daf496dcaa
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-31 19:16:53 +0530

    Time: 604 ms (6.40%), Space: 308.2 MB (5.06%) - LeetHub

--- 0732-my-calendar-iii/0732-my-calendar-iii.cpp
+++ 0732-my-calendar-iii/0732-my-calendar-iii.cpp
@@ -1,28 +1,28 @@
 class MyCalendarThree {
-public:
-    map<int, int> events;
+private:
+    unordered_map<int, int> maxMap;  // To store the maximum overlap at each segment
+    unordered_map<int, int> lazyMap; // To store the lazy propagation updates
 
-    MyCalendarThree() {
-        
-    }
-    
-    int book(int st, int et) {
-        events[st]++;
-        events[et]--;
-        
-        int currentOverlap = 0;
-        int maxOverlap = 0;
-        
-        for (const auto& event : events) {
-            currentOverlap += event.second;
-            maxOverlap = max(maxOverlap, currentOverlap);
+    void update(int l, int r, int left, int right, int id) {
+        if (right <= l || r <= left) return;
+        if (l <= left && right <= r) {
+            maxMap[id] += 1;
+            lazyMap[id] += 1;
+        } else {
+            int md = (left + right) >> 1;
+            int lc = id * 2;
+            int rc = id * 2 + 1;
+            update(l, r, left, md, lc);
+            update(l, r, md, right, rc);
+            maxMap[id] = lazyMap[id] + std::max(maxMap[lc], maxMap[rc]);
         }
-        return maxOverlap;
+    }
+
+public:
+    MyCalendarThree() {}
+
+    int book(int start, int end) {
+        update(start, end, 0, 1e9, 1);
+        return maxMap[1];
     }
 };
-
-/**
- * Your MyCalendarThree object will be instantiated and called as such:
- * MyCalendarThree* obj = new MyCalendarThree();
- * int param_1 = obj->book(startTime,endTime);
- */
