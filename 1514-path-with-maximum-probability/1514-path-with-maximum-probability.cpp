d9db7913e15084ed3a6483e7c1d368f31fef2764
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-31 14:03:49 +0530

    Time: 90 ms (86.10%), Space: 53.1 MB (94.78%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,51 +1,28 @@
-#include <vector>
-#include <queue>
-#include <utility>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<int, double>>> adj(n);
-        for (int i = 0; i < edges.size(); ++i) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            double prob = succProb[i];
-            adj[u].push_back({v, prob});
-            adj[v].push_back({u, prob});
-        }
-
-        vector<double> maxProb(n, 0.0);
-        maxProb[start] = 1.0;
-        priority_queue<pair<double, int>> pq;
-        pq.push({1.0, start});
-
-        while (!pq.empty()) {
-            double curProb = pq.top().first;
-            int u = pq.top().second;
-            pq.pop();
-
-            if (u == end) {
-                return curProb;
-            }
-
-            if (curProb < maxProb[u]) {
-                continue;
-            }
-
-            for (const auto& neighbor : adj[u]) {
-                int v = neighbor.first;
-                double edgeProb = neighbor.second;
-                double newProb = curProb * edgeProb;
-                if (newProb > maxProb[v]) {
-                    maxProb[v] = newProb;
-                    pq.push({newProb, v});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& sp, int sn, int en) {
+        vector<double> mp(n, 0);
+        mp[sn] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            bool updated = false;
+            for(int j = 0; j < edges.size(); j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = sp[j];
+                if(mp[u] * prob > mp[v]) {
+                    mp[v] = mp[u] * prob;
+                    updated = true;
+                }
+                if(mp[v] * prob > mp[u]) {
+                    mp[u] = mp[v] * prob;
+                    updated = true;
                 }
             }
+            if(updated == false) {
+                break;
+            }
         }
-
-        return 0.0;
+        return mp[en];
     }
 };
