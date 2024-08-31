b7c83c56c5518dc56dc7cf9b5d8550ffb02799d3
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-08-31 19:21:50 +0530

    Time: 287 ms (13.10%), Space: 281.9 MB (6.40%) - LeetHub

--- 0732-my-calendar-iii/0732-my-calendar-iii.cpp
+++ 0732-my-calendar-iii/0732-my-calendar-iii.cpp
@@ -1,28 +1,100 @@
-class MyCalendarThree {
-private:
-    unordered_map<int, int> maxMap;  // To store the maximum overlap at each segment
-    unordered_map<int, int> lazyMap; // To store the lazy propagation updates
-
-    void update(int l, int r, int left, int right, int id) {
-        if (right <= l || r <= left) return;
-        if (l <= left && right <= r) {
-            maxMap[id] += 1;
-            lazyMap[id] += 1;
-        } else {
-            int md = (left + right) >> 1;
-            int lc = id * 2;
-            int rc = id * 2 + 1;
-            update(l, r, left, md, lc);
-            update(l, r, md, right, rc);
-            maxMap[id] = lazyMap[id] + std::max(maxMap[lc], maxMap[rc]);
+class Node{
+public:
+    Node* left,* right;  // pointers to left and right of our segment tree node
+    int val, lazy; // val equals the maximum value for the range considered under this node, lazy contains the value that's to be used for lazy propogation
+	
+    Node(): left(NULL), right(NULL), val(0), lazy(0) {}
+    
+    void extend() { // used to create the left and right nodes of this node if they don't exist
+        if(left==NULL) { // Note that since both the nodes are created together checking only the left or the right's creation ensures wether both of them have been created or not
+            left=new Node();
+            right=new Node();
         }
     }
-
+};
+class Segtree {
 public:
-    MyCalendarThree() {}
+    Node* root;
+    int n;
+    
+    Segtree(int n): n(n) {
+        root = new Node();
+    }
+    
+    void up(int l,int r, int val) {
+        update(root,1,n,l,r,val);
+    }
+    
+    int get(int l,int r) {
+        return query(root,1,n,l,r);
+    }
+    
+    void update(Node* p,int s,int e,int l,int r,int val) {
+        if(p->lazy) {
+            p->val+=p->lazy;
+            if(s!=e) { 
+                p->extend();
+                p->left->lazy+=p->lazy;
+                p->right->lazy+=p->lazy;
+            }
+            p->lazy=0;
+        }
+        
+        if(s>r || e<l) return;
+        
+        if(s>=l && e<=r) {
+            p->val+=val;
+            if(s!=e) {
+                p->extend();
+                p->left->lazy+=val;
+                p->right->lazy+=val;
+            }
+            return;
+        }
+        
+        int m=(s+e)/2;
+        p->extend();
+        update(p->left,s,m,l,r,val);
+        update(p->right,m+1,e,l,r,val);
+        p->val=max(p->left->val, p->right->val);
+    }
+    
+    int query(Node* p,int s,int e,int l, int r) {
+         if(p->lazy) {
+            p->val+=p->lazy;
+            if(s!=e) { 
+                p->extend();
+                p->left->lazy+=p->lazy;
+                p->right->lazy+=p->lazy;
+            }
+            p->lazy=0;
+        }
+        
+        if(s>r || e < l) {
+			return 0;
+		}
+        
+        if(s>=l && e<=r) {
+            return p->val;
+        }
+        
+        int m=(s+e)/2;
+        p->extend();
+        return max(query(p->left,s,m,l,r),query(p->right,m+1,e,l,r));
+    }
+};
 
+class MyCalendarThree {
+public:
+    int n;
+    Segtree* sg;
+    MyCalendarThree() {
+        n=1e9;
+        sg=new Segtree(n);
+    }
+    
     int book(int start, int end) {
-        update(start, end, 0, 1e9, 1);
-        return maxMap[1];
+        sg->up(start,end-1,1);
+        return sg->get(1,n);
     }
 };
