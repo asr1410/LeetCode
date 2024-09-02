975c501854e21f9c1e209c8a835c084a2e811e7f
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-02 10:30:23 +0530

    Time: 828 ms (5.88%), Space: 330.9 MB (5.88%) - LeetHub

--- 3276-select-cells-in-grid-with-maximum-score/3276-select-cells-in-grid-with-maximum-score.cpp
+++ 3276-select-cells-in-grid-with-maximum-score/3276-select-cells-in-grid-with-maximum-score.cpp
@@ -1,53 +1,33 @@
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    int recur(const vector<vector<int>>& values, int idx, int mask_row, unordered_map<long long, int>& dp) {
-        int n = values.size();
-        if (idx == n)
+    int helper(int n, int mask, vector<pair<int, int>>& vp,  map<pair<int, int>, int>& mp) {
+        if(n < 0) {
             return 0;
-        
-        long long state = ((long long)idx << 10) | mask_row;
-        if (dp.find(state) != dp.end())
-            return dp[state];
-        
-        int ans = 0;
-        int row = values[idx][1];
-        if ((1 << row) & mask_row) {
-            ans = recur(values, idx + 1, mask_row, dp);
-        } else {
-            int j = idx;
-            while (j < n && values[idx][0] == values[j][0])
-                j++;
-            
-            int ans1 = values[idx][0] + recur(values, j, mask_row | (1 << row), dp);
-            int ans2 = recur(values, idx + 1, mask_row, dp);
-            
-            ans = max(ans1, ans2);
         }
-            
-        dp[state] = ans;
-        return ans;
+        if(mp.find({n, mask})!= mp.end())
+            return mp[{n, mask}];
+        int ans = 0;
+        if((1 << vp[n].second) & mask) {
+            ans = helper(n - 1, mask, vp, mp);
+        } else {
+            int j = n;
+            while(j >= 0 and vp[n].first == vp[j].first) {
+                j--;
+            }
+            ans = max(vp[n].first + helper(j, (1 << vp[n].second) | mask, vp, mp), helper(n - 1, mask, vp, mp));
+        }
+        return mp[make_pair(n, mask)] = ans;
     }
-    
     int maxScore(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> values;
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                values.push_back({grid[i][j], i});
+        vector<pair<int, int>> vp;
+        int rows = grid.size(), cols = grid[0].size();
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                vp.push_back(make_pair(grid[i][j], i));
             }
         }
-        
-        sort(values.begin(), values.end(), greater<vector<int>>());
-        
-        unordered_map<long long, int> dp;
-        
-        return recur(values, 0, 0, dp);
+        sort(vp.begin(), vp.end());
+        map<pair<int, int>, int> mp;
+        return helper(rows * cols - 1, 0, vp, mp);
     }
 };
