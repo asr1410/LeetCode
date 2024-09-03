4fe619412dfde9cf7a4586a6367328b610a0d0d2
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-04 00:26:45 +0530

    Time: 206 ms (55.20%), Space: 139.7 MB (90.42%) - LeetHub

--- 3273-minimum-amount-of-damage-dealt-to-bob/3273-minimum-amount-of-damage-dealt-to-bob.cpp
+++ 3273-minimum-amount-of-damage-dealt-to-bob/3273-minimum-amount-of-damage-dealt-to-bob.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    long long minDamage(int power, vector<int>& damage, vector<int>& health) {
-        long long ans = 0, sum = 0;
-        priority_queue<pair<double, int>> maxpq;
-        for(int i = 0; i < damage.size(); i++) {
-            int t = health[i] / power + (health[i] % power != 0);
-            double dps = (1.0 * damage[i]) / (1.0 * t);
-            maxpq.emplace(dps, i);
-            sum += damage[i];
+        long long minDamage(int power, vector<int>& D, vector<int>& H) {
+        vector<int> A(D.size());
+        for (int i = 0; i < D.size(); ++i) {
+            H[i] = (H[i] + power - 1) / power;
+            A[i] = i;
         }
-        while(maxpq.empty() == false) {
-            int i = maxpq.top().second;
-            maxpq.pop();
-            int t = health[i] / power + (health[i] % power != 0);
-            ans += sum * t;
-            sum -= damage[i];
+        sort(A.begin(), A.end(), [&](int i, int j) { 
+            return H[i] * D[j] < H[j] * D[i];
+        });
+
+        long long res = 0, t = 0;
+        for (int i : A) {
+            res += D[i] * (t += H[i]);
         }
-        return ans;
+        return res;
     }
 };
