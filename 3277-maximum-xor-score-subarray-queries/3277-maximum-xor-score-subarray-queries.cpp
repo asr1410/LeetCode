8bde8b3162b0b4a1a88fbcb822cf918b41b3dff3
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-04 01:20:52 +0530

    Time: 824 ms (20.67%), Space: 450.7 MB (15.91%) - LeetHub

--- 3277-maximum-xor-score-subarray-queries/3277-maximum-xor-score-subarray-queries.cpp
+++ 3277-maximum-xor-score-subarray-queries/3277-maximum-xor-score-subarray-queries.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-  pair<int,int> solve(int i,int j, vector<vector<pair<int,int>>>&dp,vector<int>&nums)
-  {
- if(i==j)return {nums[i],nums[i]};
-            if(dp[i][j]!=make_pair(-1,-1))return dp[i][j];
-            pair<int,int> left=solve(i,j-1,dp,nums);
-            pair<int,int> down=solve(i+1,j,dp,nums);
-            int window=left.second^down.second;
-            int maxi=max({window,left.first,down.first});
-   return dp[i][j]={maxi,window};
-  }
-    vector<int> maximumSubarrayXor(vector<int>& nums, vector<vector<int>>& queries) {     
-        int n=nums.size();
-        vector<vector<pair<int,int>>>dp(n,vector<pair<int,int>>(n,{-1,-1}));
+    pair<int, int> solve(int i, int j, vector<vector<pair<int, int>>>& dp, vector<int>& nums) {
+        if (i == j) return {nums[i], nums[i]};
+        if (dp[i][j] != make_pair(-1, -1)) return dp[i][j];
+        
+        pair<int, int> left = solve(i, j - 1, dp, nums);
+        pair<int, int> down = solve(i + 1, j, dp, nums);
+        
+        int window = left.second ^ down.second;
+        int maxi = max({window, left.first, down.first});
+        
+        return dp[i][j] = {maxi, window};
+    }
 
-     vector<int>res;
-     for(auto&i:queries)
-     {
-          res.push_back(solve(i[0],i[1],dp,nums).first);
-     }
-   
-     return res;
- }
+    vector<int> maximumSubarrayXor(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<vector<pair<int, int>>> dp(n, vector<pair<int, int>>(n, {-1, -1}));
+        
+        vector<int> res;
+        for (const auto& query : queries) {
+            res.push_back(solve(query[0], query[1], dp, nums).first);
+        }
+        
+        return res;
+    }
 };
