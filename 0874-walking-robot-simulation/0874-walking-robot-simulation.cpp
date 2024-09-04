c4b9dfee2440266be931f9cf8909e0e7f06d5802
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-04 23:17:24 +0530

    Time: 70 ms (71.27%), Space: 39.8 MB (47.51%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,57 +1,57 @@
 class Solution {
 public:
     void update(char& dir, int c) {
-        if (dir == 'n') {
-            if (c == -1) {
+        if(dir == 'n') {
+            if(c == -1) {
                 dir = 'e';
             } else {
                 dir = 'w';
             }
-        } else if (dir == 'e') {
-            if (c == -1) {
+        } else if(dir == 'e') {
+            if(c == -1) {
                 dir = 's';
             } else {
                 dir = 'n';
             }
-        } else if (dir == 'w') {
-            if (c == -1) {
+        } else if(dir == 'w') {
+            if(c == -1) {
                 dir = 'n';
             } else {
                 dir = 's';
             }
         } else {
-            if (c == -1) {
+            if(c == -1) {
                 dir = 'w';
             } else {
                 dir = 'e';
             }
         }
     }
-
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
         set<pair<int, int>> uset;
-        for (auto& o : obstacles) {
-            uset.insert({o[0], o[1]});
+        for(auto o : obstacles) {
+            uset.emplace(o[0], o[1]);
         }
-        int x = 0, y = 0, ans = 0;
+        int x = 0, y = 0;
         char dir = 'n';
-        for (int c : commands) {
-            if (c > 0) {
+        int ans = 0;
+        for(int c : commands) {
+            if(c > 0) {
                 int n = c;
-                if (dir == 'e') {
-                    while (n && uset.find({x + 1, y}) == uset.end()) {
+                if(dir == 'e') {
+                    while(n and uset.find(make_pair(x + 1, y)) == uset.end()) {
                         x++, n--;
                     }
-                } else if (dir == 'w') {
-                    while (n && uset.find({x - 1, y}) == uset.end()) {
+                } else if(dir == 'w') {
+                    while(n and uset.find(make_pair(x - 1, y)) == uset.end()) {
                         x--, n--;
                     }
-                } else if (dir == 'n') {
-                    while (n && uset.find({x, y + 1}) == uset.end()) {
+                } else if(dir == 'n') {
+                    while(n and uset.find(make_pair(x, y + 1)) == uset.end()) {
                         y++, n--;
                     }
                 } else {
-                    while (n && uset.find({x, y - 1}) == uset.end()) {
+                    while(n and uset.find(make_pair(x, y - 1)) == uset.end()) {
                         y--, n--;
                     }
                 }
