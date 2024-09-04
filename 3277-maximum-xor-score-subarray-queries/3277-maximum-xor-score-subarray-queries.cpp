d23ec6ebf64612b76730d1ddae637c3d87b17e7f
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-04 19:31:35 +0530

    Time: 675 ms (44.27%), Space: 418.4 MB (41.04%) - LeetHub

--- 3277-maximum-xor-score-subarray-queries/3277-maximum-xor-score-subarray-queries.cpp
+++ 3277-maximum-xor-score-subarray-queries/3277-maximum-xor-score-subarray-queries.cpp
@@ -3,23 +3,23 @@ public:
     vector<int> maximumSubarrayXor(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int> (n, 0));
-        
+        int t, r;
         for(int i = 0; i < n; i++) {
             dp[i][i] = nums[i];
         }
         
         for(int i = 2; i <= n; i++) {
-            int t = n + 1 - i;
+            t = n + 1 - i;
             for(int l = 0; l < t; l++) {
-                int r = l + i - 1;
+                r = l + i - 1;
                 dp[l][r] = dp[l][r - 1] ^ dp[l + 1][r];
             }
         }
         
         for(int i = 2; i <= n; i++) {
-            int t = n + 1 - i;
+            t = n + 1 - i;
             for(int l = 0; l < t; l++) {
-                int r = l + i - 1;
+                r = l + i - 1;
                 dp[l][r] = max(dp[l][r], max(dp[l][r - 1], dp[l + 1][r]));
             }
         }
