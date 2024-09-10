01bd23293d70bfad4edcd501259ce2862f4d895e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-10 17:29:48 +0530

    Time: 211 ms (50.87%), Space: 109.7 MB (35.18%) - LeetHub

--- 3281-maximize-score-of-numbers-in-ranges/3281-maximize-score-of-numbers-in-ranges.cpp
+++ 3281-maximize-score-of-numbers-in-ranges/3281-maximize-score-of-numbers-in-ranges.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     int maxPossibleScore(vector<int>& start, int d) {
         sort(begin(start), end(start));
-        long long n = start.size(), l = 1e9, r = -1e9, ans = 0;
-        for(int i = 1; i < n; i++) {
-            long long t = start[i] - start[i - 1];
-            l = min(l, t);
-            r = max(r, t + d);
-        }
-        while(l <= r) {
-            long gap = (l + r) >> 1;
-            bool possible = true;
-            for(long long i = 1, ls = start[0]; i < n; i++) {
-                ls += gap;
-                if(ls <= start[i] + d) {
-                    ls = max(ls, (long long)start[i]);
-                } else {
-                    possible = false;
-                }
+        long long l = 0, r = start[1] + d - start[0];
+        while (l < r) {
+            long long m = (l + r + 1) / 2, point = start[0], i = 1;
+            for (; i < start.size(); ++i) {
+                point = max((long long)start[i], point + m);
+                if (point > start[i] + d)
+                    break;
             }
-            if(possible) {
-                ans = gap;
-                l = gap + 1;
-            } else {
-                r = gap - 1;
-            }
+            if (i == start.size())
+                l = m;
+            else
+                r = m - 1;
         }
-        return ans;
+        return l;
     }
 };
