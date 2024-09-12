a14ff3020baf371818130b579ca32314debee7f5
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-12 16:17:01 +0530

    Time: 236 ms (14.27%), Space: 203.7 MB (5.10%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    long long findMaximumScore(vector<int>& A) {
-        long long res = 0;
-        int ma = 0;
-        for (int a: A) {
-            res += ma;
-            ma = max(ma, a);
+    long long findMaximumScore(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> dp{0};
+        for(int i = 1, mi = 0; i < n; i++) {
+            dp.push_back((i - mi) * 1LL * nums[mi] + dp[mi]);
+            if(nums[i] > nums[mi]) {
+                mi = i;
+            }
         }
-        return res;
+        return dp[n - 1];
     }
 };
