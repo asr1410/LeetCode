ccd829bad9f754486e84babda5a74bbf2ab50717
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-13 10:43:00 +0530

    Time: 67 ms (39.75%), Space: 45 MB (14.22%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        vector<int> xors{0};
-        for(int i = 0; i < arr.size(); i++) {
-            xors.emplace_back(xors[i] ^ arr[i]);
+        int n = arr.size();
+        vector<int> xors(n + 1, 0);
+        for(int i = 0, j = 1; i < n; j++, i++) {
+            xors[j] = xors[i] ^ arr[i];
         }
         vector<int> ans;
         for(auto q : queries) {
