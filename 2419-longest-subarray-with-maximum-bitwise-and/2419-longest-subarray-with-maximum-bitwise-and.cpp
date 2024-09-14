dcd8ea271685829e25ebf5ceb26f09ae6ce30404
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-14 11:30:43 +0530

    Time: 88 ms (90.35%), Space: 84.7 MB (90.79%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -3,17 +3,14 @@ public:
     int longestSubarray(vector<int>& nums) {
         int mx = *max_element(nums.begin(), nums.end());
         int ans = 0;
-        int count = 0, temp = 0;
-        cout << count << endl;
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0, count = 0; i < nums.size(); i++) {
             if(nums[i] == mx) {
-                temp++;
+                count++;
+                ans = max(ans, count);
             } else {
-                ans = max(ans, temp);
-                temp = 0;
+                count = 0;
             }
         }
-        ans = max(ans, temp);
         return ans;
     }
 };
