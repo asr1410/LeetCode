84d6429bbb9ae8e79eb33853529e6cfa43073ed9
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-14 22:57:29 +0530

    Time: 77 ms (44.44%), Space: 29.4 MB (100.00%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    bool helper(int i, int j, int health, const vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if (i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || health < 1) {
+    bool helper(int i, int j, int m, int n, int health, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if(i < 0 or j < 0 or i == m or j == n or health < 1 or grid[i][j] == -1) {
             return false;
         }
-        if (i == 0 && j == 0) {
-            return (grid[i][j] == 1 && health >= 2) || (grid[i][j] == 0 && health >= 1);
+        if(i == m - 1 and j == n - 1) {
+            return (grid[i][j] == 1 and health >= 2) or (grid[i][j] == 0 and health >= 1);
         }
-        if (dp[i][j] >= health) {
+        if(dp[i][j] >= health) {
             return false;
         }
         dp[i][j] = health;
-        health -= grid[i][j];
-        return  helper(i - 1, j, health, grid, dp) ||
-                helper(i, j - 1, health, grid, dp) || 
-                helper(i + 1, j, health, grid, dp) || 
-                helper(i, j + 1, health, grid, dp);
+        int val = grid[i][j];
+        health -= val;
+        grid[i][j] = -1;
+        int top = helper(i - 1, j, m, n, health, grid, dp);
+        int left = helper(i, j - 1, m, n, health, grid, dp);
+        int right = helper(i, j + 1, m, n, health, grid, dp);
+        int down = helper(i + 1, j, m, n, health, grid, dp);
+        health += val;
+        grid[i][j] = val;
+        return top or left or right or down;
     }
-
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return helper(m - 1, n - 1, health, grid, dp);
+        vector<vector<int>> dp(grid.size(), vector<int> (grid[0].size(), -1));
+        return helper(0, 0, grid.size(), grid[0].size(), health, grid, dp);
     }
 };
