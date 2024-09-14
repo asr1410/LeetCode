3ba35aee686cc633268103236ef2a5fd0df66584
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-14 23:34:31 +0530

    Time: 58 ms (44.44%), Space: 34.5 MB (66.67%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        priority_queue<pair<int, pair<int, int>>> pq;
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        pq.push(make_pair(health - grid[0][0], make_pair(0, 0)));
-        vector<vector<int>> dir{{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-        while (!pq.empty()) {
-            auto top = pq.top();
-            int i = top.second.first;
-            int j = top.second.second;
-            int currentHealth = top.first;
-            pq.pop();
-            if (i == m - 1 && j == n - 1 && currentHealth > 0) {
-                return true;
-            }
-            for (auto d : dir) {
-                int ni = i + d[0];
-                int nj = j + d[1];
-                if (ni >= 0 && nj >= 0 && ni < m && nj < n && vis[ni][nj] == 0 && currentHealth > 0) {
-                    vis[ni][nj] = 1;
-                    pq.push(make_pair(currentHealth - grid[ni][nj], make_pair(ni, nj)));
-                }
+bool findSafeWalk(vector<vector<int>>& g, int health) {
+    int m = g.size(), n = g[0].size(), cnt[51][51] = {};
+    priority_queue<array<int, 3>> pq;
+    pq.push({health - g[0][0], 0, 0});
+    while (!pq.empty()) {
+        auto [h, i, j] = pq.top(); pq.pop();
+        if (i == m - 1 && j == n - 1)
+            return true;
+        for (auto [dx, dy] : vector<pair<int, int>>{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}) {
+            int x = i + dx, y = j + dy;
+            if (min(x, y) >= 0 && x < m && y < n && cnt[x][y] < h - g[x][y]) {
+                cnt[x][y] = h - g[x][y];
+                pq.push({h - g[x][y], x, y});
             }
         }
-        return false;
     }
+    return false;
+}
 };
