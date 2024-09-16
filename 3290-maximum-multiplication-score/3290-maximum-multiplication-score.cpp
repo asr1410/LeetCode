72841c23be1b5c358263330c89c532e05120d476
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-16 16:05:10 +0530

    Time: 176 ms (33.33%), Space: 145.5 MB (100.00%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     long long maxScore(vector<int>& a, vector<int>& b) {
         int na = a.size(), nb = b.size();
-        vector<vector<long long>> dp(na + 1, vector<long long>(nb + 1, INT_MIN));
+        vector<long long> curr(nb + 1, INT_MIN), prev(nb + 1, INT_MIN);
         for (int j = 0; j <= nb; j++) {
-            dp[na][j] = 0;
+            prev[j] = 0;
         }
         for (int i = na - 1; i >= 0; i--) {
             for (int j = nb - 1; j >= 0; j--) {
-                long long ntaken = dp[i][j + 1];
-                long long taken = static_cast<long long>(a[i]) * b[j] + dp[i + 1][j + 1];
-                dp[i][j] = max(ntaken, taken);
+                long long ntaken = curr[j + 1];
+                long long taken = static_cast<long long>(a[i]) * b[j] + prev[j + 1];
+                curr[j] = max(ntaken, taken);
             }
+            prev = curr;
         }
 
-        return dp[0][0];
+        return curr[0];
     }
 };
