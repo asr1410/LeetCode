c232333bd7a10e917c288dc8511efc0aeba93061
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-16 11:30:43 +0530

    Time: 8 ms (86.39%), Space: 17.6 MB (77.18%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int findMinDifference(vector<string>& tp) {
-        vector<int> ntp;
-        int minutes = 24 * 60, n = tp.size(), ans = INT_MAX;
-        for(int i = 0; i < n; i++) {
-            int min = 0;
-            stringstream ss(tp[i]);
-            string s;
-            char sep = ':';
-            bool hour = true;
-            while(getline(ss, s, sep)) {
-                if(hour) {
-                    min += stoi(s) * 60;
-                    hour = false;
-                } else {
-                    min += stoi(s);
-                }
-            }
-            ntp.push_back(min);
+    int findMinDifference(vector<string>& timePoints) {
+        vector<int> minutes;
+        int n = timePoints.size();
+        
+        for (const auto& time : timePoints) {
+            int hour = stoi(time.substr(0, 2));
+            int minute = stoi(time.substr(3, 2));
+            minutes.push_back(hour * 60 + minute);
         }
-        sort(ntp.begin(), ntp.end());
-        for(int i = 1; i < n; i++) {
-            ans = min(ans, ntp[i] - ntp[i - 1]);
+        
+        sort(minutes.begin(), minutes.end());
+        
+        int minDiff = INT_MAX;
+        for (int i = 1; i < n; ++i) {
+            minDiff = min(minDiff, minutes[i] - minutes[i - 1]);
         }
-        ans = min(ans, minutes - ntp[n - 1] + ntp[0]);
-        return ans;
+        minDiff = min(minDiff, (24 * 60 - minutes.back() + minutes.front()));
+        
+        return minDiff;
     }
 };
