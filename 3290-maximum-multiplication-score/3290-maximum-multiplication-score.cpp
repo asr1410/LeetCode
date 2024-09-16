c97b747042e6fea47ae0f256bce73e53b5426284
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-16 16:11:07 +0530

    Time: 143 ms (66.67%), Space: 128.3 MB (100.00%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     long long maxScore(vector<int>& a, vector<int>& b) {
-        vector<long long> t(4, INT_MIN);
-        for (long long n : b) {
-            t[3] = max(t[3], t[2] + n * a[3]);
-            t[2] = max(t[2], t[1] + n * a[2]);
-            t[1] = max(t[1], t[0] + n * a[1]);
-            t[0] = max(t[0], n * a[0]);
-        }
-        return t[3];
+        vector<long long> dp(4, -1e11);
+        for (int bi : b)
+            for (int i = 3; i >= 0; --i)
+                dp[i] = max(dp[i], (i > 0 ? dp[i - 1] : 0LL) + 1LL * a[i] * bi);
+        return dp[3];
     }
 };
