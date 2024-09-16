58c6f68efe22a26ce5088188b8123fd17785e50d
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-16 21:29:46 +0530

    Time: 597 ms (23.08%), Space: 233.6 MB (7.69%) - LeetHub

--- 3291-minimum-number-of-valid-strings-to-form-target-i/3291-minimum-number-of-valid-strings-to-form-target-i.cpp
+++ 3291-minimum-number-of-valid-strings-to-form-target-i/3291-minimum-number-of-valid-strings-to-form-target-i.cpp
@@ -1,68 +1,73 @@
-const int ALPHABET_SIZE = 26;
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
 
-class TrieNode {
+using namespace std;
+
+class Node {
 public:
-    TrieNode* children[ALPHABET_SIZE];
-
-    TrieNode() {
-        fill(children, children + 26, nullptr);
+    Node* child[26];
+    Node() {
+        fill(begin(child), end(child), nullptr);
     }
 };
 
 class Trie {
 public:
-    TrieNode* root;
-
+    Node* root;
     Trie() {
-        root = new TrieNode();
+        root = new Node();
     }
 
-    void insert(const string& word) {
-        TrieNode* node = root;
-        for (char c : word) {
-            int idx = c - 'a';
-            if (node->children[idx] == nullptr) {
-                node->children[idx] = new TrieNode();
+    void insert(const string& s) {
+        auto node = root;
+        for(char c : s) {
+            int i = c - 'a';
+            if(node->child[i] == nullptr) {
+                node->child[i] = new Node();
             }
-            node = node->children[idx];
+            node = node->child[i];
         }
     }
 };
 
 class Solution {
 public:
-    int n;
-    string target;
-    vector<int> dp;
-
-    int solve(int i, TrieNode* root) {
-        if (i == n) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        TrieNode* node = root;
-        int res = 1e5;
-
-        for (int j = i; j < n; ++j) {
-            char c = target[j];
-            int idx = c - 'a';
-            if (node->children[idx] == nullptr) break;
-            node = node->children[idx];
-            int t = solve(j + 1, root);
-            res = min(res, t + 1);
+    int helper(int i, int n, Node* root, const string& s, vector<int>& dp) {
+        if(i == n) {
+            return 0;
         }
-        return dp[i] = res;
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        
+        auto node = root;
+        int ans = INT_MAX;
+        
+        for(int j = i; j < n; j++) {
+            int t = s[j] - 'a';
+            if(node->child[t] == nullptr) {
+                break;
+            }
+            node = node->child[t];
+            int result = helper(j + 1, n, root, s, dp);
+            if(result != INT_MAX) {
+                ans = min(ans, 1 + result);
+            }
+        }
+        dp[i] = ans;
+        return dp[i];
     }
 
     int minValidStrings(const vector<string>& words, const string& target) {
+        int n = target.size();
         Trie trie;
-        for (const string& word : words) {
+        for(const auto& word : words) {
             trie.insert(word);
         }
-        n = target.size();
-        this->target = target;
-        dp.resize(n, -1);
-
-        int res = solve(0, trie.root);
-        return res == 1e5 ? -1 : res;
+        vector<int> dp(n, -1);
+        int ans = helper(0, n, trie.root, target, dp);
+        return ans == INT_MAX ? -1 : ans;
     }
 };
