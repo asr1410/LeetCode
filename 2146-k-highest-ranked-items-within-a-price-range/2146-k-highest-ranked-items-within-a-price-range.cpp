5ac8ef5ac8ef6266d1d0e3e6fb18ef617e12996e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-17 19:58:16 +0530

    Time: 410 ms (63.39%), Space: 144.4 MB (100.00%) - LeetHub

--- 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
+++ 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> highestRankedKItems(vector<vector<int>>& grid, vector<int>& price, vector<int>& start, int k) {
-        int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>ans,res;
-        vector<vector<bool>>seen(m,vector<bool>(n,false));
-        queue<pair<int,int>>q;
-        q.push({start[0],start[1]});
-        vector<vector<int>>dirs={{-1,0},{0,-1},{0,1},{1,0}};
-        // distance
-        int dist=0;
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                auto p=q.front();
-                    q.pop();
-                if(seen[p.first][p.second])
-                    continue;
-                if(grid[p.first][p.second]==0)
-                    continue;
-                seen[p.first][p.second]=true;
-                if(grid[p.first][p.second]!=1){
-                    int val=grid[p.first][p.second];
-                    if(val>=price[0] && val<=price[1])
-                       res.push_back({dist,val,p.first,p.second});
+    int dir[5] = {0, -1, 0, 1, 0};
+    vector<vector<int>> highestRankedKItems(vector<vector<int>>& grid, vector<int>& pricing, vector<int>& start, int k) {
+        priority_queue<array<int, 4>, vector<array<int, 4>>, greater<array<int, 4>>> pq;
+        vector<vector<int>> res;
+        pq.push({0, grid[start[0]][start[1]], start[0], start[1]});
+        grid[start[0]][start[1]] = 0;
+        while (!pq.empty() && res.size() < k) {
+            auto [dist, price, x, y] = pq.top(); pq.pop();
+            if (price >= pricing[0] && price <= pricing[1])
+                res.push_back({x, y});
+            for (int d = 0; d < 4; ++d) {
+                int x1 = x + dir[d], y1 = y + dir[d + 1];
+                if (min(x1, y1) >= 0 && x1 < grid.size() && y1 < grid[x1].size() && grid[x1][y1] != 0) {
+                    pq.push({dist + 1, grid[x1][y1], x1, y1});
+                    grid[x1][y1] = 0;
                 }
-                for(auto & dir:dirs){
-                    int row=p.first+dir[0],col=dir[1]+p.second;
-                    if(row>=0 && row<m && col>=0 && col<n)
-                        q.push({row,col});
-                }      
             }
-            dist++;
         }
-        sort(res.begin(),res.end());
-        for(int i=0;i<min(int(k),int(res.size()));i++)
-            ans.push_back({res[i][2],res[i][3]});
-        return ans;
-        
+        return res;
     }
 };
