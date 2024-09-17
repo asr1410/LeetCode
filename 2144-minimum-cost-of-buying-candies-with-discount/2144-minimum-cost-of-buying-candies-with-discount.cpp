717d8e2fb0e340c530d8db77763b38b07e831f9c
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-17 16:47:15 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (99.17%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+        int ans = 0, n = cost.size();
+        for(int i = 0; i < n; i++) {
+            if(i % 3 == 2) {
+                continue;
+            } else {
+                ans += cost[i];
+            }
+        }
+        return ans;
+    }
+};
