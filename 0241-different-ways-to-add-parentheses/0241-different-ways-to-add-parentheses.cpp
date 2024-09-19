312042a3e77eb52842ec614967b9b324a9c0b6c3
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-19 20:13:16 +0530

    Time: 8 ms (10.46%), Space: 13.7 MB (40.75%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
+    // function to get the result of the operation
     int perform(int x, int y, char op) {
         if(op == '+') return x + y;
         if(op == '-') return x - y;
         if(op == '*') return x * y;
         return 0;
     }
-
-    int helper(string exp) {
-        for(int i = 0; i < exp.length(); i++) {
-            if(!isdigit(exp[i])) {
-                int left = helper(exp.substr(0, i));
-                int right = helper(exp.substr(i + 1));
-                return perform(left, right, exp[i]);
-            }
-        }
-        return stoi(exp);
-    }
     
     vector<int> diffWaysToCompute(string exp) {
+        
         vector<int> results;
-        bool isNumber = true;
+        bool isNumber = 1;
+    
         for(int i = 0; i < exp.length(); i++) {
+            // check if current character is an operator
             if(!isdigit(exp[i])) {
-                isNumber = false;
+                
+                // if current character is not a digit then
+                // exp is not purely a number
+                isNumber = 0;
+                
+                // list of first operands
                 vector<int> left = diffWaysToCompute(exp.substr(0, i));
+                
+                // list of second operands
                 vector<int> right = diffWaysToCompute(exp.substr(i + 1));
-                for(int l : left) {
-                    for(int r : right) {
-                        results.push_back(perform(l, r, exp[i]));
+                
+                // performing operations
+                for(int x : left) {
+                    for(int y : right) {
+                        int val = perform(x, y, exp[i]);
+                        results.push_back(val);
                     }
                 }
+                
             }
         }
         
-        if(isNumber) results.push_back(stoi(exp));
+        if(isNumber == 1) results.push_back(stoi(exp));
         return results;
     }
 };
