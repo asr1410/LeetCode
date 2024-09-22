7bf726bd5d2e87a342e8bc0372a3aa8e6e6e4b60
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-23 01:22:12 +0530

    Time: 30 ms (100.00%), Space: 27.8 MB (100.00%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    long long minNumberOfSeconds(int mountainHeight, vector<int>& wt) {
-        long long l = 1, r = 100000000000LL * mountainHeight / wt.size();
-        while (l < r) {
-            long long m = (l + r) / 2, h = transform_reduce(begin(wt), end(wt), 0LL, plus{}, [&](int t){
-                return (long long)(sqrt(m / t * 2 + 0.25) - 0.5);
-            });
-            if (h < mountainHeight)
-                l = m + 1;
-            else
-                r = m;
+    bool canDo(long long d, long long mh, std::vector<int>& wt) {
+        long long wd = 0;
+        for(int w : wt) {
+            wd += static_cast<long long>(sqrt(d / w * 2 + 0.25) - 0.5);
         }
-        return l;
+        return wd >= mh;
+    }
+    
+    long long minNumberOfSeconds(int mh, std::vector<int>& wt) {
+        long long l = 0, r = LLONG_MAX, ans = -1;
+        while(l <= r) {
+            long long m = (l + r) / 2;
+            if(canDo(m, mh, wt)) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans;
     }
 };
