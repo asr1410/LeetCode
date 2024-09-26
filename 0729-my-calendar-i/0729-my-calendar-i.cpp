92be9ebdfb071de2b38129faf4681ec59951e420
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-26 10:00:26 +0530

    Time: 71 ms (81.80%), Space: 42.8 MB (36.63%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -3,7 +3,7 @@ class MyCalendar {
 public:    
     bool book(int s1, int e1) {
         auto slot = bookings.lower_bound({s1, e1});
-        if(slot != end(bookings)   && !(slot -> first >= e1) || slot != begin(bookings) && !(prev(slot) -> second <= s1))
+        if(slot != end(bookings) && !(slot->first >= e1) || slot != begin(bookings) && !(prev(slot)->second <= s1))
 			    return false;
         bookings.insert({s1, e1});
         return true;
