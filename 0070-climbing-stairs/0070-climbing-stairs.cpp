87438c0fea3972a69c467a85438334d5330e1622
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-27 00:05:36 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (49.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,8 +9,13 @@ public:
         return dp[n] = otaken + ttaken;
     }
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1, 0);
         dp[0] = 1;
-        return helper(n, dp);
+        for(int i = 1; i <= n; i++) {
+            int otaken = dp[i - 1];
+            int ttaken = i >= 2 ? dp[i - 2]: 0;
+            dp[i] = otaken + ttaken;
+        }
+        return dp[n];
     }
 };
