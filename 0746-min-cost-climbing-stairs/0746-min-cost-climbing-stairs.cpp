e1644f9f9f7cccc1bd0f3b8df26f0288d63be46c
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-27 00:58:58 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (98.00%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    int helper(int i, vector<int>& cost, vector<int>& dp) {
-        if(i >= cost.size()) {
-            return 0;
-        }
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-        int otaken = cost[i] + helper(i + 1, cost, dp);
-        int ttaken = cost[i] + helper(i + 2, cost, dp);
-        return dp[i] = min(otaken, ttaken);
-    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size(), pp = 0, p = 0;
         for(int i = n - 1; i >= 0; i--) {
-            int otaken = cost[i] + p;
-            int ttaken = cost[i] + pp;
+            int temp = cost[i] + min(p, pp);
             pp = p;
-            p = min(otaken, ttaken);
+            p = temp;
         }
         return min(pp, p);
     }
