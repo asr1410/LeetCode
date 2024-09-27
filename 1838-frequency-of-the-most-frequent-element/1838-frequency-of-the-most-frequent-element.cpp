eb615dbd396d65b9594c8c616253591e51cc42f1
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-09-28 00:01:19 +0530

    Time: 156 ms (56.45%), Space: 105.8 MB (47.31%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        long long l = 0, n = nums.size(), total = 0, ans = 0;
-        long long r = 0;
+        long long l = 0, total = 0, ans = 0;
 
-        while (r < n) {
+        for (int r = 0; r < nums.size(); r++) {
             total += nums[r];
-            long long cost = nums[r] * (r - l + 1) - total;
-
-            while (cost > k) {
-                total -= nums[l];
-                l++;
-                cost = nums[r] * (r - l + 1) - total;
+            while (nums[r] * (r - l + 1) - total > k) {
+                total -= nums[l++];
             }
-
             ans = max(ans, r - l + 1);
-            r++;
         }
 
         return ans;
