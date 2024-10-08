ba3ab06181a0592c5328ce1465862cc1e06c0d74
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-10-08 12:20:25 +0530

    Time: 0 ms (100.00%), Space: 26.6 MB (56.96%) - LeetHub

---
+++ 3309-maximum-possible-number-by-binary-concatenation/3309-maximum-possible-number-by-binary-concatenation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxGoodNumber(vector<int>& nums) {
+        vector<string> snums;
+        for(int num : nums) {
+            string s;
+            while(num) {
+                s.push_back('0' + (num & 1));
+                num >>= 1;
+            }
+            reverse(s.begin(), s.end());
+            snums.push_back(s);
+        }
+        sort(snums.begin(), snums.end(), [](string& a, string& b) {
+            return a + b > b + a;
+        });
+        for(string s : snums)
+            cout << s << " ";
+        cout << endl;
+        int ans = 0;
+        for(string s : snums) {
+            for(char c : s) {
+                ans |= (c - '0');
+                ans <<= 1;
+            }
+        }
+        return ans >> 1;
+    }
+};
