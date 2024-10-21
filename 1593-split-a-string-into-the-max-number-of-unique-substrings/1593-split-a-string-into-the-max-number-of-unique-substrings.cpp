d3265b39a81e4303928980430e9587f6edd8b67d
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-10-22 01:57:21 +0530

    Time: 191 ms (87.61%), Space: 47.7 MB (88.18%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int maxUniqueSplit(string s) {
-        unordered_set<string> uset;
-        return maxUniqueSplitHelper(s, 0, uset);
-    }
-    
-private:
-    int maxUniqueSplitHelper(string &s, int start, unordered_set<string> &uset) {
-        if (start == s.size()) {
-            return uset.size();
+    int ans = 0;
+    void helper(int n, string& s, unordered_set<string>& uset) {
+        if (n == s.size()) {
+            ans = max(ans, (int)uset.size());
+            return;
         }
-        
-        int maxCount = 0;
-        for (int end = start + 1; end <= s.size(); end++) {
-            string substring = s.substr(start, end - start);
-            if (uset.insert(substring).second) {
-                maxCount = max(maxCount, maxUniqueSplitHelper(s, end, uset));
-                uset.erase(substring);
+        string t;
+        for (int i = n; i < s.size(); i++) {
+            t += s[i];
+            if (uset.insert(t).second) {
+                helper(i + 1, s, uset);
+                uset.erase(t);
             }
         }
-        return maxCount;
+    }
+    int maxUniqueSplit(string s) {
+        unordered_set<string> uset;
+        helper(0, s, uset);
+        return ans;
     }
 };
