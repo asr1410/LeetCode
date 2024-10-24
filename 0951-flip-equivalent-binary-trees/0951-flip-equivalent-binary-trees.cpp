dcefd67568f7ad2187d4051999496168b514aad7
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-10-25 02:38:04 +0530

    Time: 0 ms (100.00%), Space: 14.5 MB (61.08%) - LeetHub

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (root1 == nullptr && root2 == nullptr) {
+            return true;
+        }
+        if (root1 == nullptr || root2 == nullptr) {
+            return false;
+        }
+        if(root1->val == root2->val) {
+            return (flipEquiv(root1->left, root2->left) and flipEquiv(root1->right, root2->right)) or
+                (flipEquiv(root1->left, root2->right) and flipEquiv(root1->right, root2->left));
+        }
+        return false;
+    }
+};
