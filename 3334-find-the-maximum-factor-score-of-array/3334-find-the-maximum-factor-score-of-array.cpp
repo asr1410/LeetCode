cd2c9a9536420cd2bfd531a04893eead1a3c9ff8
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-10-30 00:43:06 +0530

    Time: 115 ms (6.68%), Space: 32.4 MB (25.97%) - LeetHub

--- 3334-find-the-maximum-factor-score-of-array/3334-find-the-maximum-factor-score-of-array.cpp
+++ 3334-find-the-maximum-factor-score-of-array/3334-find-the-maximum-factor-score-of-array.cpp
@@ -1,46 +1,59 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+
 class Solution {
 public:
     long long lcmf(int i, int n, vector<int> nums) {
         long long ans = 1;
-        for(int j = 2; j < 31; j++) {
-            int cnt = 0;
-            for(int k = 0; k < n; k++) {
-                if(i != k) {
-                    int t = 0, val = nums[k];
-                    while(val % j == 0) {
-                        t++;
-                        val /= j;
+        vector<int> prime_counts(31, 0);
+        
+        for (int k = 0; k < n; k++) {
+            if (i != k) {
+                for (int j = 2; j < 31; j++) {
+                    int count = 0;
+                    while (nums[k] % j == 0) {
+                        count++;
+                        nums[k] /= j;
                     }
-                    nums[k] = val;
-                    cnt = max(cnt, t);
+                    prime_counts[j] = max(prime_counts[j], count);
                 }
             }
-            ans *= pow(j, cnt);
+        }
+        
+        for (int j = 2; j < 31; j++) {
+            ans *= pow(j, prime_counts[j]);
         }
         return ans;
     }
+    
     int gcdf(int i, int n, vector<int>& nums) {
         int ans = 1;
-        for(int j = 2; j < 31; j++) {
+        
+        for (int j = 2; j < 31; j++) {
             int cnt = 0;
-            for(int k = 0; k < n; k++) {
-                if(i != k and nums[k] % j == 0) {
+            for (int k = 0; k < n; k++) {
+                if (i != k && nums[k] % j == 0) {
                     cnt++;
                 }
             }
-            if((i == -1 and cnt == n) or (i != -1 and cnt == n - 1)) {
+            if ((i == -1 && cnt == n) || (i != -1 && cnt == n - 1)) {
                 ans = j;
             }
         }
         return ans;
     }
+
     long long maxScore(vector<int>& nums) {
-        if(nums.size() == 1) {
+        if (nums.size() == 1) {
             return nums[0] * nums[0];
         }
         long long ans = 0;
         int n = nums.size();
-        for(int i = -1; i < n; i++) {
+        for (int i = -1; i < n; i++) {
             long long l = lcmf(i, n, nums);
             int g = gcdf(i, n, nums);
             ans = max(ans, l * g);
