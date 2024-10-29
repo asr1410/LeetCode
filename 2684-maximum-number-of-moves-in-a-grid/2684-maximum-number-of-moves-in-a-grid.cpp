0c778800cd98d23aaaf0d037eef44b639b9cfe74
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-10-29 18:58:37 +0530

    Time: 0 ms (100.00%), Space: 74.4 MB (57.28%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int helper(int i, int j, int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if (j == n - 1) return 0;
         if(dp[i][j] != -1) {
             return dp[i][j];
         }
-        int f = (i > 0 && j < n - 1 && grid[i][j] < grid[i - 1][j + 1]) ? 1 + helper(i - 1, j + 1, m, n, grid, dp) : 0;
-        int s = (j < n - 1 && grid[i][j] < grid[i][j + 1]) ? 1 + helper(i, j + 1, m, n, grid, dp) : 0;
-        int t = (i < m - 1 && j < n - 1 && grid[i][j] < grid[i + 1][j + 1]) ? 1 + helper(i + 1, j + 1, m, n, grid, dp) : 0;
+        if(j == 0) {
+            int ans = 0;
+            for(int i = 0; i < m; i++) {
+                int f = i > 0 and j < n - 1 and grid[i][j] < grid[i - 1][j + 1] ? 1 + helper(i - 1, j + 1, m, n, grid, dp) : 0;
+                int s = j < n - 1 and grid[i][j] < grid[i][j + 1] ? 1 + helper(i, j + 1, m, n, grid, dp) : 0;
+                int t = i < m - 1 and j < n - 1 and grid[i][j] < grid[i + 1][j + 1] ? 1 + helper(i + 1, j + 1, m, n, grid, dp) : 0;
+                ans = dp[i][j] = max(ans, max(f, max(s, t)));
+            }
+            return ans;
+        }
+        int f = i > 0 and j < n - 1 and grid[i][j] < grid[i - 1][j + 1] ? 1 + helper(i - 1, j + 1, m, n, grid, dp) : 0;
+        int s = j < n - 1 and grid[i][j] < grid[i][j + 1] ? 1 + helper(i, j + 1, m, n, grid, dp) : 0;
+        int t = i < m - 1 and j < n - 1 and grid[i][j] < grid[i + 1][j + 1] ? 1 + helper(i + 1, j + 1, m, n, grid, dp) : 0;
         return dp[i][j] = max(f, max(s, t));
     }
     
     int maxMoves(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> dp(m, vector<int> (n, -1));
-        int maxMoves = 0;
-        for (int i = 0; i < m; i++) {
-            maxMoves = max(maxMoves, helper(i, 0, m, n, grid, dp));
-        }
-        return maxMoves;
+        return helper(0, 0, m, n, grid, dp);
     }
 };
