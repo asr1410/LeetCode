5b92b9363642055c50dd719eef876b547bfdd913
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-10-31 02:20:16 +0530

    Time: 562 ms (5.33%), Space: 188.7 MB (5.88%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,44 +1,40 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
-
 class Solution {
 public:
-    int lhelper(int i, const std::vector<int>& nums) {
-        std::vector<int> temp;
+    int lhelper(int i, vector<int>& nums) {
+        vector<int> temp;
         for (int j = 0; j <= i; j++) {
             if (temp.empty() || temp.back() < nums[j]) {
                 temp.push_back(nums[j]);
             } else {
-                int idx = std::lower_bound(temp.begin(), temp.end(), nums[j]) - temp.begin();
+                int idx = lower_bound(temp.begin(), temp.end(), nums[j]) - temp.begin();
                 temp[idx] = nums[j];
             }
         }
-        return temp.size();  // Return the size of the longest increasing subsequence
+        return temp.size();
     }
 
-    int rhelper(int i, const std::vector<int>& nums) {
-        std::vector<int> temp;
+    int rhelper(int i, vector<int>& nums) {
+        vector<int> temp;
         for (int j = nums.size() - 1; j >= i; j--) {
             if (temp.empty() || temp.back() < nums[j]) {
                 temp.push_back(nums[j]);
             } else {
-                int idx = std::lower_bound(temp.begin(), temp.end(), nums[j]) - temp.begin();
+                int idx = lower_bound(temp.begin(), temp.end(), nums[j]) - temp.begin();
                 temp[idx] = nums[j];
             }
         }
-        return temp.size();  // Return the size of the longest increasing subsequence
+        return temp.size();
     }
 
-    int minimumMountainRemovals(std::vector<int>& nums) {
+    int minimumMountainRemovals(vector<int>& nums) {
         int n = nums.size(), ans = INT_MAX;
         for (int i = 1; i < n - 1; i++) {
-            int l = lhelper(i, nums) - 1;  // Exclude the peak
-            int r = rhelper(i, nums) - 1;  // Exclude the peak
-            if (l > 0 && r > 0) {  // Both sides must have increasing sequences
-                ans = std::min(ans, n - l - r - 1);  // Total removals to form a mountain
+            int l = lhelper(i, nums) - 1;
+            int r = rhelper(i, nums) - 1;
+            if (l > 0 && r > 0) {
+                ans = min(ans, n - l - r - 1);
             }
         }
-        return ans == INT_MAX ? 0 : ans;  // If no valid mountain found, return 0
+        return ans;
     }
 };
