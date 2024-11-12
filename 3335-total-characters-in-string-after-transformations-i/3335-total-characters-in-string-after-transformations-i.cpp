fe24aafd6fa23f8d8f84e64cd2199aea0df329be
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-12 23:19:59 +0530

    Time: 199 ms (71.98%), Space: 370.5 MB (58.35%) - LeetHub

---
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    const int mod = 1e9 + 7;
+    
+    int helper(char c, int t, vector<vector<int>>& dp) {
+        if (t < 0) {
+            return 0;
+        }
+        if (c + t - 'a' < 26) {
+            return 1;
+        }
+        if (dp[c - 'a'][t] != -1) {
+            return dp[c - 'a'][t];
+        }
+        long long ans = 0;
+        ans += helper('a', t - ('z' - c + 1), dp);
+        ans %= mod;
+        ans += helper('b', t - ('z' - c + 1), dp);
+        ans %= mod;
+        return dp[c - 'a'][t] = ans;
+    }
+    
+    int lengthAfterTransformations(string s, int t) {
+        long long ans = 0;
+        vector<vector<int>> dp(26, vector<int>(t + 1, -1));
+        for (char c : s) {
+            ans += helper(c, t, dp);
+            ans %= mod;
+        }
+        return ans;
+    }
+};
