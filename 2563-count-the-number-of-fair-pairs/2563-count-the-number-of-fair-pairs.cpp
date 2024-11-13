3d13e9b2d6dbb2b8d2adfe10f07220eb22c94e41
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-13 19:06:34 +0530

    Time: 67 ms (49.40%), Space: 60.4 MB (61.66%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -3,11 +3,12 @@ public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(), nums.end());
         long long ans = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int low = lower - nums[i], high = upper - nums[i];
-            auto lowerBound = lower_bound(nums.begin() + i + 1, nums.end(), low);
-            auto upperBound = upper_bound(nums.begin() + i + 1, nums.end(), high);
-            ans += distance(lowerBound, upperBound);
+        for (int i = 0; i < nums.size(); ++i) {
+            int lbrem = lower - nums[i];
+            int ubrem = upper - nums[i];
+            int left = lower_bound(nums.begin() + i + 1, nums.end(), lbrem) - nums.begin();
+            int right = upper_bound(nums.begin() + i + 1, nums.end(), ubrem) - nums.begin();
+            ans += (right - left);
         }
         return ans;
     }
