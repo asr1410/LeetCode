da16e576b62ba9b80aea7808fc4d1fa3b01ffb5f
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-17 14:23:39 +0530

    Time: 960 ms (9.09%), Space: 324.8 MB (18.18%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> count(n + 1, 0);
+        for(auto q : queries) {
+            count[q[0]]--;
+            count[q[1] + 1]++;
+        }
+        for(int i = 1; i <= n; i++) {
+            count[i] += count[i - 1];
+        }
+        for(int i = 0; i <= n; i++) {
+            cout << count[i] << " ";
+        }
+        cout << endl;
+        for(int i = 0; i < n; i++) {
+            if(count[i] + nums[i] > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
