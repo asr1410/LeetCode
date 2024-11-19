10bb3ecfa53bb795c9d8964aa8f8c7c015a2e8cf
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-19 21:41:58 +0530

    Time: 99 ms (87.63%), Space: 95.2 MB (77.52%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> ump;
-        long long l = 0, ans = 0, temp = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            ump[nums[i]]++;
-            temp += nums[i];
-            if (i >= k - 1) {
-                if (ump.size() == k) {
+        long long l = 0, ans = 0;
+        for(long long i = 0, temp = 0; i < nums.size(); i++) {
+            if(i < k - 1) {
+                ump[nums[i]]++;
+                temp += nums[i];
+            } else {
+                ump[nums[i]]++;
+                temp += nums[i];
+                if(ump.size() == k) {
                     ans = max(ans, temp);
                 }
                 ump[nums[l]]--;
-                if (ump[nums[l]] == 0) {
+                if(ump[nums[l]] == 0) {
                     ump.erase(nums[l]);
                 }
                 temp -= nums[l];
