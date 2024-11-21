7e39b2cdcd8daea7592f9ce41268cded75840bb1
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-21 10:50:58 +0530

    Time: 7 ms (87.32%), Space: 12.1 MB (80.20%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -4,20 +4,20 @@ class Solution {
     int n = s.size();
     int ans = n;
     for (int i = 0; i < n; i++) {
-      if (s[i] == 'a') ca++;
-      if (s[i] == 'b') cb++;
-      if (s[i] == 'c') cc++;
+        ca += s[i] == 'a';
+        cb += s[i] == 'b';
+        cc += s[i] == 'c';
     }
     if (ca < k || cb < k || cc < k) return -1;
     int i = n - 1, j = n - 1;
     while (i >= 0) {
-      if (s[i] == 'a') ca--;
-      if (s[i] == 'b') cb--;
-      if (s[i] == 'c') cc--;
+        ca -= s[i] == 'a';
+        cb -= s[i] == 'b';
+        cc -= s[i] == 'c';
       while (ca < k || cb < k || cc < k) {
-        if (s[j] == 'a') ca++;
-        if (s[j] == 'b') cb++;
-        if (s[j] == 'c') cc++;
+        ca += s[j] == 'a';
+        cb += s[j] == 'b';
+        cc += s[j] == 'c';
         j--;
       }
       ans = min(ans, i + n - 1 - j);
