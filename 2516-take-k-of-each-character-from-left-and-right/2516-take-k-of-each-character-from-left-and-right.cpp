91f5c3a7278e1c01a5f3f78bed7772bca31bca34
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-21 10:51:58 +0530

    Time: 9 ms (77.82%), Space: 12 MB (91.48%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,28 +1,29 @@
 class Solution {
-  public: int takeCharacters(string s, int k) {
-    int ca = 0, cb = 0, cc = 0;
-    int n = s.size();
-    int ans = n;
-    for (int i = 0; i < n; i++) {
-        ca += s[i] == 'a';
-        cb += s[i] == 'b';
-        cc += s[i] == 'c';
+    public: 
+    int takeCharacters(string s, int k) {
+        int ca = 0, cb = 0, cc = 0;
+        int n = s.size();
+        int ans = n;
+        for (int i = 0; i < n; i++) {
+            ca += s[i] == 'a';
+            cb += s[i] == 'b';
+            cc += s[i] == 'c';
+        }
+        if (ca < k || cb < k || cc < k) return -1;
+        int i = n - 1, j = n - 1;
+        while (i >= 0) {
+            ca -= s[i] == 'a';
+            cb -= s[i] == 'b';
+            cc -= s[i] == 'c';
+            while (ca < k || cb < k || cc < k) {
+                ca += s[j] == 'a';
+                cb += s[j] == 'b';
+                cc += s[j] == 'c';
+                j--;
+            }
+            ans = min(ans, i + n - 1 - j);
+            i--;
+        }
+        return ans;
     }
-    if (ca < k || cb < k || cc < k) return -1;
-    int i = n - 1, j = n - 1;
-    while (i >= 0) {
-        ca -= s[i] == 'a';
-        cb -= s[i] == 'b';
-        cc -= s[i] == 'c';
-      while (ca < k || cb < k || cc < k) {
-        ca += s[j] == 'a';
-        cb += s[j] == 'b';
-        cc += s[j] == 'c';
-        j--;
-      }
-      ans = min(ans, i + n - 1 - j);
-      i--;
-    }
-    return ans;
-  }
 };
