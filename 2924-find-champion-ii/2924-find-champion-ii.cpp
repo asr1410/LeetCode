0571893bb69b5379a9a6883bf8aaa59cc6af49d5
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-26 17:32:24 +0530

    Time: 26 ms (58.41%), Space: 104.7 MB (19.54%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int parent[101];
+    vector<int> parent;
+
     int uparent(int u) {
         if(parent[u] == u) {
             return u;
@@ -14,8 +15,10 @@ public:
     }
 
     int findChampion(int n, vector<vector<int>>& edges) {
-        for(int i = 0; i < 101; i++)
+        parent.resize(n);
+        for(int i = 0; i < n; i++) {
             parent[i] = i;
+        }
         for(auto edge : edges) {
             ujoin(edge[0], edge[1]);
         }
