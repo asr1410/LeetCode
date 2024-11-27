346c03cde7fdd4c5bba18ceda5d348c62435e552
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-27 11:47:59 +0530

    Time: 44 ms (85.95%), Space: 51.8 MB (98.54%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<int> ans;
-        vector<vector<int>> mat(n, vector<int>(n, 0));
-        
-        for (int i = 1; i < n; i++) {
-            mat[i - 1][i] = 1;
-        }
-
-        for (auto q : queries) {
-            mat[q[0]][q[1]] = 1;
-            vector<int> dist(n, INT_MAX);
-            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-            pq.emplace(0, 0);
-            dist[0] = 0;
-
-            while (!pq.empty()) {
-                int u = pq.top().second;
-                int d = pq.top().first;
-                pq.pop();
-
-                if (u == n - 1) {
-                    ans.push_back(d);
-                    break;
-                }
-
-                for (int i = 0; i < n; i++) {
-                    if (mat[u][i] == 1 && d + 1 < dist[i]) {
-                        dist[i] = d + 1;
-                        pq.emplace(dist[i], i);
-                    }
-                }
+        vector<vector<int>> roads(n);
+        vector<int> dp(n), res;
+        iota(begin(dp), end(dp), 0);
+        for (const auto &q : queries) {
+            roads[q[1]].push_back(q[0]);
+            for (int i = q[1]; i < n; ++i) {
+                dp[i] = min(dp[i], dp[i - 1] + 1);
+                for (int l : roads[i])
+                    dp[i] = min(dp[i], dp[l] + 1);
             }
+            res.push_back(dp[n - 1]);
         }
-
-        return ans;
+        return res;
     }
 };
