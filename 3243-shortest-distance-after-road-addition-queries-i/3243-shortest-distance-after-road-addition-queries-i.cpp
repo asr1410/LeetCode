aa34a7abcbcae1ee30b1fb34abcb5f7732e2b84a
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-27 11:46:03 +0530

    Time: 2013 ms (5.24%), Space: 152.8 MB (19.84%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    #define pi pair<int, int>
-    vector<list<pi>> graph;
-    int dijkstra(int n){
-        vector<int> dist(n, 1e9);
-        dist[0] = 0;
-        priority_queue<pi, vector<pi>, greater<pi>> pq;
-        pq.push({0, 0});
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        vector<int> ans;
+        vector<vector<int>> mat(n, vector<int>(n, 0));
+        
+        for (int i = 1; i < n; i++) {
+            mat[i - 1][i] = 1;
+        }
 
-        while(! pq.empty()){
-            auto ele = pq.top();
-            int cd = ele.first, node = ele.second;
-            pq.pop();
-            if(node == n - 1) return dist[n-1];
-            if(cd > dist[node]) continue;
+        for (auto q : queries) {
+            mat[q[0]][q[1]] = 1;
+            vector<int> dist(n, INT_MAX);
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+            pq.emplace(0, 0);
+            dist[0] = 0;
 
-            for(auto ele : graph[node]){
-                int nbr = ele.first, wt = ele.second;
-                if(cd + wt < dist[nbr]){
-                    dist[nbr] = cd + wt;
-                    pq.push({cd + wt, nbr});
+            while (!pq.empty()) {
+                int u = pq.top().second;
+                int d = pq.top().first;
+                pq.pop();
+
+                if (u == n - 1) {
+                    ans.push_back(d);
+                    break;
+                }
+
+                for (int i = 0; i < n; i++) {
+                    if (mat[u][i] == 1 && d + 1 < dist[i]) {
+                        dist[i] = d + 1;
+                        pq.emplace(dist[i], i);
+                    }
                 }
             }
         }
-        return dist[n-1];
-    }
-    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        graph.clear();
-        graph.resize(n);
-        vector<int> res;
-        for(int i = 0; i < n - 1; ++i){
-            graph[i].push_back({i + 1, 1});
-        }
-        for(auto ele : queries){
-            graph[ele[0]].push_back({ele[1], 1});
-            int d = dijkstra(n);
-            res.push_back(d);
-        }
-        return res;
+
+        return ans;
     }
 };
