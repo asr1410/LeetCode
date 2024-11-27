4120b248199ad7f91d48518808ff2f550b6c209e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-27 12:33:28 +0530

    Time: 106 ms (69.52%), Space: 137.7 MB (33.50%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -6,24 +6,23 @@ public:
             adj[i - 1].emplace_back(i);
         }
         vector<int> ans;
-        for(auto& q : queries) {
-            adj[q[0]].push_back(q[1]);
-            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(auto& query : queries) {
+            adj[query[0]].push_back(query[1]);
+            queue<int> bfsQueue;
             vector<int> dis(n, 1e9);
-            pq.emplace(0, 0);
+            bfsQueue.push(0);
             dis[0] = 0;
-            while(!pq.empty()) {
-                int u = pq.top().second;
-                int d = pq.top().first;
-                pq.pop();
+            while(!bfsQueue.empty()) {
+                int u = bfsQueue.front();
+                bfsQueue.pop();
                 if(u == n - 1) {
-                    ans.emplace_back(d);
+                    ans.emplace_back(dis[u]);
                     break;
                 }
                 for(int v : adj[u]) {
-                    if(dis[v] > d + 1) {
-                        dis[v] = d + 1;
-                        pq.emplace(d + 1, v);
+                    if(dis[v] > dis[u] + 1) {
+                        dis[v] = dis[u] + 1;
+                        bfsQueue.push(v);
                     }
                 }
             }
