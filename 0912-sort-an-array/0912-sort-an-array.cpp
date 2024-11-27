017d1285e369b9a5255407163adcc21716b1d4da
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-27 12:42:25 +0530

    Time: 9 ms (97.75%), Space: 71 MB (65.87%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), [](int &a, int &b) {return a < b;});
+        sort(nums.begin(), nums.end());
         return nums;
     }
 };
