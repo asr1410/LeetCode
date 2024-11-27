69b1051a87e6f64bc875fbb0cdb3c7d732c752cc
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-11-27 12:41:57 +0530

    Time: 21 ms (89.87%), Space: 71 MB (65.87%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,39 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        if (nums.empty()) return {};
-        vector<int> temp;
-        temp.resize(nums.size());
-        mergeSort(nums, temp, 0, nums.size() - 1);
+        sort(nums.begin(), nums.end(), [](int &a, int &b) {return a < b;});
         return nums;
-    }
-
-private:
-    void mergeSort(vector<int>& nums, vector<int>& temp, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(nums, temp, left, mid);
-            mergeSort(nums, temp, mid + 1, right);
-            merge(nums, temp, left, mid, right);
-        }
-    }
-
-    void merge(vector<int>& nums, vector<int>& temp, int left, int mid, int right) {
-        for (int i = left; i <= right; ++i) {
-            temp[i] = nums[i];
-        }
-        
-        int i = left, j = mid + 1, k = left;
-        while (i <= mid && j <= right) {
-            if (temp[i] <= temp[j]) {
-                nums[k++] = temp[i++];
-            } else {
-                nums[k++] = temp[j++];
-            }
-        }
-
-        while (i <= mid) {
-            nums[k++] = temp[i++];
-        }
     }
 };
