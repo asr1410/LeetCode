b324c07a5bb937c9fdd6c07a73ca13d49546b58c
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-12-09 19:59:05 +0530

    Time: 280 ms (100.00%), Space: 25.1 MB (100.00%) - LeetHub

--- 3376-minimum-time-to-break-locks-i/3376-minimum-time-to-break-locks-i.cpp
+++ 3376-minimum-time-to-break-locks-i/3376-minimum-time-to-break-locks-i.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int findMinimumTime(vector<int>& strength, int k) {
-        // sort(strength.begin(), strength.end());
+        sort(strength.begin(), strength.end());
         int res = INT_MAX, size = strength.size(), prev = 0, energy = 1;
-        vector<int> permutation(size, 0);
-
-        for (int i = 0; i < size; i++) {
-            permutation[i] = i;
-        }
-
         do {
             int time = 0;
             int x = 1;
             for (int i = 0; i < size; i++) {
-                int s = strength[permutation[i]];
+                int s = strength[i];
                 time += ceil((double)s / (double)x);
                 x += k;
             }
             res = min(res, time);
-        } while (std::next_permutation(permutation.begin(), permutation.end()));
+        } while (next_permutation(strength.begin(), strength.end()));
 
         return res;
     }
