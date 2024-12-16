f0f666fca9705d82beb017cab3288117a826cbf2
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-12-16 10:46:14 +0530

    Time: 2 ms (35.42%), Space: 27.6 MB (29.75%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    vector<int> getFinalState(vector<int> nums, int k, int multiplier) {
-        int n = nums.size();
-        while(k--) {
-            int idx = -1;
-            for(int i = 0, mn = INT_MAX; i < n; i++) {
-                if(nums[i] < mn) {
-                    mn = nums[i];
-                    idx = i;
-                }
-            }
-            nums[idx] *= multiplier;
+    vector<int> getFinalState(vector<int>& v, int k, int m) {
+        int n = v.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        for(int i=0;i<n;i++) {
+            q.push({v[i],i});
         }
-        return nums;
+        while(k--) {
+            pair<int,int>p=q.top();
+            q.pop();
+            int val=p.first;
+            int idx = p.second;
+            v[idx]=val*m;
+            q.push({val*m,idx});
+        }
+        return v;
     }
 };
