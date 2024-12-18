07a0baa31553d305cf74ba2956de30c68cea97d3
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2024-12-19 00:02:47 +0530

    Time: 34 ms (96.17%), Space: 180.3 MB (66.92%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,32 +1,32 @@
 class NumArray {
 public:
-    vector<int> nums, bit;
+    vector<int> nums, fen;
     int n;
     NumArray(vector<int>& nums) {
-        this->nums = nums;
         n = nums.size();
-        bit.resize(n + 1, 0);
+        this->nums = nums;
+        fen.resize(n + 1, 0);
         for(int i = 0; i < n; i++) {
             add(i, nums[i]);
         }
     }
     
-    void add(int idx, int delta) {
+    void add(int idx, int val) {
         for(int i = idx + 1; i <= n; i += i & -i) {
-            bit[i] += delta;
+            fen[i] += val;
         }
     }
     
     int sum(int idx) {
         int sum = 0;
         for(int i = idx + 1; i > 0; i -= i & -i) {
-            sum += bit[i];
+            sum += fen[i];
         }
         return sum;
     }
     
     void update(int index, int val) {
-        int delta = val - nums[index];
+        int delta = val- nums[index];
         nums[index] = val;
         add(index, delta);
     }
