f8bcc0b8ede6b1591c9088bb11c39b484377dd41
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-01-09 23:25:22 +0530

    Time: 109 ms (62.38%), Space: 158 MB (65.03%) - LeetHub

---
+++ 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long maximumCoins(vector<vector<int>>& coins, int k) {
+        sort(coins.begin(), coins.end());
+        int n = coins.size();
+        long long res = 0, cur = 0;
+        for (int i = 0, j = 0; i < n; ++i) {
+            while (j < n && coins[j][1] <= coins[i][0] + k - 1) {
+                cur += 1L * (coins[j][1] - coins[j][0] + 1) * coins[j][2];
+                j++;
+            }
+            if (j < n) {
+                long long part = 1L * max(0, coins[i][0] + k - 1 - coins[j][0] + 1) * coins[j][2];
+                res = max(res, cur + part);
+            }
+            cur -= 1L * (coins[i][1] - coins[i][0] + 1) * coins[i][2];
+        }
+        cur = 0;
+        for (int i = 0, j = 0; i < n; ++i) {
+            cur += 1L * (coins[i][1] - coins[i][0] + 1) * coins[i][2];
+            while (coins[j][1] < coins[i][1] - k + 1) {
+                cur -= 1L * (coins[j][1] - coins[j][0] + 1) * coins[j][2];
+                j++;
+            }
+            long long part = 1L * max(0, coins[i][1] - k - coins[j][0] + 1) * coins[j][2];
+            res = max(res, cur - part);
+        }
+
+        return res;
+    }
+};
