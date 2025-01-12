4081255c115d198661d0e392fd6f5a2bc50590ec
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-01-13 00:59:22 +0530

    Time: 12 ms (68.27%), Space: 30.2 MB (34.26%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -3,21 +3,19 @@ public:
     bool canBeValid(string s, string locked) {
         if (s.size() & 1) return false;
 
-        int open = 0, balance = 0;
+        int balance = 0;
         for (int i = 0; i < s.size(); i++) {
             if (locked[i] == '0' || s[i] == '(') {
-                open++;
                 balance++;
             } else {
                 balance--;
             }
             if (balance < 0) return false;
         }
-
-        open = 0, balance = 0;
+        
+        balance = 0;
         for (int i = s.size() - 1; i >= 0; i--) {
             if (locked[i] == '0' || s[i] == ')') {
-                open++;
                 balance++;
             } else {
                 balance--;
