873b73e068e7acd8c349464a78f551e01081479e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-01-14 17:50:51 +0530

    Time: 360 ms (79.39%), Space: 100.5 MB (81.87%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,47 +1,34 @@
 class Solution {
-public:
-    int maximumAmount(vector<vector<int>>& coins) {
-        int m = coins.size(), n = coins[0].size();
-        int k = 2; // Maximum skips allowed
-
-        // Initialize DP table: dp[i][j][k] stores max value collectible at (i, j) with up to k skips.
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k + 1, -1e7)));
-
-        // Base case for the starting point
-        for (int skip = 0; skip <= k; ++skip) {
-            dp[0][0][skip] = (coins[0][0] < 0 && skip > 0) ? 0 : coins[0][0];
-        }
-
-        // Fill DP table
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                for (int skip = 0; skip <= k; ++skip) {
-                    if (i == 0 && j == 0) continue; // Skip the starting cell
-
-                    int fromTop = -1e7, fromLeft = -1e7;
-
-                    // Coming from the top
-                    if (i > 0) {
-                        fromTop = dp[i - 1][j][skip] + coins[i][j];
-                        if (coins[i][j] < 0 && skip > 0) {
-                            fromTop = max(fromTop, dp[i - 1][j][skip - 1]);
-                        }
-                    }
-
-                    // Coming from the left
-                    if (j > 0) {
-                        fromLeft = dp[i][j - 1][skip] + coins[i][j];
-                        if (coins[i][j] < 0 && skip > 0) {
-                            fromLeft = max(fromLeft, dp[i][j - 1][skip - 1]);
-                        }
-                    }
-
-                    dp[i][j][skip] = max(fromTop, fromLeft);
-                }
+private:
+    int cache[501][501][3];
+    int f(vector<vector<int>>& coins, int i, int j, int neu){
+        int n = coins.size();
+        int m = coins[0].size();
+        if(i == n - 1 && j == m - 1){
+            if(neu > 0 && coins[i][j] < 0){
+                return 0;
+            }else{
+                return coins[i][j];
             }
         }
-
-        // Return the maximum value at the bottom-right corner with up to 2 skips
-        return *max_element(dp[m - 1][n - 1].begin(), dp[m - 1][n - 1].end());
+        if(i >= n || j>= m){
+            return INT_MIN;
+        }
+        if (cache[i][j][neu] != INT_MIN) {
+            return cache[i][j][neu];
+        }
+        int take = INT_MIN;int nottake = INT_MIN;
+        take = coins[i][j] + max(f(coins, i + 1, j, neu), f(coins, i, j + 1, neu));
+        if(neu > 0 && coins[i][j] < 0)
+            nottake =  max(f(coins, i + 1, j, neu - 1), f(coins, i, j + 1, neu - 1));
+        return cache[i][j][neu] = max(take, nottake);
+    }
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+    for (int i = 0; i < 501; ++i)
+        for (int j = 0; j < 501; ++j)
+            for (int k = 0; k < 3; ++k)
+                cache[i][j][k] = INT_MIN;
+        return f(coins, 0, 0, 2);
     }
 };
