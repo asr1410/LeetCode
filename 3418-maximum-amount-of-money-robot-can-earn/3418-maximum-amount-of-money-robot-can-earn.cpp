a904b44c94fb5079ba55c66a123cf796b10e30ea
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-01-14 18:04:08 +0530

    Time: 514 ms (45.63%), Space: 250.3 MB (9.71%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,34 +1,44 @@
 class Solution {
-private:
-    int cache[501][501][3];
-    int f(vector<vector<int>>& coins, int i, int j, int neu){
-        int n = coins.size();
-        int m = coins[0].size();
-        if(i == n - 1 && j == m - 1){
-            if(neu > 0 && coins[i][j] < 0){
-                return 0;
-            }else{
-                return coins[i][j];
+public:
+    int helper(int i, int j, int k, vector<vector<int>>& coins, vector<vector<vector<int>>>& dp) {
+        if(i < 0 or j < 0) {
+            return -1e7;
+        }
+        if(i == 0 and j == 0) {
+            return coins[0][0] < 0 and k > 0 ? 0 : coins[0][0];
+        }
+        if(dp[i][j][k] != -1) {
+            return dp[i][j][k];
+        }
+        int up = coins[i][j] < 0 and k > 0 ? max(coins[i][j] + helper(i - 1, j, k, coins, dp), helper(i - 1, j, k - 1, coins, dp)) : coins[i][j] + helper(i - 1, j, k, coins, dp);
+        int left = coins[i][j] < 0 and k > 0 ? max(coins[i][j] + helper(i, j - 1, k, coins, dp), helper(i, j - 1, k - 1, coins, dp)) : coins[i][j] + helper(i, j - 1, k, coins, dp);
+        return dp[i][j][k] = max(up, left);
+    }
+    int maximumAmount(vector<vector<int>>& coins) {
+        int m = coins.size(), n = coins[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int> (3, INT_MIN)));
+        dp[0][0][0]=coins[0][0];
+        if (coins[0][0]<0) {
+            dp[0][0][1]=0; 
+            dp[0][0][2]=0;
+        } else {
+            dp[0][0][1]=dp[0][0][2]=coins[0][0];
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < 3; k++) {
+                    if (i==0 && j==0) continue;
+                    if(i > 0) {
+                        int up = coins[i][j] < 0 and k > 0 ? max(coins[i][j] + dp[i - 1][j][k], dp[i - 1][j][k - 1]) : coins[i][j] + dp[i - 1][j][k];
+                        dp[i][j][k] = max(dp[i][j][k], up);
+                    }
+                    if(j > 0) {
+                        int left = coins[i][j] < 0 and k > 0 ? max(coins[i][j] + dp[i][j - 1][k], dp[i][j - 1][k - 1]) : coins[i][j] + dp[i][j - 1][k];
+                        dp[i][j][k] = max(dp[i][j][k], left);
+                    }
+                }
             }
         }
-        if(i >= n || j>= m){
-            return INT_MIN;
-        }
-        if (cache[i][j][neu] != INT_MIN) {
-            return cache[i][j][neu];
-        }
-        int take = INT_MIN;int nottake = INT_MIN;
-        take = coins[i][j] + max(f(coins, i + 1, j, neu), f(coins, i, j + 1, neu));
-        if(neu > 0 && coins[i][j] < 0)
-            nottake =  max(f(coins, i + 1, j, neu - 1), f(coins, i, j + 1, neu - 1));
-        return cache[i][j][neu] = max(take, nottake);
-    }
-public:
-    int maximumAmount(vector<vector<int>>& coins) {
-    for (int i = 0; i < 501; ++i)
-        for (int j = 0; j < 501; ++j)
-            for (int k = 0; k < 3; ++k)
-                cache[i][j][k] = INT_MIN;
-        return f(coins, 0, 0, 2);
+        return max(dp[m - 1][n - 1][0], max(dp[m - 1][n - 1][1], dp[m - 1][n - 1][2]));
     }
 };
