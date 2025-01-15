30c59512e3eb842b82542de478e86c875f2d5510
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-01-15 12:07:12 +0530

    Time: 0 ms (100%), Space: 8.1 MB (10.75%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.cpp
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
+        int bc1 = __builtin_popcount(num1);
         int bc2 = __builtin_popcount(num2);
-        int result = 0;
 
-        for (int i = 31; i >= 0 && bc2 > 0; --i) {
-            if (num1 & (1 << i)) {
-                result |= (1 << i);
-                --bc2;
+        if(bc1 == bc2) {
+            return num1;
+        } else if(bc2 > bc1) {
+            int ans = num1;
+            int rem = bc2 - bc1;
+            int idx = 0;
+            while(rem) {
+                if(!((num1 >> idx) & 1)) {
+                    ans |= (1 << idx);
+                    rem--;
+                }
+                idx++;
             }
+            return ans;
         }
-
-        for (int i = 0; i < 32 && bc2 > 0; ++i) {
-            if (!(result & (1 << i))) {
-                result |= (1 << i);
-                --bc2;
+        int idx = 30;
+        int ans = 0;
+        while(bc2) {
+            if((num1 >> idx) & 1) {
+                ans |= (1 << idx);
+                bc2--;
             }
+            idx--;
         }
-
-        return result;
+        return ans;
     }
 };
