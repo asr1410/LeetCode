1a5c6a87484f69f0d189311f334cfe89bdd6533a
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-01-25 23:50:04 +0530

    Time: 187 ms (64.19%), Space: 184.1 MB (50.67%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        vector<pair<int, int>> b;
         int n = nums.size();
-        
-        vector<pair<int, int>> indexed_nums;
-        for (int i = 0; i < n; i++) {
-            indexed_nums.push_back({nums[i], i});
+
+        for (int i = 0; i < n; ++i)
+            b.push_back(make_pair(nums[i], i));
+
+        sort(b.begin(), b.end(), [](const auto& x, const auto& y) {
+            return x.first < y.first;
+        });
+        vector<vector<pair<int, int>>> c = {{b[0]}};
+
+        for (int i = 1; i < n; ++i) {
+            if (b[i].first - b[i - 1].first <= limit)
+                c.back().push_back(b[i]);
+            else
+                c.push_back({b[i]});
         }
-        
-        sort(indexed_nums.begin(), indexed_nums.end());
-        
-        vector<vector<int>> groups;
-        vector<int> current_group;
-        
-        for (int i = 0; i < n; i++) {
-            if (current_group.empty() || 
-                indexed_nums[i].first - indexed_nums[current_group.back()].first <= limit) {
-                current_group.push_back(i);
-            } else {
-                groups.push_back(current_group);
-                current_group = {i};
-            }
+
+        for (const auto& t : c) {
+            vector<int> ind;
+            for (const auto& p : t)
+                ind.push_back(p.second);
+            sort(ind.begin(), ind.end());
+
+            for (int i = 0; i < ind.size(); ++i)
+                nums[ind[i]] = t[i].first;
         }
-        
-        if (!current_group.empty()) {
-            groups.push_back(current_group);
-        }
-        
-        vector<int> result(n);
-        for (auto& group : groups) {
-            vector<int> values, indices;
-            for (int idx : group) {
-                values.push_back(indexed_nums[idx].first);
-                indices.push_back(indexed_nums[idx].second);
-            }
-            
-            sort(indices.begin(), indices.end());
-            
-            for (int i = 0; i < group.size(); i++) {
-                result[indices[i]] = values[i];
-            }
-        }
-        
-        return result;
+        return nums;
     }
 };
