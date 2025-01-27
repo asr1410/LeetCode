7b9985353318a970c0bfe79fd2b9c53572644e4e
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-01-27 10:28:07 +0530

    Time: 182 ms (70.08%), Space: 184 MB (58.38%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        vector<pair<int, int>> b;
+        vector<pair<int, int>> vpi;
         int n = nums.size();
-
-        for (int i = 0; i < n; ++i) b.push_back(make_pair(nums[i], i));
-
-        sort(b.begin(), b.end());
-        vector<vector<pair<int, int>>> c = {{b[0]}};
-
-        for (int i = 1; i < n; ++i) {
-            if (b[i].first - b[i - 1].first <= limit) c.back().push_back(b[i]);
-            else c.push_back({b[i]});
+        for(int i = 0; i < n; i++) {
+            vpi.emplace_back(nums[i], i);
+        }
+        sort(vpi.begin(), vpi.end());
+        vector<vector<pair<int, int>>> vvpi = {{vpi[0]}};
+        for(int i = 1; i < n; i++) {
+            if(vpi[i].first - vpi[i - 1].first <= limit) {
+                vvpi.back().emplace_back(vpi[i]);
+            } else {
+                vvpi.push_back({vpi[i]});
+            }
         }
 
-        for (const auto& t : c) {
-            vector<int> ind;
-            for (const auto& p : t) ind.push_back(p.second);
-            sort(ind.begin(), ind.end());
-            for (int i = 0; i < ind.size(); ++i) nums[ind[i]] = t[i].first;
+        for(auto& vpi : vvpi) {
+            vector<int> idx;
+            for(auto& pi : vpi) {
+                idx.emplace_back(pi.second);
+            }
+            sort(idx.begin(), idx.end());
+            for(int i = 0; i < idx.size(); i++) {
+                nums[idx[i]] = vpi[i].first;
+            }
         }
         return nums;
     }
