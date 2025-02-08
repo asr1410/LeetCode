8a7e6618baafb0d8c25104b3f4c991dc867ee865
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-02-08 15:14:29 +0530

    Time: 111 ms (67.5%), Space: 193.2 MB (83.13%) - LeetHub

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -2,31 +2,19 @@ class NumberContainers {
 public:
     unordered_map<int, int> ump;
     unordered_map<int, set<int>> ucnt;
-    NumberContainers() {
-        
-    }
-    
+
     void change(int index, int number) {
-        if(ump[index] != number) {
-            if(ump[index] != 0) {
-                ucnt[ump[index]].erase(index);
-            }
-            ucnt[number].insert(index);
-            ump[index] = number;
+        if (ump.count(index)) {
+            int prev = ump[index];
+            if (prev == number) return;
+            ucnt[prev].erase(index);
+            if (ucnt[prev].empty()) ucnt.erase(prev);
         }
+        ump[index] = number;
+        ucnt[number].insert(index);
     }
-    
+
     int find(int number) {
-        if(ucnt[number].empty() == false) {
-            return *ucnt[number].begin();
-        }
-        return -1;
+        return ucnt.count(number) && !ucnt[number].empty() ? *ucnt[number].begin() : -1;
     }
 };
-
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * NumberContainers* obj = new NumberContainers();
- * obj->change(index,number);
- * int param_2 = obj->find(number);
- */
