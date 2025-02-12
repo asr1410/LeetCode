165e423977f87808e389a5c350f1f432ccc398d7
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-02-13 00:26:36 +0530

    Time: 3 ms (48.78%), Space: 51 MB (80.96%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.js
+++ 3174-clear-digits/3174-clear-digits.js
@@ -7,9 +7,7 @@ var clearDigits = function(s) {
     
     for (let i = 0; i < s.length; i++) {
         if (s[i] >= '0' && s[i] <= '9') {
-            if (result.length > 0) {
-                result.pop();
-            }
+            result.pop();
         } else {
             result.push(s[i]);
         }
