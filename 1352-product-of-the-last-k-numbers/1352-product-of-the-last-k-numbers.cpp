045c86cd48141e22e5b559171bdcf2f867be6c6a
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-02-15 12:54:09 +0530

    Time: 16 ms (81.75%), Space: 78.5 MB (17.33%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -1,29 +1,27 @@
+#include <vector>
+using namespace std;
+
 class ProductOfNumbers {
 public:
-    vector<unsigned long long> products; // Store cumulative products
-    vector<int> zeroIdx; // Store indexes of zeros
+    vector<unsigned long long> products;
+    int zeroCount = 0;
 
     ProductOfNumbers() {
-        products.push_back(1); // Base product (neutral element)
+        products.push_back(1);
     }
-    
+
     void add(int num) {
         if (num == 0) {
-            products.push_back(1); // Reset product after zero
-            zeroIdx.push_back(products.size() - 1); // Store zero index
+            products.clear();
+            products.push_back(1);
+            zeroCount = 0;
         } else {
             products.push_back(products.back() * num);
         }
     }
-    
+
     int getProduct(int k) {
-        int startIdx = products.size() - k - 1;
-        
-        // If there is a zero in the last k elements, return 0
-        if (!zeroIdx.empty() && zeroIdx.back() > startIdx) {
-            return 0;
-        }
-        
-        return products.back() / products[startIdx];
+        if (k >= products.size()) return 0;
+        return products.back() / products[products.size() - k - 1];
     }
 };
