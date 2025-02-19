e6bb7b50809ac4c48e9b01ddf3dea462ad00d1db
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-02-20 00:48:41 +0530

    Time: 746 ms (5.18%), Space: 9.2 MB (8.17%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> taken;
+    vector<char> chars;
+    string ans = "999999999";
+    string temp;
+    
+    bool check(int& n, const string& temp, const string& pattern) {
+        for(int i = 0; i < n - 1; i++) {
+            if((pattern[i] == 'I' && temp[i] > temp[i + 1]) || 
+               (pattern[i] == 'D' && temp[i] < temp[i + 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    void helper(int& n, string& pattern) {
+        if(temp.size() == n) {
+            if(check(n, temp, pattern)) {
+                if(ans == "999999999" || stoll(temp) < stoll(ans)) {
+                    ans = temp;
+                }
+            }
+            return;
+        }
+        for(const char c : chars) {
+            if(taken[c - '0'] == 0) {
+                taken[c - '0'] = 1;
+                temp.push_back(c);
+                helper(n, pattern);
+                temp.pop_back();
+                taken[c - '0'] = 0;
+            }
+        }
+    }
+    
+    string smallestNumber(string pattern) {
+        int n = pattern.size() + 1;
+        for(int i = 0; i < n; i++) {
+            chars.push_back('1' + i);
+        }
+        taken.resize(n + 1, 0);
+        temp.reserve(n);
+        helper(n, pattern);
+        return ans;
+    }
+};
