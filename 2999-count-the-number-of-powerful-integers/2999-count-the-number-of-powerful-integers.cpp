2738f8d623ea439e567bb42c9c4371ee37883248
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-04-11 12:08:26 +0530

    Time: 0 ms (100%), Space: 10.5 MB (45.7%) - LeetHub

--- 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
+++ 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     long long cnt[16] = {};
-    long long dfs(int i, char limit, const string &n, const string &s) {
+
+    long long dfs(int i, char limit, std::string_view n, std::string_view s) {
         if (n.size() < s.size())
             return 0;
         if (i == n.size() - s.size())
             return n.substr(i) >= s;
-        int digits = min(limit, n[i]) - '1' + (i > 0);
+        int digits = std::min(limit, n[i]) - '1' + (i > 0);
         return cnt[n.size() - i - 1] * digits + (n[i] < limit ? dfs(i + 1, limit, n, s) : 0);
     }
+
     long long numberOfPowerfulInt(long long start, long long finish, int limit, const string &s) {
         string hi = to_string(finish), lo = to_string(start - 1);
         for (int i = s.size(); i < hi.size(); ++i)
