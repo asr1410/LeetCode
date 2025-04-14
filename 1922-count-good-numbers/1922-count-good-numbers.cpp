bdb523c78d56eb144e11228c7675b30f628cce74
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-04-14 17:16:13 +0530

    Time: 0 ms (100%), Space: 7.9 MB (69.95%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,22 +1,20 @@
 class Solution {
-private:
-    static constexpr int mod = 1000000007;
-
 public:
-    int countGoodNumbers(long long n) {
-        // use fast exponentiation to calculate x^y % mod
-        auto quickmul = [](int x, long long y) -> int {
-            int ret = 1, mul = x;
-            while (y > 0) {
-                if (y % 2 == 1) {
-                    ret = (long long)ret * mul % mod;
-                }
-                mul = (long long)mul * mul % mod;
-                y /= 2;
+    long long powmod(long long a, long long b, long long mod) {
+        long long result = 1;
+        a %= mod;
+        while (b > 0) {
+            if (b & 1) {
+                result = (result * a) % mod;
             }
-            return ret;
-        };
-
-        return (long long)quickmul(5, (n + 1) / 2) * quickmul(4, n / 2) % mod;
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+        return result;
+    }
+    int countGoodNumbers(long long n) {
+        long long mod = 1e9 + 7;
+        long long even = 5, prime = 4, ans = 1, ecount = (n + 1LL) >> 1, ocount = n >> 1;
+        return (powmod(even, ecount, mod) * powmod(prime, ocount, mod)) % mod;
     }
 };
