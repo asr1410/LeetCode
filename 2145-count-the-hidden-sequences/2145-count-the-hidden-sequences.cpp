be97ad722ef1d2a56978ae2e12e2f0c8a1de3ac1
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-04-21 17:17:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
@@ -1,15 +1,17 @@
-class Solution
-{
+class Solution {
 public:
-     int numberOfArrays(vector<int> &A, int lower, int upper)
-     {
-          long total = 0, minimum = 0, maximum = 0;
-          for (int num : A)
-          {
-               total += num;
-               maximum = max(maximum, total);
-               minimum = min(minimum, total);
-          }
-          return max(0L, minimum - maximum + upper - lower + 1);
-     }
+    int numberOfArrays(vector<int>& diff, int lower, int upper) {
+        // 0 1 -2 2
+        // 1 2 -1 3
+        // 2 3 0 4
+        // 3 4 1 5 
+        int n = diff.size(), mn = 0, mx = 0;
+        for(int i = 0, prev = 0; i < n; i++) {
+            prev += diff[i];
+            mn = min(mn, prev);
+            mx = max(mx, prev);
+        }
+        int up = mx + (lower - mn);
+        return max(0, upper - up + 1);
+    }
 };
