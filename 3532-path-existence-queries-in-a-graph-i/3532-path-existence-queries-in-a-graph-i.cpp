cc173ace4391adf5d4ba12244e9a156d4dc4659d
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-04-29 11:27:21 +0530

    Time: 2 ms (96.83%), Space: 224.1 MB (82.96%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,62 +1,25 @@
-#include <vector>
-using namespace std;
-
-class FenwickTree {
-private:
-    vector<int> bit;
-    int n;
-public:
-    FenwickTree(int size) {
-        n = size;
-        bit.assign(n + 2, 0); // safer size
-    }
-    void update(int i, int val) {
-        i++; // make 1-based
-        while (i <= n + 1) {
-            bit[i] += val;
-            i += (i & -i);
-        }
-    }
-    int query(int i) {
-        i++; // make 1-based
-        int sum = 0;
-        while (i > 0) {
-            sum += bit[i];
-            i -= (i & -i);
-        }
-        return sum;
-    }
-    int rangeQuery(int l, int r) {
-        return query(r) - query(l - 1);
-    }
-};
-
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<int> breaks(n-1, 0);
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - nums[i-1] > maxDiff) {
-                breaks[i-1] = 1;
-            }
+        vector<int> pre(n, 0);
+        for(int i = 1; i < n; i++) {
+            pre[i] = (abs(nums[i - 1] - nums[i]) > maxDiff);
         }
-
-        FenwickTree fen(n-1);
-        for (int i = 0; i < n-1; i++) {
-            if (breaks[i]) {
-                fen.update(i, 1);
-            }
+        // for(int i = 0; i < n; i++) {
+        //     cout << pre[i] << " ";
+        // }
+        // cout << endl;
+        for(int i = 1; i < n; i++) {
+            pre[i] += pre[i - 1];
         }
-
-        vector<bool> ans;
-        for (const auto& q : queries) {
-            int u = q[0], v = q[1];
-            if (u == v) {
-                ans.push_back(true);
-            } else {
-                int l = min(u, v), r = max(u, v) - 1; // notice -1 because breaks are between nodes
-                ans.push_back(fen.rangeQuery(l, r) == 0);
-            }
+        // for(int i = 0; i < n; i++) {
+        //     cout << pre[i] << " ";
+        // }
+        // cout << endl;
+        int t = queries.size();
+        vector<bool> ans(t, false);
+        for(int i = 0; i < t; i++) {
+            ans[i] = (pre[queries[i][1]] - pre[queries[i][0]]) == 0;
         }
         return ans;
     }
