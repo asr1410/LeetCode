605d4850702348efc6c8a57447690bceccfdde5f
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-05-02 00:04:44 +0530

    Time: 82 ms (92.05%), Space: 77.8 MB (89.92%) - LeetHub

--- 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.cpp
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    int maxTaskAssign(vector<int>& tasks, vector<int>& ws, int pills, int strength) {
-        int l = 0, r = min(tasks.size(), ws.size());
-        sort(begin(tasks), end(tasks));
-        sort(begin(ws), end(ws));
-        while (l < r) {
-            int m = (l + r + 1) / 2, need = 0;
-            multiset<int> ms(end(ws) - m, end(ws));
-            for (int i = m - 1; i >= 0; --i) {
-                auto it = prev(end(ms));
-                if (*it < tasks[i]) {
-                    it = ms.lower_bound(tasks[i] - strength);
-                    if (it == end(ms) || ++need > pills)
-                        break;
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills,
+                      int strength) {
+        int n = tasks.size(), m = workers.size();
+        sort(tasks.begin(), tasks.end());
+        sort(workers.begin(), workers.end());
+
+        auto check = [&](int mid) -> bool {
+            int p = pills;
+            deque<int> ws;
+            int ptr = m - 1;
+            // Enumerate each task from largest to smallest
+            for (int i = mid - 1; i >= 0; --i) {
+                while (ptr >= m - mid && workers[ptr] + strength >= tasks[i]) {
+                    ws.push_front(workers[ptr]);
+                    --ptr;
                 }
-                ms.erase(it);
+                if (ws.empty()) {
+                    return false;
+                }
+                // If the largest element in the deque is greater than or equal
+                // to tasks[i]
+                else if (ws.back() >= tasks[i]) {
+                    ws.pop_back();
+                } else {
+                    if (!p) {
+                        return false;
+                    }
+                    --p;
+                    ws.pop_front();
+                }
             }
-            if (ms.empty())
-                l = m;
-            else
-                r = m - 1;
+            return true;
+        };
+
+        int left = 1, right = min(m, n), ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (check(mid)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return l;
+        return ans;
     }
 };
