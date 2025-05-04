78e4723c08d4abd2aa875453fd7ec1f22e0d46b9
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-05-04 17:39:22 +0530

    Time: 24 ms (5.54%), Space: 115.6 MB (8.74%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    
-    int dominoRotations(vector<int>& tops, vector<int>& bottoms)
-    {
-        int minRotation = INT_MAX;
-        for(int i = 1; i <= 6; ++i)
-        {
-            int swap = 0 , j;
-            for(j = 0; j<tops.size(); ++j)
-                {
-                    if(tops[j] == i)
-                        continue;
-                    else if(bottoms[j] == i)
-                        ++swap;
-                    else
-                        break;
-                }
-                
-                // if we reach at the end of vector && minRotation > swap
-                if(j == tops.size() && minRotation > swap)
-                    minRotation = swap;
-        }
-        return minRotation == INT_MAX ? -1 : minRotation;
-    }
-    
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        
-        // try to make all tops faces same
-        int ab = dominoRotations(tops,bottoms);
-        
-        // try to make all tops faces same
-        int ba = dominoRotations(bottoms,tops);
-        
-        return ab == -1 ? ba : ba == -1 ? ab : min(ab,ba);
+        int ans = INT_MAX, n = tops.size();
+        for(int i = 1; i <= 6; i++) {
+            int tc = 0, bc = 0, ts = 0, bs = 0;
+            for(int j = 0; j < n; j++) {
+                if(tops[j] == i) {
+                    tc++;
+                } else if(bottoms[j] == i) {
+                    bs++;
+                }
+            }
+            if(tc + bs == n) {
+                ans = min(ans, bs);
+            }
+
+            for(int j = 0; j < n; j++) {
+                if(bottoms[j] == i) {
+                    bc++;
+                } else if(tops[j] == i) {
+                    ts++;
+                }
+            }
+            if(bc + ts == n) {
+                ans = min(ans, ts);
+            }
+        }
+        return ans == INT_MAX ? -1 : ans;
     }
 };
