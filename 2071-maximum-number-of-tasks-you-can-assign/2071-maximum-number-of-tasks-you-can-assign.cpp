1a146f875967bd8a9d25ea1474564f6a81fa3ab5
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-05-04 12:13:08 +0530

    Time: 731 ms (44.29%), Space: 286.3 MB (21.35%) - LeetHub

--- 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.cpp
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     bool possible(int m, vector<int>& tasks, vector<int>& workers, int pills, int strength) {
-        multiset<int> mset(workers.end() - m, workers.end());  // Take strongest 'm' workers
-        for (int i = m - 1; i >= 0; i--) {  // Traverse from hardest to easiest task
-            auto it = mset.lower_bound(tasks[i]);
-            if (it == mset.end()) {
+        multiset<int> workerSet(workers.end() - m, workers.end());
+        for (int i = m - 1; i >= 0; i--) {
+            auto it = workerSet.lower_bound(tasks[i]);
+            if (it == workerSet.end()) {
                 if (pills == 0) return false;
-                // Try using a pill
-                it = mset.lower_bound(tasks[i] - strength);
-                if (it == mset.end()) return false;
+                it = workerSet.lower_bound(tasks[i] - strength);
+                if (it == workerSet.end()) return false;
                 pills--;
             }
-            if (*it < tasks[i]) return false;
-            mset.erase(it);
+            workerSet.erase(it);
         }
         return true;
     }
 
     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
-        sort(workers.begin(), workers.end());
         sort(tasks.begin(), tasks.end());
-        int l = 0, r = min((int)tasks.size(), (int)workers.size()), ans = 0;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (possible(m, tasks, workers, pills, strength)) {
-                ans = m + 1;
-                l = m + 1;
+        sort(workers.begin(), workers.end());
+        int left = 0, right = min(tasks.size(), workers.size());
+        int ans = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (possible(mid, tasks, workers, pills, strength)) {
+                ans = mid;
+                left = mid + 1;
             } else {
-                r = m - 1;
+                right = mid - 1;
             }
         }
         return ans;
