1ff9c56007458eeada69b58dcbf6e4b9ac4c4456
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-05-08 14:12:56 +0530

    Time: 287 ms (68.35%), Space: 97.5 MB (76.79%) - LeetHub

--- 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.cpp
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& mt) {
-        priority_queue<array<int, 4>, vector<array<int, 4>>, greater<array<int, 4>>> pq;
-        pq.emplace(array<int, 4>{0, 0, 0, 1});
+        priority_queue<array<int, 4>, vector<array<int, 4>>, greater<array<int, 4>>> minpq;
+        minpq.push({0, 0, 0, 1});
         int m = mt.size(), n = mt[0].size();
         int dir[5] = {-1, 0, 1, 0, -1};
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-
-        while (!pq.empty()) {
-            auto [move, row, col, step] = pq.top();
-            pq.pop();
-            if (row == m - 1 && col == n - 1) {
-                return move;
+        vector<vector<int>> mc(m, vector<int>(n, INT_MAX));
+        while (!minpq.empty()) {
+            auto [cst, row, col, adj] = minpq.top();
+            minpq.pop();
+            if (cst > mc[row][col]) {
+                continue;
             }
-            if (vis[row][col] == 1) continue;
-            vis[row][col] = 1;
-
-            for (int i = 1; i < 5; i++) {
-                int nrow = dir[i - 1] + row, ncol = dir[i] + col;
-                if (nrow < 0 || ncol < 0 || nrow >= m || ncol >= n || vis[nrow][ncol] == 1) continue;
-                pq.emplace(array<int, 4>{max(move, mt[nrow][ncol]) + (step == 1 ? 1 : 2), nrow, ncol, step == 1 ? 2 : 1});
+            if (row == m - 1 && col == n - 1) {
+                return cst;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + dir[i], ncol = col + dir[i + 1];
+                if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n) {
+                    int ncst = max(cst, mt[nrow][ncol]);
+                    ncst += (adj == 1) ? 1 : 2;
+                    if (ncst < mc[nrow][ncol]) {
+                        mc[nrow][ncol] = ncst;
+                        minpq.push({ncst, nrow, ncol, (adj == 1) ? 2 : 1});
+                    }
+                }
             }
         }
         return -1;
