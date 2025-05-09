5b1534a9034536033fbcc590408ca41281114288
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-05-09 23:24:13 +0530

    Time: 0 ms (100%), Space: 7.9 MB (86.42%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    const int MOD = 1e9+7;
+    int dp[1001][2];
+    int numTilings(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(0, n, false);
+    }
+    long solve(int i, int n, bool previousGap) {
+        if(i > n)
+            return 0;
+        if(i == n)
+            return !previousGap;
+        if(dp[i][previousGap] != -1) {
+            return dp[i][previousGap];
+        }
+        if(previousGap)
+            return dp[i][previousGap] = (solve(i+1, n, false) + solve(i+1, n, true)) % MOD;
+        return dp[i][previousGap] = (solve(i+1, n, false) + solve(i+2, n, false) + 2l*solve(i+2, n, true)) % MOD;
+    }
+};
