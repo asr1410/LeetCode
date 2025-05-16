d035c16c8b45beaaa87e111c41679e2af195ddf9
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-05-16 16:42:00 +0530

    Time: 143 ms (17.19%), Space: 107.1 MB (11.72%) - LeetHub

---
+++ 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    vector<vector<int>> choice;
+
+    bool check(string& c, string& p) {
+        int cnt = 0;
+        for (int i = 0; i < c.size(); i++) {
+            cnt += c[i] != p[i];
+        }
+        return cnt == 1;
+    }
+
+    int helper(int c, int p, vector<string>& words, vector<int>& groups) {
+        if (c == words.size()) return 0;
+
+        if (dp[c][p + 1] != -1) return dp[c][p + 1];
+
+        int take = 0, notTake = 0;
+        if (p == -1 || (groups[c] != groups[p] && words[c].size() == words[p].size() && check(words[c], words[p]))) {
+            take = 1 + helper(c + 1, c, words, groups);
+        }
+        notTake = helper(c + 1, p, words, groups);
+
+        if (take > notTake) {
+            dp[c][p + 1] = take;
+            choice[c][p + 1] = 1; // we took c
+        } else {
+            dp[c][p + 1] = notTake;
+            choice[c][p + 1] = 0; // we skipped c
+        }
+        return dp[c][p + 1];
+    }
+
+    vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n = words.size();
+        dp.assign(n, vector<int>(n + 1, -1));
+        choice.assign(n, vector<int>(n + 1, -1));
+
+        int cnt = helper(0, -1, words, groups);
+
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j <= n; j++) {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // cout << endl;
+        // cout << endl;
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j <= n; j++) {
+        //         cout << choice[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        // Reconstruct the sequence
+        vector<string> result;
+        int i = 0, prev = -1;
+
+        while (cnt > 0 and i < n) {
+            while (i < n and dp[i][prev + 1] == cnt) {
+                i++;
+            }
+            result.push_back(words[i - 1]);
+            prev = i - 1;
+            cnt--;
+        }
+
+        return result;
+    }
+};
