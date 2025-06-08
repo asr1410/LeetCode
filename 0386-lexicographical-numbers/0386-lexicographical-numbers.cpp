7abc9b546991835ec9081e23360581bf4c89c3cd
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2025-06-09 00:12:12 +0530

    Time: 6 ms (28.09%), Space: 15.1 MB (26.31%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> ans;
-
     void helper(int k, int n) {
-        if (k > n) return;
+        if(k > n) {
+            return;
+        }
         ans.push_back(k);
-        for (int i = 0; i <= 9; i++) {
-            int next = k * 10 + i;
-            helper(next, n);
+        for(int i = 0; i <= 9; i++) {
+            helper(k * 10 + i, n);
         }
     }
-
     vector<int> lexicalOrder(int n) {
-        for (int i = 1; i <= 9; i++) {
-            if (i > n) break;
+        for(int i = 1; i <= 9; i++) {
             helper(i, n);
         }
         return ans;
